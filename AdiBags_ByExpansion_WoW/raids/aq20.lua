30e1e51e62bd167d192607823738d2f0b0f7c29f
davidcraig <davidcraig1986@gmail.com>
2018-05-26 15:05:47 +0100

    Add all AQ20 boss drops.

---
+++ AdiBags_ByExpansion_WoW/raids/aq20.lua
@@ -0,0 +1,75 @@
+local AddonName, AddonTable = ...
+
+AddonTable.aq20 = {
+    -- Shared Boss Loot
+    20888, -- Qiraji Ceremonial Ring
+    20884, -- Qiraji Magisterial Ring
+    20885, -- Qiraji Martial Drape
+    20889, -- Qiraji Regal Drape
+    20890, -- Qiraji Ornate Hilt
+    20886, -- Qiraji Spiked Hilt
+
+    -- Kurinaxx
+    21498, -- Qiraji Sacrificial Dagger
+    21499, -- Vestments of the Shifting Sands
+    21502, -- Sand Reaver Wristguards
+    21501, -- Toughened Silithid Hide Gloves
+    21500, -- Belt of the Inquisition
+    21503, -- Belt of the Sand Reaver
+
+    -- General Rajaxx
+    21492, -- Manslayer of the Qiraji
+    21496, -- Bracers of Qiraji Command
+    21494, -- Southwind's Grasp
+    21495, -- Legplates of the Qiraji Command
+    21497, -- Boots of the Qiraji General
+    21493, -- Boots of the Vanguard
+
+    -- Moam
+    21471, -- Talon of Furious Concentration
+    21472, -- Dustwind Turban
+    21455, -- Southwind Helm
+    21474, -- Chitinous Shoulderguards
+    21468, -- Mantle of Maz'Nadir
+    21470, -- Cloak of the Savior
+    21467, -- Thick Silithid Chestguard
+    21469, -- Gauntlets of Southwind
+    21479, -- Gauntlets of the Immovable
+    21475, -- Legplates of the Destroyer
+    21476, -- Obsidian Scaled Leggings
+    21477, -- Ring of Fury
+    21473, -- Eye of Moam
+
+    -- Buru the Gorger
+    21485, -- Buru's Skull Fragment
+    21491, -- Scaled Bracers of the Gorger
+    21486, -- Gloves of the Swarm
+    21487, -- Slimy Scaled Gauntlets
+    21489, -- Quicksand Waders
+    21490, -- Slime Kickers
+    21488, -- Fetish of Chitinous Spikes
+
+    -- Ayamiss the Hunter
+    21466, -- Stinger of Ayamiss
+    21478, -- Bow of Taut Sinew
+    21484, -- Helm of Regrowth
+    21479, -- Gauntlets of the Immovable
+    21480, -- Scaled Silithid Gauntlets
+    21481, -- Boots of the Desert Protector
+    21483, -- Ring of the Desert Winds
+
+    -- Ossirian the Unscarred
+    21452, -- Staff of the Ruins
+    21715, -- Sand Polished Hammer
+    21459, -- Crossbow of Imminent Doom
+    21460, -- Helm of Domination
+    21453, -- Mantle of the Horusath
+    21454, -- Runic Stone Shoulders
+    21456, -- Sandstorm Cloak
+    21457, -- Bracers of Brutality
+    21464, -- Shackles of the Unscarred
+    21458, -- Gauntlets of New Life
+    21462, -- Gloves of Dark Wisdom
+    21463, -- Ossirian's Binding
+    21461, -- Leggings of the Black Blizzard
+}
