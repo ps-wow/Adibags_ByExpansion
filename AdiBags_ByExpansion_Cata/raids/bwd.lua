2ca2334be4132d25b89d113f1cb0f0003a86794f
davidcraig <davidcraig1986@gmail.com>
2018-05-27 12:24:22 +0100

    Add all boss drops and 2x hunter tier pieces.

--- AdiBags_ByExpansion_Cata/raids/bwd.lua
+++ AdiBags_ByExpansion_Cata/raids/bwd.lua
@@ -32,4 +32,75 @@ AddonTable.bwd = {
     65051, -- Molten Tantrum Boots
     65048, -- Symbiotic Worm
     65259, -- Shadowflame Handwraps
+
+    -- Atramedes
+    65058, -- Themios the Darkbringer
+    65052, -- Kingdom's Heart
+    65066, -- Helm of the Blind Seer
+    65059, -- Ironstar Amulet
+    65054, -- Mantle of Roaring Flames
+    65062, -- Breastplate of Ancient Kings
+    65060, -- Sark of the Unwatched
+    65056, -- Bracers of the Burningeye
+    65055, -- Gloves of Cacophony
+    65061, -- Legguards of the Unseeing
+    65063, -- Boots of Vertigo
+    65053, -- Bell of Enraging Resonance
+
+    -- Chimaeron
+    65064, -- Finkle's Mixer Upper
+    65067, -- Burden of Mortality
+    65068, -- Chaos Beast Bracers
+    65028, -- Chimaeron Armguards
+    65021, -- Manacles of the Sleeping Beast
+    65065, -- Brackish Gloves
+    65073, -- Double Attack Handguards
+    65071, -- Plated Fists of Provocation
+    65069, -- Einhorn's Galoshes
+    65075, -- Massacre Treads
+    65070, -- Bile-O-Tron Nut
+    65072, -- Heart of Rage
+
+    -- Maloriak
+    152967, -- Experiment-In-A-Jar (Discarded Experiment) [Battle Pet]
+    67428, -- Leggings of the Forlorn Conqueror (Pal, Priest, Lock)
+    67437, -- Leggings of the Forlorn Protector (Warr, Hunt, Sham)
+    67426, -- Leggings of the Forlorn Vanquisher (Rog, DK, Mage, Druid)
+    65036, -- Mace of Acrid Death
+    65038, -- Dragon Bone Warhelm
+    65035, -- Cloak of Biting Chill
+    65037, -- Tunic of Failed Experiments
+    65031, -- Flash Freeze Gauntlets
+    65040, -- Belt of Absolute Zero
+    65034, -- Belt of Arcane Storms
+    65039, -- Aberration's Leggings
+    65030, -- Leggings of Consuming Flames
+    65032, -- Legwraps of the Greatest Son
+    65033, -- Treads of Flawless Creation
+    65029, -- Jar of Ancient Remedies
+
+    -- Nefarion's End
+    122195, -- Music Roll: Legends of Azeroth
+    65001, -- Crown of the Forlorn Conqueror (Pal, Priest, Lock)
+    65000, -- Crown of the Forlorn Protector (Warr, Hunt, Sham)
+    65002, -- Crown of the Forlorn Vanquisher (Rog, DK, Mage, Druid)
+    138802, -- Illusion: Power Torrent
+    152968, -- Shadowy Pile of Bones (Rattlejaw) [Battle Pet]
+    65003, -- Reclaimed Ashkandi, Greatsword of the Brotherhood
+    65017, -- Andoros, Fist of the Dragon King
+    65024, -- Crul'korak, the Lightning's Arc
+    65023, -- Akmin-Kurai, Dominion's Shield
+    65025, -- Rage of Ages
+    65043, -- Mantle of Nerarius
+    65027, -- Pauldrons of the Apocalypse
+    65074, -- Spaulders of the Scarred Lady
+    65018, -- Shadow of Dread
+    65019, -- Shadowblaze Robes
+    65022, -- Belt of the Blackhand
+    65057, -- Belt of the Nightmare
+    65026, -- Prestor's Talisman of Machination
+
+    -- Tier Pieces (From Tokens)
+    65205, -- Lightning-Charged Gloves
+    65205, -- Lightning-Charged Legguards
 }
