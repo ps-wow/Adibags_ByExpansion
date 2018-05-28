ff4a846678607d2579cc542b93e10b255b8b6b03
davidcraig <davidcraig1986@gmail.com>
2018-05-28 18:13:06 +0100

    Add remaining AQ40 loot.

--- AdiBags_ByExpansion_WoW/raids/aq40.lua
+++ AdiBags_ByExpansion_WoW/raids/aq40.lua
@@ -4,6 +4,8 @@ AddonTable.aq40 = {
     -- Shared Boss Loot
     21232, -- Imperial Qiraji Armaments
     21237, -- Imperial Qiraji Regalia
+    20928, -- Qiraji Bindings of Command
+    20932, -- Qiraji Bindings of Dominance
 
     -- The Prophet Skeram
     93041, -- Jewel of Maddening Whispers (Mini Mindslayer) [Battle Pet]
@@ -42,4 +44,96 @@ AddonTable.aq40 = {
     21681, -- Ring of the Devoured
     21687, -- Ukko's Ring of Darkness
     21685, -- Petrified Scarab
+
+    -- Battleguard Sartura
+    21673, -- Silithid Claw
+    21666, -- Sartura's Might
+    21669, -- Creeping Vine Helm
+    21678, -- Necklace of Purity
+    21671, -- Robes of the Battleguard
+    21674, -- Gauntlets of Steadfast Determination
+    21672, -- Gloves of Enforcement
+    21675, -- Thick Qirajihide Belt
+    21676, -- Leggings of the Festering Swarm
+    21667, -- Legplates of Blazing Light
+    21668, -- Scaled Leggings of Qiraji Fury
+    21648, -- Recomposed Boots
+    21670, -- Badge of the Swarmguard
+
+    -- Fankriss the Unyielding
+    21635, -- Barb of the Sand Reaver
+    21650, -- Ancient Qiraji Ripper
+    21664, -- Barbed Choker
+    21665, -- Mantle of Wicked Revenge
+    21639, -- Pauldrons of the Unrelenting
+    21627, -- Cloak of Untold Secrets
+    21663, -- Robes of the Guardian Spirit
+    21652, -- Silithid Carapace Chestguard
+    21651, -- Scaled Sand Reaver Leggings
+    21645, -- Hive Tunneler's Boots
+    21647, -- Fetish of the Sand Reaver
+
+    -- Viscidus
+    93039, -- Viscidus Globule [Battle Pet]
+    21622, -- Sharpened Silithid Femur
+    21624, -- Gauntlets of Kalimdor
+    21623, -- Gauntlets of the Righteous Champion
+    21626, -- Slime-Coated Leggings
+    21677, -- Ring of the Qiraji Fury
+    21625, -- Scarab Brooch
+
+    -- Princess Huhuran
+    21616, -- Huhuran's Stinger
+    21621, -- Cloak of the Golden Hive
+    21618, -- Hive Defiler Wristguards
+    21619, -- Gloves of the Messiah
+    21617, -- Wasphide Gauntlets
+    21620, -- Ring of the Martyr
+
+    -- The Twin Emperors
+    93040, -- Anubisath Idol [Battle Pet]
+    20735, -- Formula: Enchant Cloak - Subtlety
+    20726, -- Formula: Enchant Cloak - Threat
+    20930, -- Vek'lor's Diadem
+    20926, -- Vek'nilash's Circlet
+    21679, -- Kalimdor's Revenge
+    21597, -- Royal Scepter of Vek'lor
+    21608, -- Amulet of Vek'nilash
+    21604, -- Bracelets of Royal Redemption
+    21602, -- Qiraji Execution Bracers
+    21605, -- Gloves of the Hidden Temple
+    21599, -- Vek'lor's Gloves of Devastation
+    21606, -- Belt of the Fallen Emperor
+    21607, -- Grasp of the Fallen Emperor
+    21609, -- Regenerating Belt of Ven'nilash
+    21598, -- Royal Qiraji Belt
+    21600, -- Boots of Epiphany
+    21601, -- Ring of Emperor Vek'lor
+
+    -- Ouro
+    20927, -- Ouro's Intact Hide
+    20931, -- Skin of the Great Sandworm
+    23557, -- Larvae of the Great Worm
+    21610, -- Wormscale Blocker
+    21615, -- Don Rigoberto's Lost Hat
+    21611, -- Burrower Bracers
+    23570, -- Jom Gabbar
+    23558, -- The Burrower's Shell
+
+    -- C'thun
+    20929, -- Carapace of the Old God
+    20933, -- Husk of the Old God
+    21134, -- Dark Edge of Insanity
+    21126, -- Death's Sting
+    21839, -- Scepter of the False Prophet
+    22732, -- Mark of C'Thun
+    21583, -- Cloak of Clarity
+    22731, -- Cloak of the Devoured
+    21585, -- Dark Storm Gauntlets
+    21581, -- Gauntlets of Annihilation
+    21586, -- Belt of Never-Ending Agony
+    22730, -- Eyestalk Waist Cord
+    21582, -- Grasp of the Old God
+    21596, -- Ring of the Godslayer
+    21579, -- Vanquished Tentacle of C'Thun
 }
