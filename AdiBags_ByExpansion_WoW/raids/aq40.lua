bfa9bc4d7518391dbb1b4523cf19f718a9ea3e04
davidcraig <davidcraig1986@gmail.com>
2018-05-28 17:21:58 +0100

    AQ40 first 2 bosses.

---
+++ AdiBags_ByExpansion_WoW/raids/aq40.lua
@@ -0,0 +1,45 @@
+local AddonName, AddonTable = ...
+
+AddonTable.aq40 = {
+    -- Shared Boss Loot
+    21232, -- Imperial Qiraji Armaments
+    21237, -- Imperial Qiraji Regalia
+
+    -- The Prophet Skeram
+    93041, -- Jewel of Maddening Whispers (Mini Mindslayer) [Battle Pet]
+    22222, -- Plans: Thick Obsidian Breastplate (Blacksmithing)
+    21703, -- Hammer of Ji'zhi
+    21128, -- Staff of the Qiraji Prophets
+    21702, -- Amulet of Foul Warding
+    21700, -- Pendant of the Qiraji Guardian
+    21699, -- Barrage Shoulders
+    21701, -- Cloak of Concentrated Hatred
+    21814, -- Breastplate of Annihilation
+    21708, -- Beetle Scaled Wristguards
+    21698, -- Leggings of Immersion
+    21705, -- Boots of the Fallen Prophet
+    21704, -- Boots of the Redeemed Prophecy
+    21706, -- Boots of the Unwavering Will
+    21707, -- Ring of Swarming Thought
+
+    -- Silithid Royalty
+    21603, -- Wand of Qiraji Nobility
+    21693, -- Guise of the Devourer
+    21690, -- Angelista's Charm
+    21686, -- Mantle of Phrenic Power
+    21683, -- Mantle of the Desert Crusade
+    21684, -- Mantle of the Desert's Fury
+    21694, -- Ternary Mantle
+    21697, -- Cape of the Trinity
+    21696, -- Robes of the Triumverate
+    21680, -- Vest of Swift Execution
+    21682, -- Bile-Covered Gauntlets
+    21689, -- Gloves of Ebru
+    21691, -- Ooze-Ridden Gauntlets
+    21692, -- Triad Girdle
+    21688, -- Boots of the Fallen Hero
+    21695, -- Angelista's Touch
+    21681, -- Ring of the Devoured
+    21687, -- Ukko's Ring of Darkness
+    21685, -- Petrified Scarab
+}
