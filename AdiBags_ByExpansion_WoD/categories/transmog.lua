cc9cdf1b2b5e5dc77cf5b686e49c29b3efb60d6d
davidcraig <davidcraig1986@gmail.com>
2018-06-02 22:14:47 +0100

    WoD transmog.

--- AdiBags_ByExpansion_WoD/categories/transmog.lua
+++ AdiBags_ByExpansion_WoD/categories/transmog.lua
@@ -3,22 +3,32 @@ local AddonName, AddonTable = ...
 AddonTable.transmog = {
     -- Green
     106426, -- Rimeplate Helm
+    106699, -- Sabermaw Gloves
+    106702, -- Sabermaw Shoulders
+    106705, -- Voidwrap Cowl
+    106707, -- Voidwrap Leggings
     106728, -- Stonecrag Vambraces
     106729, -- Meadowstomper Bindings [Leather][Wrist]
     106732, -- Meadowstomper Gloves
     106735, -- Meadowstomper Shoulders [Leather][Shoulder]
     106742, -- Dark Star Sandals
+    106743, -- Dark Star Shoulderpads [Cloth][Shoulder]
     106744, -- Dark Star Wristwraps
-    106699, -- Sabermaw Gloves
     106752, -- Packrunner Vest
+
     116612, -- Gorian Bow
     116616, -- Gorian Knuckles
     116633, -- Howling Knuckles
     116635, -- Howling Mace
     116638, -- Howling Spear
+    116721, -- Ancestral Branch [OffHand]
 
     -- Epic (Purple)
     119331, -- Bracers of Visceral Force [Plate][Wrist] BrF
-    119340, -- Iron-Flecked Sandals [Leather][Feet] BrF
+    119335, -- Eyeripper Girdle [Leather][Belt] (HM)
+    119338, -- Belt of Inebriated Sorrows [Mail][Belt] (HM)
     119339, -- Treads of the Veteran Smith [Mail][Feet] BrF
+    119340, -- Iron-Flecked Sandals [Leather][Feet] BrF
+    119343, -- Eye-Blinder Greatcloak [Back] (HM)
+    119345, -- Milenah's Intricate Cloak [Back] (HM)
 }
