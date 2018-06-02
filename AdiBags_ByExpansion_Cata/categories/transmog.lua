dbcecf472a23521000a73afc8f6791d491a2176d
davidcraig <davidcraig1986@gmail.com>
2018-06-02 17:16:41 +0100

    cata tmog

--- AdiBags_ByExpansion_Cata/categories/transmog.lua
+++ AdiBags_ByExpansion_Cata/categories/transmog.lua
@@ -7,41 +7,57 @@ AddonTable.transmog = {
     55288, -- Irontree Axe
     55296, -- Bladefist Broadaxe
     55306, -- Irontree Bow
+    55315, -- Blackwolf Cloak
+    55350, -- Bladefist Knuckles
     55367, -- Mereldar Scepter
     55377, -- Thondroril Crystal
+    55387, -- Irontree Mace
     55412, -- Mardenholde Spear
     55414, -- Irontree Spear
     55430, -- Ravencrest Shield
+    55431, -- Stonewrought Shield
     55449, -- Bladefist Staff
     55458, -- Bladefist Sword
     55459, -- Irontree Sword
+    55484, -- Mereldar Wand
+    55485, -- Thondroril Wand
     55548, -- Stonewrought Pauldrons
+    55556, -- Ravencrest Girdle
     55612, -- Talondeep Legguards
     55621, -- Talondeep Spaulders
     55628, -- Highperch Belt
+    55664, -- Dandred Gloves
     55675, -- Ravenholdt Hood
+    55700, -- Dandred Waistband
     55701, -- Haldarr Waistband
     55711, -- Bluefen Wristwraps
+    55738, -- Bluefen Handwraps
     55745, -- Everstill Cowl
     55747, -- Bluefen Cowl
     55763, -- Everstill Shoulderpads
+    55765, -- Bluefen Shoulderpads
     59463, -- Maldo's Sword Cane
     59521, -- Soul Blade
     59901, -- Heaving Plates of Protection [Plate][Shoulder]
     67119, -- Thousand Bandage Drape
-    71366, -- Laba Bolt Crossbow
+    71366, -- Lava Bolt Crossbow
+    55648, -- Ravenholdt Jerkin
+    55602, -- Thornsnarl Helm
 
     -- Rare (Blue)
     66941, -- Triple-Loop Belt (BoT)
     67061, -- Bulwark of the Scorned Man
 
     -- Epic (Purple)
+    59460, -- Theresa's Booklight (BoE Wand) (BwD)
     59462, -- Maimgor's Bite [1H Axe] (BoT)
+    59464, -- Treads of Savage Beatings [Plate][Boots]
     59466, -- Ironstar's Impenetrable Cover
     59467, -- Hide of Chromaggus
     59525, -- Chelley's Staff of Dark Mending [Staff] (BoT)
     60210, -- Crossfire Carbine [Gun] (BoT)
     60211, -- Bracers of the Dark Pool [Cloth][Wrist]
+    68601, -- Scaleslicer [Dagger]
+    71359, -- Chelley's Sterilized Scalpel
     71360, -- Spire of Scarlet Pain
-    59464, -- Treads of Savage Beatings [Plate][Boots]
 }
