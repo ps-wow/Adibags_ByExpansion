6d298c78a865141507b63a4c96fcb0a55b5c1cc9
davidcraig <davidcraig1986@gmail.com>
2018-06-02 17:22:05 +0100

    Add all firelands 25 heroic loot.

--- AdiBags_ByExpansion_Cata/raids/firelands.lua
+++ AdiBags_ByExpansion_Cata/raids/firelands.lua
@@ -1,7 +1,142 @@
 local AddonName, AddonTable = ...
 
 AddonTable.firelands = {
+    -- Shared Boss Loot
     71617, -- Crystallized Firestone
+    71774, -- Smoldering Censer of Purity
+    71781, -- Zoid's Firelit Greatsword
+    71778, -- Avool's Incendiary Shanker
+    71786, -- Entrail Disgorger
+    71777, -- Eye of Purification
+    71784, -- Firethorn Mindslicer
+    71783, -- Shatterskull Bonecrusher
+
+    -- Beth'tilac
+    152976, -- Cinderweb Egg (Cinderweb Recluse) [Battle Pet]
+    71409, -- Funeral Pyre
+    71406, -- Mandible of Beth'tilac
+    71408, -- Ward of the Red Widow
+    71411, -- Cowl of the Clicking Menace
+    71403, -- Flickering Shoulders
+    71413, -- Spaulders of Manifold Eyes
+    71405, -- Carapace of Imbibed Flame
+    71407, -- Robes of Smoldering Devastation
+    71410, -- Cindersilk Gloves
+    71402, -- Cinderweb Leggings
+    71412, -- Thoracic Flame Kilt
+    71404, -- Aracnaflame Treads
+    71401, -- Widow's Kiss
+    69138, -- Spidersilk Spindle
+
+    -- Lord Rhyolith
+    71422, -- Volcanospike
+    71414, -- Arbalest of Erupting Fury
+    71421, -- Flickering Cowl
+    71416, -- Hood of Rampant Disdain
+    71423, -- Heartstone of Rhyolith
+    71415, -- Dreadfire Drape
+    71417, -- Flaming Core Chestguard
+    71424, -- Incendic Chestguard
+    71418, -- Earthcrack Bracers
+    71425, -- Lava Line Wristbands
+    71419, -- Fireskin Gauntlets
+    71426, -- Grips of the Raging Giant
+    71420, -- Cracked Obsidian Stompers
+
+    -- Alysrazor
+    71679, -- Chest of the Fiery Conqueror
+    71686, -- Chest of the Fiery Protector
+    71672, -- Chest of the Fiery Vanquisher
+    71665, -- Flametalon of Alysrazor [Mount]
+    71427, -- Alysra's Razor
+    71430, -- Greathelm of the Voracious Maw
+    71438, -- Craterflame Spaulders
+    71432, -- Spaulders of Recurring Flame
+    71434, -- Wings of Flame
+    71439, -- Clutch of the Firemother
+    71428, -- Flickering Wristbands
+    71437, -- Clawshaper Gauntlets
+    71431, -- Lavaworm Legplates
+    71435, -- Leggings of Billowing Fire
+    71429, -- Moltenfeather Leggings
+    71436, -- Phoenix-Down Treads
+    71433, -- Alysrazor's Band
+    69149, -- Eye of Blazing Power
+
+    -- Shannox
+    71678, -- Leggings of the Fiery Conqueror
+    71685, -- Leggings of the Fiery Protector
+    71671, -- Leggings of the Fiery Vanquisher
+    152975, -- Smoldering Treat (Blazehound) [Battle Pet]
+    71445, -- Skullstealer Greataxe
+    71441, -- Feeding Frenzy
+    71448, -- Goblet of Anger
+    71442, -- Scalp of the Bandit Prince
+    71446, -- Neclace of Fetishes
+    71450, -- Flickering Shoulderpads
+    71452, -- Bracers of the Dread Hunter
+    71440, -- Gloves of Dissolving Smoke
+    71443, -- Uncrushable Belt of Fury
+    71453, -- Legplates of Absolute Control
+    71444, -- Legplates of Frenzied Devotion
+    71447, -- Coalwalker Sandals
+    71451, -- Treads of Implicit Obedience
+    71449, -- Crystal Prison Band
+
+    -- Baleroc, the Gatekeeper
+    71676, -- Gauntlets of the Fiery Conqueror
+    71683, -- Gauntlets of the Fiery Protector
+    71669, -- Gauntlets of the Fiery Vanquisher
+    152977, -- Vibrating Stone (Surger) [Battle Pet]
+    71454, -- Gatecrasher
+    71460, -- Shard of Torment
+    71462, -- Molten Scream
+    71465, -- Casque of Flame
+    71459, -- Helm of Blazing Glory
+    71461, -- Mantle of Closed Doors
+    71456, -- Shoulderpads of the Forgotten Gate
+    71455, -- Breastplate of the Incendiary Soul
+    71464, -- Gatekeeper's Embrace
+    71463, -- Glowing Wing Bracers
+    71458, -- Flickering Handguards
+    71457, -- Decimation Treads
+    69139, -- Necromantic Focus
+
+    -- Majordomo Staghelm
+    152978, -- Fandral's Pet Carrier
+    71680, -- Gauntlets of the Fiery Conqueror
+    71687, -- Gauntlets of the Fiery Protector
+    71673, -- Gauntlets of the Fiery Vanquisher
+    71466, -- Fandral's Flamescythe
+    71473, -- Stinger of the Flaming Scorpion
+    71472, -- Flowform Choker
+    71469, -- Breastplate of Shifting Visions
+    71470, -- Bracers of the Fiery Path
+    71471, -- Wristwraps of Arrogant Doom
+    71468, -- Grips of Unerring Precision
+    71474, -- Firecat Leggings
+    71467, -- Sandals of Leaping Coals
+    71475, -- Treads of the Pentitent Man
+    69111, -- Jaws of Defeat
+    69112, -- The Hungerer
+
+    -- Ragnaros
+    71677, -- Crown of the Fiery Conqueror
+    71684, -- Crown of the Fiery Protector
+    71670, -- Crown of the Fiery Vanquisher
+    69224, -- Smoldering Egg of Millagazor
+    71797, -- Sho'ravon, Greatstaff of Annihilation
+    70723, -- Sulfuras, the Extinguished Hand
+    71615, -- Ko'ggun, Hammer of the Firelord
+    71611, -- Arathar, the Eye of Flame
+    71616, -- Crown of Flame
+    71610, -- Choker of the Vanquished Lord
+    71612, -- Pauldrons of Roaring Flame
+    71614, -- Fingers of Incineration
+    71613, -- Majordomo's Chain of Office
+    69150, -- Matrix Restabilizer
+    69110, -- Variable Pulse Lightning Capacitor
+    69167, -- Vessel of Acceleration
 
     -- Set Pieces (from Tokens)
     71602, -- Legplates of the Molten Giant (Warrior)
@@ -9,4 +144,12 @@ AddonTable.firelands = {
     71601, -- Gauntlets of the Molten Giant (Warrior)
     71603, -- Pauldrons of the Molten Giant (Warrior)
     71599, -- Helmet of the Molten Giant (Warrior)
+    71491, -- Obsidian Arborweave Handwraps (Druid)
+    71493, -- Obsidian Arborweave Legwraps (Druid)
+    71494, -- Obsidian Arborweave Tunic (Druid)
+    71495, -- Obsidian Arborweave Mantle (Druid)
+    71597, -- Balespider's Robes (Warlock)
+    71598, -- Balespider's Mantle (Warlock)
+    71594, -- Balespider's Handwraps (Warlock)
+    71595, -- Balespider's Hood (Warlock)
 }
