03c5e7484da6b240355c33112cca300c0551dccd
davidcraig <davidcraig1986@gmail.com>
2018-06-07 23:40:00 +0100

    Legion junk.

--- AdiBags_ByExpansion_Legion/categories/junk.lua
+++ AdiBags_ByExpansion_Legion/categories/junk.lua
@@ -1,10 +1,12 @@
 local AddonName, AddonTable = ...
 
 AddonTable.junk = {
-    132200, -- Ashen Ring
-    132199, -- Congealed Felblood
-    132184, -- Intact Shimmering Scale
+    121363, -- Warped-Plate Helmet
+    121368, -- Sunbleached Shirt
     132183, -- Razor Tooth
+    132184, -- Intact Shimmering Scale
+    132199, -- Congealed Felblood
+    132200, -- Ashen Ring
     132204, -- Sticky Volatile Substance
     132231, -- Won Hooked Claw
     132233, -- Lingering Essence
@@ -12,7 +14,7 @@ AddonTable.junk = {
 
     -- Blingtron 6000
     114009, -- Viral Sample (Blingtron)
-    114013, -- "01000010" (Blingtron)
     114005, -- Mark of the Khor (Blingtron)
     114006, -- Spirium
+    114013, -- "01000010" (Blingtron)
 }
