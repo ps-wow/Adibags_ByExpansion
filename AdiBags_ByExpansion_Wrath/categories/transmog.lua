0f8565733cf743d9330e5a672998e836a22d6cfc
davidcraig <davidcraig1986@gmail.com>
2018-06-08 17:59:12 +0100

    Wrath transmog.

---
+++ AdiBags_ByExpansion_Wrath/categories/transmog.lua
@@ -0,0 +1,13 @@
+local AddonName, AddonTable = ...
+
+AddonTable.transmog = {
+    36037, -- Condor Robe
+    36045, -- Crystalsong Robe
+    36053, -- Vizier Robe
+    36057, -- Vizier Mantle
+    36167, -- Webspinner Hood
+
+    36262, -- Cormorant Gloves
+
+    36372, -- Kraken Greaves
+}
