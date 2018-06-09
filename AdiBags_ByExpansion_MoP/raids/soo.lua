23cc52fbcfb7f10c9265f36f3b5167548368470b
davidcraig <davidcraig1986@gmail.com>
2018-06-09 23:36:46 +0100

    All norushen loot

--- AdiBags_ByExpansion_MoP/raids/soo.lua
+++ AdiBags_ByExpansion_MoP/raids/soo.lua
@@ -48,6 +48,30 @@ AddonTable.soo = {
     112484, -- Sorrowpath Signet
     112476, -- Rook's Unlucky Talisman
 
+    -- Norushen
+    112547, -- Norushen's Shortblade
+    112546, -- Xifeng, Longblade of the Titanic Guardian
+    112560, -- Norushen's Enigmatic Barrier
+    112552, -- Rage-Blind Greathelm
+    112559, -- Untainted Guardian's Chain
+    112550, -- Quarantine Shoulderguards
+    112558, -- Rime-Rift Shoulders
+    112554, -- Drape of the Despairing Pit
+    112549, -- Robes of the Blackened Watcher
+    112545, -- Bracers of Blind Hatred
+    112555, -- Bracers of Broken Causation
+    112563, -- Bracers of Final Serenity
+    112565, -- Wristplates of Broken Doubt
+    112556, -- Blight Hurlers
+    112562, -- Confident Grips
+    112548, -- Gloves of the Golden Protector
+    112557, -- Sash of the Last Guardian
+    112551, -- Leggings of Unabashed Anger
+    112564, -- Shadow-Binder's Kilt
+    112561, -- Boots of Broken Reliance
+    112553, -- Reality Ripper Ring
+    112503, -- Fusion-Fire Core
+
     -- Garrosh
     105868, -- Essence of the Cursed Vanquisher
 }
