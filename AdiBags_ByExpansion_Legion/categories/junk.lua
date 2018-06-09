55312ea7aa488a3c221fd84b2b3bcb6b22d1f92d
davidcraig <davidcraig1986@gmail.com>
2018-06-09 10:10:14 +0100

    Legion junk.

--- AdiBags_ByExpansion_Legion/categories/junk.lua
+++ AdiBags_ByExpansion_Legion/categories/junk.lua
@@ -3,14 +3,23 @@ local AddonName, AddonTable = ...
 AddonTable.junk = {
     121363, -- Warped-Plate Helmet
     121368, -- Sunbleached Shirt
+
     132183, -- Razor Tooth
     132184, -- Intact Shimmering Scale
+    132193, -- Tuft of Plumage
     132199, -- Congealed Felblood
+
     132200, -- Ashen Ring
     132204, -- Sticky Volatile Substance
+    132212, -- Frothing Essence
+    132213, -- Drop of Magic
+    132217, -- Elemental Pebbles
     132231, -- Won Hooked Claw
     132233, -- Lingering Essence
     132239, -- Bone Splinters
+
+    -- Sentinax Consumables
+    146907, -- Sentinax Beacon of Warbeasts
 
     -- Blingtron 6000
     114009, -- Viral Sample (Blingtron)
