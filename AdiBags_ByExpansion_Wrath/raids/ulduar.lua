8c723698bae4a9ae9a506909c221c68a8ab82e12
davidcraig <davidcraig1986@gmail.com>
2018-06-26 19:19:00 +0100

    Ulduar - Flame Leviathan - Normal

--- AdiBags_ByExpansion_Wrath/raids/ulduar.lua
+++ AdiBags_ByExpansion_Wrath/raids/ulduar.lua
@@ -1,5 +1,43 @@
 local AddonName, AddonTable = ...
 
 AddonTable.ulduar = {
+    -- NORMAL
+    -- Shared
+    45038, -- Fragment of Val'anyr
+    45087, -- Runed Orb
+    -- Flame Leviathan
+    45282, -- Ironsoul
+    45287, -- Firesoul
+    45132, -- Golden Saronite Dragon
+    45284, -- Kinetic Ripper
+    45110, -- Titanguard
+    45115, -- Leviathan Fueling Manual
+    45107, -- Iron Riveted War Helm
+    45289, -- Lifespark Visage
+    45118, -- Steamworker's Goggles
+    45116, -- Freya's Choker of Warding
+    45285, -- Might of the Leviathan
+    45133, -- Pendant of Fiery Havoc
+    45300, -- Mantle of Fiery Vengeance
+    45136, -- Shoulderpads of Dormant Energies
+    45288, -- Firestrider Chestguard
+    45291, -- Combustion Bracers
+    45283, -- Flamewatch Armguards
+    45108, -- Mechanist's Bindings
+    45111, -- Mimiron's Inferno Couplings
+    45117, -- Constructor's Handwraps
+    45109, -- Gloves of the Fiery Behemoth
+    45293, -- Handguards of Potent Cures
+    45119, -- Embrace of the Leviathan
+    45295, -- Gilded Steel Legplates
+    45134, -- Plated Leggings of Ruination
+    45135, -- Boots of Fiery Resolution
+    45113, -- Glowing Ring of Reclamation
+    45297, -- Shimmering Seal
+    45106, -- Strength of the Automaton
+    45112, -- The Leviathan's Coil
+    45292, -- Energy Siphon
+    45286, -- Pyrite Infuser
 
+    -- TIMEWALKING
 }
