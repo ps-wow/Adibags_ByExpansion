c055b4d3fc4a0bfca6519ba4343fbf4d7c180ece
davidcraig <davidcraig1986@gmail.com>
2018-06-30 10:43:10 +0100

    Gruul's Lair.

--- AdiBags_ByExpansion_TBC/categories/junk.lua
+++ AdiBags_ByExpansion_TBC/categories/junk.lua
@@ -9,6 +9,7 @@ AddonTable.junk = {
     25405, -- Rusted Musket
     25406, -- Broken Longbow
     25409, -- Resilient Tail Hair
+    25411, -- Worn Hoof
 
     33415, -- Frost-Worn Plate Chestpiece
     33366, -- Frost-Rimed Cloth Boots
--- AdiBags_ByExpansion_TBC/raids/gl.lua
+++ AdiBags_ByExpansion_TBC/raids/gl.lua
@@ -1,4 +1,32 @@
 local AddonName, AddonTable = ...
 
+-- [Raid] Gruul's Lair: 25 Normal
 AddonTable.gl = {
+    -- High King Maulgar
+    29763, -- Pauldrons of the Fallen Champion
+    29764, -- Pauldrons of the Fallen Defender
+    29762, -- Pauldrons of the Fallen Hero
+    28800, -- Hammer of the Naaru
+    28796, -- Malefic Mask of the Shadows
+    28801, -- Maulgar's Warhelm
+    28797, -- Brute Cloak of the Ogre-Magi
+    28795, -- Bladespire Warbands
+    28799, -- Belt of Divine Inspiration
+
+    -- Gruul the Dragonkiller
+    29766, -- Leggings of the Fallen Champion
+    29767, -- Leggings of the Fallen Defender
+    29765, -- Leggings of the Fallen Hero
+    28794, -- Axe of the Gronn Lords
+    28802, -- Bloodmaw Magus-Blade
+    28825, -- Aldori Legacy Defender
+    28804, -- Collar of Cho'gall
+    28803, -- Cowl of Nature's Breath
+    28822, -- Teeth of Gruul
+    28824, -- Gauntlets of Martial Perfection
+    28827, -- Gauntlets of the Dragonslayer
+    28828, -- Gronn-Stitched Girdle
+    28810, -- Windshear Boots
+    28830, -- Dragonspine Trophy
+    28823, -- Eye of Gruul
 }
