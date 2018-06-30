d247de67ad8b783d74e843eb19107478b8be16d9
davidcraig <davidcraig1986@gmail.com>
2018-06-30 10:47:10 +0100

    Magtheridon's Lair.

--- AdiBags_ByExpansion_TBC/raids/ml.lua
+++ AdiBags_ByExpansion_TBC/raids/ml.lua
@@ -1,4 +1,21 @@
 local AddonName, AddonTable = ...
 
+-- [Raid] Magtheridon's Lair: 25 Normal
 AddonTable.ml = {
+    29754, -- Chestguard of the Fallen Champion
+    29753, -- Chestguard of the Fallen Defender
+    29755, -- Chestguard of the Fallen Hero
+    34845, -- Pit Lord's Satchel
+    28782, -- Crystalheart Pulse-Staff
+    28774, -- Glaive of the Pit
+    28783, -- Eredar Wand of Obliteration
+    29458, -- Aegis of the Vindicator
+    28781, -- Karaborian Talisman
+    28775, -- Thundering Greathelm
+    28777, -- Cloak of the Pit Stalker
+    28776, -- Lair's Tongue Gloves
+    28780, -- Soul-Eater's Handwraps
+    28779, -- Girdle of the Endless Pit
+    28778, -- Terror Pit Girdle
+    28789, -- Eye of Magtheridon
 }
