5b2313459fd3d7aae2e97fa11aedf613d610f3d2
davidcraig <davidcraig1986@gmail.com>
2018-06-30 10:14:44 +0100

    More boss loot.

--- AdiBags_ByExpansion_TBC/raids/kara.lua
+++ AdiBags_ByExpansion_TBC/raids/kara.lua
@@ -29,4 +29,64 @@ AddonTable.kara = {
     28508, -- Gloves of Saintly Blessings
     28507, -- Handwraps of Flowing Thought
     28510, -- Spectral Band of Innervation
+
+    -- Moroes
+    22559, -- Formula: Enchant Weapon - Mongoose
+    138797, -- Illusion: Mongoose
+    28524, -- Emerald Ripper
+    28525, -- Signet of Unshakable Faith
+    28530, -- Brooch of Unquenchable Fury
+    28529, -- Royal Cloak of Arathi Kings
+    28570, -- Shadow-Cloak of Dalaran
+    28567, -- Belt of Gale Force
+    28566, -- Crimson Girdle of the Indomitable
+    28565, -- Nethershard Girdle
+    28569, -- Boots of Valiance
+    28545, -- Edgewalker Longboots
+    28528, -- Moroes' Lucky Pocket Watch
+
+    -- Maiden of Virtue
+    28522, -- Shard of the Virtuous
+    28516, -- Barbed Choker of Discipline
+    28511, -- Bands of Indwelling
+    28515, -- Bands of Nefarious Deeds
+    28512, -- Bracers of Justice
+    28514, -- Bracers of Maliciousness
+    28520, -- Gloves of Centering
+    28519, -- Gloves of Quickening
+    28518, -- Iron Gauntlets of the Maiden
+    28521, -- Mitts of the Treemender
+    28517, -- Boots of Foretelling
+
+    -- Opera Hall
+    97548, -- Spiky Collar (Lil' Bad Wolf) [Battle Pet]
+    28573, -- Despair
+    28587, -- Legacy
+    28584, -- Big Bag Wolf's Paw
+    28572, -- Blade of the Unrequited
+    28588, -- Blue Diamond Witchwand
+    28581, -- Wolfslayer Sniper Rifle
+    28583, -- Big Bad Wolf's Head
+    28593, -- Eternium Greathelm
+    28586, -- Wicked Witch's Hat
+    28589, -- Beastmaw Pauldrons
+    28582, -- Red Riding Hood's Cloak
+    28578, -- Masquerade Gown
+    28591, -- Earthsoul Leggings
+    28594, -- Trial-Fire Trousers
+    28585, -- Ruby Slippers
+    28590, -- Ribbon of Sacrifice
+    28579, -- Romulo's Poison Vial
+
+    -- The Curator
+    29757, -- Gloves of the Fallen Champion
+    29758, -- Gloves of the Fallen Defender
+    29756, -- Gloves of the Fallen Hero
+    97549, -- Instant Arcane Sanctum Security Kit (Menagerie Custodian) [Battle Pet]
+    28633, -- Staff of Infinite Mysteries
+    28631, -- Dragon-Quake Shoulderguards
+    28647, -- Forest Wind Shoulderpads
+    28612, -- Pauldrons of the Solace-Giver
+    28621, -- Wrynn Dynasty Greaves
+    28649, -- Garona's Signet Ring
 }
