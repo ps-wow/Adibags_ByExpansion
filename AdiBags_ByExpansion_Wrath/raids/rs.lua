50932ae1d1cce0722b8a8fcd0c850a6b1271d5ee
davidcraig <david@davidcraig.eu>
2018-08-04 14:13:47 +0100

    Ruby Sanctum

--- AdiBags_ByExpansion_Wrath/raids/rs.lua
+++ AdiBags_ByExpansion_Wrath/raids/rs.lua
@@ -16,6 +16,22 @@ AddonTable.rs = {
     53116, -- Saviana's Tribute
     53110, -- Zarithian's Offering
     -- Normal (25)
+    53132, -- Penumbra Pendant
+    53489, -- Cloak of Burning Dusk
+    53486, -- Bracers of Fiery Night
+    53134, -- Phaseshifter's Bracers
+    53126, -- Umbrage Armbands
+    53488, -- Split Shape Belt
+    53125, -- Apocalypse's Advance
+    53487, -- Foreshadow Steps
+    53127, -- Returning Footfalls
+    53129, -- Treads of Impending Resurrection
+    53490, -- Ring of Phased Regeneration
+    53133, -- Signet of Twilight
+    54572, -- Charred Twilight Scale
+    54573, -- Glowing Twilight Scale
+    54571, -- Petrified Twilight Scale
+    54569, -- Sharpened Twilight Scale
     -- Heroic (10)
     54557, -- Baltharus' Gift
     54566, -- Twilight Scale Shoulders
@@ -30,4 +46,20 @@ AddonTable.rs = {
     54567, -- Saviana's Tribute
     54563, -- Zarithian's Offering
     -- Heroic (25)
+    54581, -- Penumbra Pendant
+    54583, -- Cloak of Burning Dusk
+    54582, -- Bracers of Fiery Night
+    54584, -- Phaseshifter's Bracers
+    54580, -- Umbrage Armbands
+    54587, -- Split Shape Belt
+    54578, -- Apocalypse's Advance
+    54586, -- Foreshadow Steps
+    54577, -- Returning Footfalls
+    54579, -- Treads of Impending Resurrection
+    54576, -- Ring of Phased Regeneration
+    54585, -- Signet of Twilight
+    54588, -- Charred Twilight Scale
+    54589, -- Glowing Twilight Scale
+    54591, -- Petrified Twilight Scale
+    54590, -- Sharpened Twilight Scale
 }
