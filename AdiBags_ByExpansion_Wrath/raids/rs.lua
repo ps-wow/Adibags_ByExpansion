886502fee97ee2eeab57a2b4bbef7ca21806feb4
davidcraig <david@davidcraig.eu>
2018-08-04 14:06:48 +0100

    RS 10 Normal and Heroic.

--- AdiBags_ByExpansion_Wrath/raids/rs.lua
+++ AdiBags_ByExpansion_Wrath/raids/rs.lua
@@ -1,5 +1,33 @@
 local AddonName, AddonTable = ...
 
+-- The Ruby Sanctum
 AddonTable.rs = {
-
+    -- Normal (10)
+    53103, -- Baltharus' Gift
+    53113, -- Twilight Scale Shoulders
+    53115, -- Abduction's Cover
+    53114, -- Gloaming Sark
+    53112, -- Bracers of the Heir
+    53117, -- Changeling Gloves
+    53118, -- Misbegotten Belt
+    53121, -- Surrogate Belt
+    53119, -- Boots of Divided Being
+    53111, -- Scion's Treads
+    53116, -- Saviana's Tribute
+    53110, -- Zarithian's Offering
+    -- Normal (25)
+    -- Heroic (10)
+    54557, -- Baltharus' Gift
+    54566, -- Twilight Scale Shoulders
+    54556, -- Abduction's Cover
+    54561, -- Gloaming Sark
+    54559, -- Bracers of the Heir
+    54560, -- Changeling Gloves
+    54562, -- Misbegotten Belt
+    54565, -- Surrogate Belt
+    54558, -- Boots of Divided Being
+    54564, -- Scion's Treads
+    54567, -- Saviana's Tribute
+    54563, -- Zarithian's Offering
+    -- Heroic (25)
 }
