6e3ba76dd1c3fc26696ce1f53361c2a5f52bbe4d
davidcraig <david@davidcraig.eu>
2018-08-04 14:33:20 +0100

    Taloc and MOTHER

--- AdiBags_ByExpansion_BfA/raids/uldir.lua
+++ AdiBags_ByExpansion_BfA/raids/uldir.lua
@@ -1,43 +1,36 @@
 local AddonName, AddonTable = ...
 
 AddonTable.uldir = {
-    -- Raid Finder
+    --[[
+    Note: all item id's are the same regardless of difficulty.
+    --]]
 
     -- Taloc
+    160679, -- Khor, Hammer of the Corrupted
+    160680, -- Titanspark Animator
+    160637, -- Crimson Colossus Armguards
+    160629, -- Rubywrought Sparkguards
+    160618, -- Gloves of Descending Madness
+    160622, -- Bloodstorm Buckle
+    160639, -- Greaves of Unending Vigil
+    160631, -- Legguards of Coalescing Plasma
+    160714, -- Volatile Walkers
+    160652, -- Construct Overcharger
+    160651, -- Vigilant's Bloodshaper
+
     -- MOTHER
-    -- Fetid Devourer
-    -- Zek-voz, Herald of N'zoth
-    -- Vectis
-    -- Zul, Reborn
-    -- Mythrax the Unraveler
-    -- G'huun
+    160681, -- Glaive of the Keepers
+    160683, -- Latticework Scalpel
+    160682, -- Mother's Twin Gaze
+    160695, -- Uldir Subject Manifest
+    160634, -- Gridrunner Galea [Azerite Armor / Plate]
+    160632, -- Flame-Sterilized Spaulders [Azerite Armor / Mail]
+    160626, -- Gloves of Involuntary Amputation
+    160638, -- Decontaminator's Greatbelt
+    160615, -- Leggings of Lingering Infestation
+    160625, -- Pathogenic Legwraps
+    160645, -- Rot-Scour Ring
 
-    -- Normal
-
-    -- Taloc
-    -- MOTHER
-    -- Fetid Devourer
-    -- Zek-voz, Herald of N'zoth
-    -- Vectis
-    -- Zul, Reborn
-    -- Mythrax the Unraveler
-    -- G'huun
-
-    -- Heroic
-
-    -- Taloc
-    -- MOTHER
-    -- Fetid Devourer
-    -- Zek-voz, Herald of N'zoth
-    -- Vectis
-    -- Zul, Reborn
-    -- Mythrax the Unraveler
-    -- G'huun
-
-    -- Mythic
-
-    -- Taloc
-    -- MOTHER
     -- Fetid Devourer
     -- Zek-voz, Herald of N'zoth
     -- Vectis
