422411f3e0c36836dfd47221e4f3d4faf977cc5f
davidcraig <david@davidcraig.eu>
2018-08-04 13:55:20 +0100

    Eye of Eternity (10 Normal)

--- AdiBags_ByExpansion_Wrath/raids/eye.lua
+++ AdiBags_ByExpansion_Wrath/raids/eye.lua
@@ -1,5 +1,17 @@
 local AddonName, AddonTable = ...
 
 AddonTable.eye = {
-
+    -- Malygos
+    43952, -- Reins of the Azure Drake [Mount]
+    43953, -- Reins of the Blue Drake [Mount]
+    40497, -- Black Ice
+    40489, -- Greatstaff of the Nexus
+    40491, -- Hailstorm
+    40488, -- Ice Spire Scepter
+    40475, -- Barricade of Eternity
+    40486, -- Necklace of the Glittering Chamber
+    40511, -- Focusing Energy Epaulets
+    40526, -- Gown of the Spell-Weaver
+    40519, -- Footsteps of Malygos
+    40474, -- Surge Needle Ring
 }
