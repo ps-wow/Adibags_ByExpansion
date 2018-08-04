6538888182d6e34e992a76534c604d3a4904c898
davidcraig <david@davidcraig.eu>
2018-08-04 14:00:45 +0100

    Eye of Eternity (25 Normal)

--- AdiBags_ByExpansion_Wrath/raids/eye.lua
+++ AdiBags_ByExpansion_Wrath/raids/eye.lua
@@ -1,9 +1,11 @@
 local AddonName, AddonTable = ...
 
 AddonTable.eye = {
-    -- Malygos
+    -- Malygos (10/25 Normal)
     43952, -- Reins of the Azure Drake [Mount]
     43953, -- Reins of the Blue Drake [Mount]
+
+    -- Malygos (10 Normal)
     40497, -- Black Ice
     40489, -- Greatstaff of the Nexus
     40491, -- Hailstorm
@@ -14,4 +16,26 @@ AddonTable.eye = {
     40526, -- Gown of the Spell-Weaver
     40519, -- Footsteps of Malygos
     40474, -- Surge Needle Ring
+
+    -- Malygos (25 Normal)
+    40543, -- Blue Aspect Helm
+    40562, -- Hood of Rationality
+    40555, -- Mantle of Dissemination
+    40590, -- Elevated Lair Pauldrons
+    40594, -- Spaulders of Catatonia
+    40588, -- Tunic of the Artifact Guardian
+    40539, -- Chestguard of the Recluse
+    40194, -- Blanketing Robes of Snow
+    40564, -- Winter Spectable Gloves
+    40541, -- Frosted Adroit Handguards
+    40561, -- Leash of Heedless Magic
+    40566, -- Unravelling Strands of Sanity
+    40560, -- Leggings of the Wanton Spellcaster
+    40589, -- Legplates of Sovereignty
+    40558, -- Arcanic Tramplers
+    40591, -- Melancholy Sabatons
+    40549, -- Boots of the Renewed Flight
+    40592, -- Boots of Healing Energies
+    40531, -- Mark of Norgannon
+    40532, -- Living Ice Crystals
 }
