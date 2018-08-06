5692de3d9366571a776bc41d776f42a66aac76cb
davidcraig <david@davidcraig.eu>
2018-08-06 20:27:50 +0100

    Add loot from the eye.

--- AdiBags_ByExpansion_TBC/raids/eye.lua
+++ AdiBags_ByExpansion_TBC/raids/eye.lua
@@ -1,4 +1,67 @@
 local AddonName, AddonTable = ...
 
+-- The Eye (25 Normal)
 AddonTable.eye = {
+    -- Al'ar
+    97557, -- Brilliant Phoenix Hawk Feather (Phoenix Hawk) [Battle Pet]
+    29948, -- Claw of the Phoenix
+    29924, -- Netherbane
+    32944, -- Talon of the Phoenix
+    29949, -- Arcanite Steam-Pistol
+    29923, -- Talisman of the Sun King
+    29925, -- Phoenix-Wing Cloak
+    29921, -- Fire Crest Breastplate
+    29918, -- Mindstorm Wristbands
+    29947, -- Gloves of the Searing Grip
+    29922, -- Band of Al'ar
+    29920, -- Phoenix-Ring of Rebirth
+    30448, -- Talon of Al'ar
+    30447, -- Tome of Fiery Redemption
+
+    -- Void Reaver
+    30248, -- Pauldrons of the Vanquished Champion
+    30249, -- Pauldrons of the Vanquished Defender
+    30250, -- Pauldrons of the Vanquished Hero
+    97555, -- Tiny Fel Engine Key (Pocket Reaver) [Battle Pet]
+    29986, -- Cowl of the Grand Engineer
+    29983, -- Fel-Steel Warhelm
+    32515, -- Wristguards of Determination
+    29984, -- Girdle of Zaetar
+    29985, -- Void Reaver Greaves
+    30619, -- Fel Reaver's Piston
+    30450, -- Warp-Spring Coil
+
+    -- High Astromancer Solarian
+    97556, -- Crystal of the Void (Lesser Voidcaller) [Battle Pet]
+    29981, -- Ethereum Life-Staff
+    29962, -- Heartrazor
+    29982, -- Wand of the Forgotten Star
+    29966, -- Vambraces of Ending
+    29976, -- Worldstorm Gauntlets
+    29965, -- Girdle of the Righteous Path
+    29950, -- Greaves of the Bloodwarder
+    29977, -- Star-Soul Breeches
+    29972, -- Trousers of the Astromancer
+    32267, -- Boots of the Resilient
+    29951, -- Star-Strider Boots
+    30446, -- Solarian's Sapphire
+    30449, -- Void Star Talisman
+
+    -- Kael'thas Sunstrider
+    32458, -- Ashes of Al'ar [Mount]
+    30236, -- Chestguard of the Vanquished Champion
+    30237, -- Chestguard of the Vanquished Defender
+    30238, -- Chestguard of the Vanquished Hero
+    29988, -- The Nexus Key
+    29993, -- Twinblade of the Phoenix
+    29996, -- Rod of the Sun King
+    29990, -- Crown of the Sun
+    29992, -- Royal Cloak of the Sunstriders
+    29989, -- Sunshower Light Cloak
+    29994, -- Thalassian Wildercloak
+    29987, -- Gauntlets of the Sun King
+    29998, -- Royal Gauntlets of Silvermoon
+    29995, -- Leggings of Murderous Intent
+    29991, -- Sunhawk Leggings
+    29997, -- Band of the Ranger-General
 }
