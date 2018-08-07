8eea1b6d58a680f328a2de7302643fe1bffef54a
davidcraig <david@davidcraig.eu>
2018-08-07 21:08:44 +0100

    Hyjal

--- AdiBags_ByExpansion_TBC/raids/hyjal.lua
+++ AdiBags_ByExpansion_TBC/raids/hyjal.lua
@@ -1,4 +1,77 @@
 local AddonName, AddonTable = ...
 
+-- The Battle for Mount Hyjal (25 Normal)
 AddonTable.hyjal = {
+    -- Rage Winterchill
+    30865, -- Tracker's Blade
+    30872, -- Chronicle of Dark Secrets
+    30866, -- Blood-Stained Pauldrons
+    30862, -- Blessed Adamantite Bracers
+    30871, -- Bracers of Martyrdom
+    30864, -- Bracers of the Pathfinder
+    30870, -- Cuffs of Devastation
+    30863, -- Deadly Cuffs
+    30861, -- Furious Shackles
+    30869, -- Howling Wind Bracers
+    30868, -- Rejuvenating Bracers
+    30873, -- Stillwater Boots
+
+    -- Anetheron
+    122111, -- Smelly Gravestone (Stinkrot) [Battle Pet]
+    30883, -- Pillar of Ferocity
+    30881, -- Blade of Infamy
+    30874, -- The Unbreakable Will
+    30882, -- Bastion of Light
+    30878, -- Glimmering Steel Mantle
+    30884, -- Hatefury Mantle
+    30887, -- Golden Links of Restoration
+    30888, -- Anetheron's Noose
+    30879, -- Don Alejandro's Money Belt
+    30885, -- Archbishop's Slippers
+    30886, -- Enchanted Leather Sandles
+    30880, -- Quickstrider Moccasins
+
+    -- Kaz'rogal
+    30918, -- Hammer of Atonement
+    30889, -- Kaz'rogal's Hardened Heart
+    30892, -- Beast-Tamer's Shoulders
+    30917, -- Razorfury Mantle
+    30895, -- Angelista's Sash
+    30915, -- Belt of Seething Fury
+    30914, -- Belt of the Crescent Moon
+    30919, -- Valestalker Girdle
+    30916, -- Leggings of Channeled Elements
+    30893, -- Sun-Touched Chain Leggings
+    30891, -- Black Featherlight Boots
+    30894, -- Blue Suede Shoes
+
+    -- Azgalor
+    31092, -- Gloves of the Forgotten Conqueror
+    31094, -- Gloves of the Forgotten Protector
+    31093, -- Gloves of the Forgotten Vanquisher
+    122105, -- Grotesque Statue (Grotesque) [Battle Pet]
+    30901, -- Boundless Agony
+    30899, -- Don Rodrigo's Poncho
+    30896, -- Glory of the Defender
+    30897, -- Girdle of Hope
+    30900, -- Bow-Stitched Leggings
+    30898, -- Shady Dealer's Pantaloons
+
+    -- Archimonde
+    31097, -- Helm of the Forgotten Conqueror
+    31095, -- Helm of the Forgotten Protector
+    31096, -- Helm of the Forgotten Vanquisher
+    122112, -- Hyjal Wisp [Battle Pet]
+    30908, -- Apostle of Argus
+    30902, -- Cataclysm's Edge
+    30910, -- Tempest of Chaos
+    30906, -- Bristleblitz Striker
+    30909, -- Antonidas' Aegis of Rapt Concentration
+    30911, -- Scepter of Purification
+    30907, -- Mail of Fevered Pursuit
+    30905, -- Midnight Chestguard
+    30913, -- Robes of Rhonin
+    30904, -- Savior's Grasp
+    30912, -- Leggings of Eternity
+    30903, -- Legguards of Endless Rage
 }
