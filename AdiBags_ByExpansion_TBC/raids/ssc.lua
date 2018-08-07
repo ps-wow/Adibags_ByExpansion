3bc40d7228387e8bc8e828ccd21201ec62ef8848
davidcraig <david@davidcraig.eu>
2018-08-07 20:51:15 +0100

    SSC.

--- AdiBags_ByExpansion_TBC/raids/ssc.lua
+++ AdiBags_ByExpansion_TBC/raids/ssc.lua
@@ -20,8 +20,72 @@ AddonTable.ssc = {
     30629, -- Scarab of Displacement
 
     -- The Lurker Below
+    30058, -- Mallet of the Tides
+    30059, -- Choker of Animalistic Fury
+    30065, -- Glowing Breastplate of Truth
+    30057, -- Bracers of Eradication
+    30062, -- Grove-Bands of Remulos
+    30064, -- Cord of Screaming Terrors
+    30060, -- Boots of Effortless Striking
+    30066, -- Tempest-Strider Boots
+    30067, -- Velvet Boots of the Guardian
+    30061, -- Ancestral Ring of Conquest
+    33054, -- The Seal of Danzalar
+    30665, -- Earring of Soulful Meditation
+
     -- Leotheras the Blind
+    30239, -- Gloves of the Vanquished Champion
+    30240, -- Gloves of the Vanquished Defender
+    30241, -- Gloves of the Vanquished Hero
+    30095, -- Fang of the Leviathan
+    30097, -- Coral-Barbed Shoulderpads
+    30091, -- True-Aim Stalker Bands
+    30096, -- Girdle of the Invulnerable
+    30092, -- Orca-Hide Boots
+    30627, -- Tsunami Talisman
+
     -- Fathom-Lord Karathress
+    30245, -- Leggings of the Vanquished Champion
+    30246, -- Leggings of the Vanquished Defender
+    30247, -- Leggings of the Vanquished Hero
+    30090, -- World Breaker
+    30099, -- Frayed Tether of the Drowned
+    30101, -- Bloodsea Brigand's Vest
+    30100, -- Soul-Strider Boots
+    30663, -- Fathom-Brooch of the Tidewalker
+    30626, -- Sextant of Unstable Currents
+
     -- Morogrim Tidewalker
+    97552, -- Shell of Tide-Calling (Tideskipper) [Battle Pet]
+    30082, -- Talon of Azshara
+    30080, -- Luminescent Rod of the Naaru
+    30008, -- Pendant of the Lost Ages
+    30079, -- Illidari Shoulderpads
+    30085, -- Mantle of the Tireless Tracker
+    30084, -- Pauldrons of the Argent Sentinel
+    30098, -- Razor-Scale Battlecloak
+    30075, -- Gnarled Chestpiece of the Ancients
+    30068, -- Girdle of the Tidal Call
+    30081, -- Warboots of Obliteration
+    33058, -- Band of the Vigilant
+    30083, -- Ring of Sundered Souls
+    30720, -- Serpent-Coil Braid
+
     -- Lady Vashj
+    97554, -- Dripping Strider Egg (Coilfang Stalker) [Battle Pet]
+    30242, -- Helm of the Vanquished Champion
+    30243, -- Helm of the Vanquished Defender
+    30244, -- Helm of the Vanquished Hero
+    30103, -- Fang of Vashj
+    30108, -- Lightfathom Scepter
+    30105, -- Serpent Spine Longbow
+    30111, -- Runetotem's Mantle
+    30102, -- Krakken-Heart Breastplate
+    30107, -- Vestments of the Sea-Witch
+    30112, -- Glorious Gauntlets of Crestfall
+    30106, -- Belt of One-Hundered Deaths
+    30104, -- Cobra-Lash Boots
+    30110, -- Coral Band of the Revived
+    30109, -- Ring of Endless Coils
+    30621, -- Prism of Inner Calm
 }
