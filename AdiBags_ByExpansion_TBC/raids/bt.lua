7979ddaa113c9f3f13974bdbf518ef0a277173e9
davidcraig <david@davidcraig.eu>
2018-08-07 21:15:30 +0100

    Naj'entus

--- AdiBags_ByExpansion_TBC/raids/bt.lua
+++ AdiBags_ByExpansion_TBC/raids/bt.lua
@@ -1,4 +1,43 @@
 local AddonName, AddonTable = ...
 
 AddonTable.bt = {
+    -- 25 (Normal)
+
+    -- High Warlord Naj'entus
+    122104, -- Leviathan Egg (Leviathan Hatchling) [Battle Pet]
+    32248, -- Halberd of Desolation
+    32236, -- Rising Tide
+    32237, -- The Maelstrom's Fury
+    32240, -- Guise of the Tidal Lurker
+    32241, -- Helm of Soothing Currents
+    32377, -- Mantle of Darkness
+    32232, -- Eternium Shell Bracers
+    32234, -- Fists of Mukoa
+    32242, -- Boots of Oceanic Fury
+    32243, -- Pearl Inlaid Boots
+    32239, -- Slippers of the Seacaller
+    32245, -- Tide-Stomper's Greaves
+    32238, -- Ring of Calming Waves
+    32247, -- Ring of Captured Storms
+
+    -- Supremus
+    -- Shade of Akama
+    -- Teron Gorefiend
+    -- Gurtogg Bloodboil
+    -- Reliquary of Souls
+    -- Mother Shahraz
+    -- The Illidari Council
+    -- Illidan Stormrage
+
+    -- 25 (Timewalking)
+
+    -- High Warlord Naj'entus
+    -- Supremus
+    -- Shade of Akama
+    -- Teron Gorefiend
+    -- Gurtogg Bloodboil
+    -- Reliquary of Souls
+    -- Mother Shahraz
+    -- The Illidari Council
+    -- Illidan Stormrage
 }
