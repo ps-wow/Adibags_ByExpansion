20e75f830c3d05f454d7f1ee7bf5bd458f312f82
davidcraig <david@davidcraig.eu>
2018-08-07 20:13:54 +0100

    Hydross

--- AdiBags_ByExpansion_TBC/raids/ssc.lua
+++ AdiBags_ByExpansion_TBC/raids/ssc.lua
@@ -1,4 +1,27 @@
 local AddonName, AddonTable = ...
 
+-- Serpentshrine Cavern (25 Normal)
 AddonTable.ssc = {
+    -- Hydross the Unstable
+    138834, -- Illusion: Frostbrand
+    97553, -- Tainted Core (Tainted Waveling) [Battle Pet]
+    30049, -- Fathomstone
+    30048, -- Brighthelm of Justice
+    30053, -- Pauldrons of the Wardancer
+    30055, -- Shoulderpads of the Stranger
+    30054, -- Ranger-General's Chestguard
+    30056, -- Robe of Hateful Echoes
+    30047, -- Blackfathom Warbands
+    32516, -- Wraps of Purification
+    30050, -- Boots of the Shifting Nightmare
+    33055, -- Band of Vile Aggression
+    30052, -- Ring of Lethality
+    30664, -- Living Root of the Wildheart
+    30629, -- Scarab of Displacement
+
+    -- The Lurker Below
+    -- Leotheras the Blind
+    -- Fathom-Lord Karathress
+    -- Morogrim Tidewalker
+    -- Lady Vashj
 }
