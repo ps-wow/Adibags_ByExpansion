2713fa983117d8a759cc47a887985fbb9c2816e4
davidcraig <davidcraig1986@gmail.com>
2018-08-20 13:41:11 +0100

    BfA loot and junk.

--- AdiBags_ByExpansion_BfA/categories/junk.lua
+++ AdiBags_ByExpansion_BfA/categories/junk.lua
@@ -1,20 +1,33 @@
 local AddonName, AddonTable = ...
 
 AddonTable.junk = {
-    158747, -- Thorny Prickle
-    155742, -- "Preserved" Foodstuffs
-    158836, -- Barbed Stinger
-    155610, -- Stinging Nettle
-    158831, -- Parched Honeycomb
-    155635, -- Sinewy Wing
-    158863, -- Chitinous Exoskeleton
+    155598, -- Severed Mandible
+
     155601, -- Pointy Leg Segment
+    155608, -- Defiled Bone
+    155610, -- Stinging Nettle
+    155635, -- Sinewy Wing
+    155629, -- Shaggy Mane
+    155615, -- Perstilent Muck
+
+    155742, -- "Preserved" Foodstuffs
+
+    155843, -- Brittle Ceremonial Necklace
+
+    157844, -- Iridescent Speck
+
+    158210, -- Fearsome Claw
+    158747, -- Thorny Prickle
+    158751, -- Matted Pelt
     158775, -- Thick Hivewax
     158806, -- Hooked Talon
-    155598, -- Severed Mandible
-    157844, -- Iridescent Speck
-    155843, -- Brittle Ceremonial Necklace
-    155629, -- Shaggy Mane
     158817, -- Razorpetal
-    155615, -- Perstilent Muck
+    158831, -- Parched Honeycomb
+    158836, -- Barbed Stinger
+    158855, -- Grim Skull Fetish
+    158859, -- Jagged Fang
+    158863, -- Chitinous Exoskeleton
+    158865, -- Ghastly Ooze
+    158866, -- Oily Blubber
+    158868, -- Golden Ornament
 }
---
+++ AdiBags_ByExpansion_BfA/categories/loot.lua
@@ -0,0 +1,16 @@
+local AddonName, AddonTable = ...
+
+-- Soulbound Items (Green or higher)
+
+AddonTable.loot = {
+    -- Uncommon (Green)
+    158947, -- Ritual Flayer's Handguards
+    158994, -- Ritual Flayer's Legwraps
+    159543, -- Loa-Blessed Maul
+
+    -- Rare (Blue)
+    155314, -- Ritual Flayer's Vest
+    158958, -- Ritual Flayer's Trousers
+    159048, -- Ritual Flayer's Treads
+    161028, -- Noxious Crocolisk Trousers
+}
