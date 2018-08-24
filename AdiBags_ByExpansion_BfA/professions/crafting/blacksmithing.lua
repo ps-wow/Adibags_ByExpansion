1d35fde8d8d5eb73e39cef8311fff7db50dc919d
davidcraig <davidcraig1986@gmail.com>
2018-08-24 10:19:48 +0100

    Add some BS items.

--- AdiBags_ByExpansion_BfA/professions/crafting/blacksmithing.lua
+++ AdiBags_ByExpansion_BfA/professions/crafting/blacksmithing.lua
@@ -2,11 +2,27 @@ local AddonName, AddonTable = ...
 
 AddonTable.blacksmithing = {
     -- Armour
+    152803, -- Monel-Hardened Boots
     152804, -- Monel-Hardened Gauntlets
     152806, -- Monel-Hardened Greaves
     152808, -- Monel-Hardened Waistguard
     152809, -- Monel-Hardened Armguards
+    152818, -- Monel-Hardened Shield
+    152805, -- Monel-Hardened Helm
+    152807, -- Monel-Hardened Pauldrons
+    152802, -- Monel-Hardened Breastplate
     -- Weapons
+    152828, -- Monel-Hardened Claymore
     152827, -- Monel-Hardened Cutlass
+    152831, -- Monel-Hardened Deckpounder
     152832, -- Monel-Hardened Shanker
+    152833, -- Monel-Hardened Polearm
+    -- Follower Equipment
+    162109, -- Storm Silver Spurs
+    162115, -- Magnetic Mining Pick
+    162120, -- Platinum Whetstone
+    -- Other
+    152812, -- Monel-Hardened Hoofplates
+    152813, -- Monel-Hardened Stirrups
+    159826, -- Monelite Skeleton Key
 }
