5d4b4955e796986511b02f4bea67162d203441a5
davidcraig <davidcraig1986@gmail.com>
2018-09-01 11:56:37 +0100

    Atal'dazar

---
+++ AdiBags_ByExpansion_BfA/dungeons/ataldazar.lua
@@ -0,0 +1,42 @@
+local AddonName, AddonTable = ...
+
+AddonTable.ataldazar = {
+    -- Priestess Alun'za
+    158321, -- Wand of Zealous Purification
+    158322, -- Aureus Vessel
+    158309, -- Wristlinks of Alchemical Transfusion
+    158306, -- Belt of Gleaming Determination
+    158347, -- Cincture of Glittering Gold
+    158313, -- Legplates of Beaten Gold
+    155861, -- Embellished Ritual Sabatons
+    158319, -- My'das Talisman
+    -- Vol'kaal
+    159632, -- Adulation Enforcer
+    158375, -- Drape of the Loyal Vassal
+    158348, -- Wraps of Everliving Fealty
+    158317, -- Gauntlets of Eternal Service
+    159445, -- Grips of the Everlasting Guardian
+    155869, -- Shambling Beserker's Leggings
+    158320, -- Revitalizing Voodoo Totem
+    -- Rezan
+    159841, -- Tome of Hex: Skeletal Hatchling [Shaman]
+    160269, -- Soulrending Claw
+    158711, -- Hallowed Ossein Longbow
+    158713, -- Disc of Indomitable Will
+    160214, -- Venerated Raptorhide Bindings
+    155868, -- Kilt of Fanatical Consumption
+    158303, -- Devilsaur Worshiper's Sandals
+    159458, -- Seal of the Regal Loa
+    158712, -- Rezan's Gleaming Eye
+    -- Yazma
+    158323, -- Soulrender's Fang
+    159358, -- Coif of the Court Spider
+    158315, -- Secret Spinner's Miter
+    155866, -- Soulspun Casque
+    158304, -- Mantle of Fastidious Machinations
+    159233, -- Loa Betrayer's Vestments
+    155860, -- Spymaster's Wrap
+    160212, -- Shadowshroud Vambraces
+    158308, -- Souldrifting Sabatons
+    159610, -- Vessel of Skittering Shadows
+}
