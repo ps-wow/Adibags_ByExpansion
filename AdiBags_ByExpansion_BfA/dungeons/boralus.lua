4a4783d449c5de4a2333529390749af135a727a6
davidcraig <davidcraig1986@gmail.com>
2018-09-03 23:02:14 +0100

    2/4

--- AdiBags_ByExpansion_BfA/dungeons/boralus.lua
+++ AdiBags_ByExpansion_BfA/dungeons/boralus.lua
@@ -1,8 +1,26 @@
 local AddonName, AddonTable = ...
 
 AddonTable.boralus = {
-    -- Boss 1
-    -- Boss 2
-    -- Boss 3
-    -- Boss 4
+    -- Sergeant Bainbridge
+    159647, -- Siegebreaker's Halberd
+    159648, -- Bainbridge's Blackjack
+    159328, -- Wharf Warden's Gloves
+    159245, -- Cord of the Pious Warder
+    159411, -- Legplates of the Maritime Guard
+    159367, -- Unstoppable Zealot's Legplates
+    159278, -- Slippers of Unwavering Faith
+    162542, -- Seal of the City Watch
+    -- Dread Captain Lockwood
+    159649, -- Saber of Dread Pirate Lockwood
+    159372, -- Dread Captain's Irons
+    159237, -- Captain's Dustfinders
+    159429, -- Rope-Scored Gauntlets
+    159434, -- Cannoneer's Toolbelt
+    159309, -- Port Pillager's Belt
+    159250, -- Powder Monkey's Leggings
+    159320, -- Besieger's Deckstalkers
+    159379, -- Sure-Foot Sabatons
+    159623, -- Dead-Eye Spyglass
+    -- Hadal Darkfathom
+    -- Viq'Goth
 }
