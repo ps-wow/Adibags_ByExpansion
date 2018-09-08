c575da247a395ae84f0c502123e3d7fbc5100e3f
davidcraig <davidcraig1986@gmail.com>
2018-09-08 09:37:26 +0100

    Add Tol'Dagor loot.

--- AdiBags_ByExpansion_BfA/dungeons/toldagor.lua
+++ AdiBags_ByExpansion_BfA/dungeons/toldagor.lua
@@ -1,11 +1,42 @@
 local AddonName, AddonTable = ...
 
 AddonTable.toldagor = {
-
+    -- The Sand Queen
+    159656, -- Halberd of the Unwary Guard
+    160110, -- Warglaive of the Sand Queen
+    159668, -- Rattling Jar of Eyes
+    159332, -- Wristguards of the Sandswimmer
+    160215, -- Sewer Grate Girdle
+    159392, -- Gaoler's Chainmail Gaiters
+    159460, -- Overseer's Lost Seal
+    -- Jes Howlis
+    159131, -- Howlis' Crystal Shiv
+    159666, -- Improvised Riot Shield
+    159293, -- Turncoat's Cape
+    159266, -- Claw-Slit Brawler's Handwraps
+    159306, -- Singe-Blotched Britches
+    159363, -- Gnawed Iron Fetters
+    159627, -- Jes' Howler
+    -- Knight Captain Valyri
+    159389, -- Flint-Linked Wristguards
+    159441, -- Valyri's Fire-Proof Bracers
+    159390, -- Sure-Grip Munition Handlers
+    159333, -- Cincture of the Azerite Arsenal
     159274, -- Knight Captain's Waistcord
-
-    -- Boss 1
-    -- Boss 2
-    -- Boss 3
-    -- Boss 4
+    159277, -- Wild Pyromancer's Trousers
+    159444, -- Gunpowder-Scoured Sabatons
+    159343, -- Spark Dampening Footpads
+    159615, -- Ignition Mage's Fuse
+    -- Overseer Korgus
+    159129, -- Flamecaster Botefeux
+    159658, -- Cudgel of Correctional Oversight
+    159657, -- Korgus' Blackpowder Rifle
+    159334, -- Flashpowder Hood [Azerite]
+    159391, -- Hood of the Dark Reaper [Azerite]
+    159414, -- Overseer's Riot Helmet [Azerite]
+    159393, -- Cannoneer's Mantle [Azerite]
+    159331, -- Pistoleer's Spaulders [Azerite]
+    159440, -- Ashvane Warden's Cuirass [Azerite]
+    159268, -- Inmate's Straight Robe [Azerite]
+    159628, -- Kul Tiran Cannonball Runner
 }
