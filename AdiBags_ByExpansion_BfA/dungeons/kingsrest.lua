237f046de6d1c2b4a44c7ad61816b2ad0db259b0
davidcraig <davidcraig1986@gmail.com>
2018-09-21 19:03:18 +0100

    King's Rest

--- AdiBags_ByExpansion_BfA/dungeons/kingsrest.lua
+++ AdiBags_ByExpansion_BfA/dungeons/kingsrest.lua
@@ -1,8 +1,42 @@
 local AddonName, AddonTable = ...
 
 AddonTable.kingsrest = {
-    -- Boss 1
-    -- Boss 2
-    -- Boss 3
-    -- Boss 4
+    -- The Golden Serpent
+    159137, -- Gilden Serpent's Tooth
+    159413, -- Gauntlets of the Avian Sentinel
+    159369, -- Belt of the Consecrated Tomb
+    159313, -- Breeches of the Sacred Hall
+    159234, -- Down-Lined Breeches
+    159412, -- Auric Puddle Stompers
+    159304, -- Goldfeather Boots
+    159617, -- Lustrous Golden Plumage
+    -- Mchimba the Embalmer
+    159642, -- Royal Purifier's Spade
+    159667, -- Vessel of Last Rites
+    159409, -- Embalmer's Steadying Bracers
+    159312, -- Desiccator's Blessed Gloves
+    160213, -- Sephulchral Construct's Gloves
+    159459, -- Ritual Binder's Ring
+    159618, -- Mchimba's Ritual Bandages
+    -- The Council of Tribes
+    160216, -- Crackling Jade Kilij
+    159136, -- Jeweled Dagger of Subjugation
+    159643, -- Crossbow of Forgotten Majesty
+    159288, -- Cloak of the Restless Tribes
+    159300, -- Kula's Butchering Wristwraps
+    159418, -- Girdle of Petilent Purification
+    159371, -- Boots of the Headlong Conqueror
+    159243, -- Sandals of Wise Voodoo
+    -- Dazar, the First King
+    159921, -- Mummified Raptor Skull
+    159644, -- Geti'ikku, Cut of Death
+    159645, -- Headcracker of Supplication
+    159236, -- Headdress of the First Empire
+    159422, -- Helm of the Raptor King
+    158344, -- Mantle of Ceremonial Ascension
+    159423, -- Pauldrons of the Great Unifier
+    159368, -- Spaulders of Prime Emperor
+    158355, -- Loa-Blessed Chestguard
+    159303, -- Vest of Reverent Adoration
+    159301, -- Primal Dinomancer's Belt
 }
