9ff137f0912f98c583d8f6cb5617be30dd40a433
davidcraig <davidcraig1986@gmail.com>
2018-09-24 10:40:22 +0100

    Add motherlode loot.

--- AdiBags_ByExpansion_BfA/dungeons/motherlode.lua
+++ AdiBags_ByExpansion_BfA/dungeons/motherlode.lua
@@ -1,8 +1,41 @@
 local AddonName, AddonTable = ...
 
 AddonTable.motherlode = {
-    -- Boss 1
-    -- Boss 2
-    -- Boss 3
-    -- Boss 4
+    -- Coin-Operated Crowd Pummeler
+    159638, -- Electro-Arm Bludgeoner
+    159663, -- G0-4W4Y Crowd Repeller
+    158353, -- Servo-Arm Bindings
+    159357, -- Linked Pummeler Grips
+    155864, -- Power-Assisted Vicegrips
+    158350, -- Rowdy Reveler's Legwraps
+    159462, -- Footbomb Championship Ring
+    -- Azerokk
+    158357, -- Bindings of Enraged Earth
+    158359, -- Stonefury Vambraces
+    159231, -- Mine Rat's Handwarmers
+    159226, -- Excavator's Safety Belt
+    159361, -- Shalebiter Interlinked Chain
+    159725, -- Unscrupulous Geologist's Belt
+    159336, -- Mercenary Miner's Boots
+    159679, -- Sabatons of Rampaging Elements
+    159612, -- Azerokk's Resonating Heart
+    -- Rixxa Fluxflame
+    159639, -- P.A.C.I.F.I.S.T. Mk7
+    159287, -- Cloak of Questionable Intent
+    159240, -- Rixxa's Sweat-Wicking Cuffs
+    159305, -- Corrosive Handler's Gloves
+    158341, -- Chemical Blaster's Legguards
+    159451, -- Leadplate Legguards
+    159235, -- Deranged Alchemist's Slippers
+    -- Mogul Razdunk
+    161135, -- Schematic: Mecha-Mogul Mk2 [Engineering]
+    159641, -- G3T-00t
+    158364, -- High Altitude Turban
+    159360, -- Crashguard Spaulders
+    159232, -- Exquisitely Aerodynamic Shaulderpads
+    159415, -- Skyscorcher Pauldrons
+    158349, -- Petticoat of the Self-Stylized Azerite Baron
+    158307, -- Shrapnel-Dampening Chestguard
+    159298, -- Venture Co. Plenipotentiary Vest
+    159611, -- Razdunk's Big Red Button
 }
