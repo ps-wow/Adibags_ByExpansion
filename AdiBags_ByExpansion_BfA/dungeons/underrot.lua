7b5913ee91b30091f3cfede7ee9802d69c360846
davidcraig <davidcraig1986@gmail.com>
2018-09-24 10:50:33 +0100

    Underrot

--- AdiBags_ByExpansion_BfA/dungeons/underrot.lua
+++ AdiBags_ByExpansion_BfA/dungeons/underrot.lua
@@ -1,11 +1,44 @@
 local AddonName, AddonTable = ...
 
 AddonTable.underrot = {
-
+    -- Elder Leaxa
+    159652, -- Leaxa's Thought-Piercer
+    159324, -- Blood Elder's Bindings
+    159402, -- Waistguard of Sanguine Fervor
+    159443, -- Legplates of Profane Sacrifice
+    159463, -- Loop of Pulsing Veins
     159624, -- Rotcrusted Voodoo Doll
-
-    -- Boss 1
-    -- Boss 2
-    -- Boss 3
-    -- Boss 4
+    -- Cragmaw the Infested
+    159653, -- Bile-Stained Crawg Tusks
+    159134, -- Heart-Piercing Stalactite
+    159433, -- Phosphorescent Armplates
+    159275, -- Wristwraps of Twined Morels
+    159382, -- Blood Tick Crushers
+    159344, -- Underrot Grotto Tenders
+    159325, -- Bloodfeaster Belt
+    159269, -- Darklight Legwarmers
+    159436, -- Fluorescent Flora Stompers
+    159396, -- Waders of the Infested
+    -- Sporecaller Zancha
+    159654, -- Corruption Borne Headlopper
+    159665, -- Targe of the Ancient Warder
+    159292, -- Sporecaller's Shroud
+    159410, -- Zancha's Venerated Greatbelt
+    159384, -- Corpuscular Greaves
+    159338, -- Pustule Bearer's Pants
+    159270, -- Blood Warder's Moccasins
+    159626, -- Lingering Sporepods
+    -- Unbound Abomination
+    160829, -- Underrot Crawg Harness
+    159655, -- Vile Exsanguinator
+    159446, -- Greathelm of the Putrid Path
+    159381, -- Visage of Bloody Horrors
+    159385, -- Amalgamated Abomination Spaulders
+    159267, -- Pauldrons of Vile Corruption
+    159323, -- Shoulders of the Sanguine Monstrosity
+    159241, -- Blood-Drenched Robes
+    159432, -- Breastplate of Arterial Protection
+    159330, -- Gore-Splattered Vest
+    159253, -- Gloves of Staunched Wounds
+    159625, -- Vial of Animated Blood
 }
