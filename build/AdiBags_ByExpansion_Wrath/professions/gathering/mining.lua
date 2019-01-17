7f6aa9a1bd3128a53dd6da81d94b98e30c0c406b
davidcraig <davidcraig1986@gmail.com>
2019-01-17 17:18:25 +0000

    JC gems #fix issue 3

--- build/AdiBags_ByExpansion_Wrath/professions/gathering/mining.lua
+++ build/AdiBags_ByExpansion_Wrath/professions/gathering/mining.lua
@@ -1,3 +1,9 @@
 local AddonName, AddonTable = ...
 AddonTable.mining = {
+36919, 
+36922, 
+36931, 
+36928, 
+36934, 
+36925, 
 }
--- src/AdiBags_ByExpansion_Wrath/professions/gathering/mining.lua
+++ src/AdiBags_ByExpansion_Wrath/professions/gathering/mining.lua
@@ -3,4 +3,10 @@ local AddonName, AddonTable = ...
 AddonTable.mining = {
     -- Ore
     -- Gems
+    36919, -- Cardinal Ruby
+    36922, -- King's Amber
+    36931, -- Ametrine
+    36928, -- Dreadstone
+    36934, -- Eye of Zul
+    36925, -- Majestic Zircon
 }
