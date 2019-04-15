4b1f131ad41c8577540aeae5d858005beccf5a95
filipleiner <fpatafta@live.com>
2019-04-15 20:02:41 +0200

    commit

---
+++ src/AdiBags_ByExpansion_TBC/professions/crafting/tailoring.lua
@@ -0,0 +1,9 @@
+local AddonName, AddonTable = ...
+
+AddonTable.tailoring = {
+    21842, -- Bolt of Imbued Netherweave
+    21844, -- Bolt of Soulcloth
+    24271, -- Spellcloth
+    24272, -- Shadowcloth
+    21845, -- Primal Mooncloth
+}
