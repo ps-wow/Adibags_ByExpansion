1c043f3caa7dda3742cac0069eb8329dc18fee8c
filipleiner <fpatafta@live.com>
2019-04-19 10:09:48 +0200

    commas fix

--- src/AdiBags_ByExpansion_TBC/professions/gathering/mining.lua
+++ src/AdiBags_ByExpansion_TBC/professions/gathering/mining.lua
@@ -10,10 +10,10 @@ AddonTable.mining = {
     -- Bars 
     23445, -- Fel Iron Bar
     23446, -- Adamantite Bar
-    23447. -- Eternium Bar
+    23447, -- Eternium Bar
     23448, -- Felsteel Bar
-    23573. -- Hardened Adamantite Bar
-    23449. -- Khorium Bar
+    23573, -- Hardened Adamantite Bar
+    23449, -- Khorium Bar
     35128, -- Hardened Khoirum
 
     -- Gems
