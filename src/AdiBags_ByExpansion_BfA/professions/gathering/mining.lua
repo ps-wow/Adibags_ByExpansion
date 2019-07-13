6fe48a98e26d0933450a5f052047cec837627098
davidcraig <david@davidcraig.eu>
2019-07-13 18:42:56 +0100

    Add owlseye and royal quartz

--- src/AdiBags_ByExpansion_BfA/professions/gathering/mining.lua
+++ src/AdiBags_ByExpansion_BfA/professions/gathering/mining.lua
@@ -12,4 +12,6 @@ AddonTable.mining = {
     153705, -- Kyanite [Uncommon]
     154123, -- Amberblaze [Rare]
     154124, -- Laribole [Rare]
+    154120, -- Owlseye [Rare]
+    154125, -- Royal Quartz [Rare]
 }
