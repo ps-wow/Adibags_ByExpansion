142cd8eb1b22a909019406a298a77f7ff39112df
David Craig <davidcraig1986@gmail.com>
2020-04-05 15:37:48 +0100

    Build BFD loot.

--- build/AdiBags_ByExpansion_WoW/dungeons/bfd.lua
+++ build/AdiBags_ByExpansion_WoW/dungeons/bfd.lua
@@ -1,4 +1,45 @@
 local AddonName, AddonTable = ...
 
 AddonTable.bfd = {
+    -- Ghamoo-Ra
+    151433,
+    6907,
+    6908,
+    151432,
+    -- Domina
+    11121,
+    3078,
+    132554,
+    888,
+    151435,
+    151434,
+    -- Subjugator Kor'ul
+    6905,
+    6906,
+    151436,
+    -- Thruk
+    120163,
+    120164,
+    120165,
+    151437,
+    -- Guardian of the Deep
+    6904,
+    6901,
+    6902,
+    132555,
+    -- Executioner Gore
+    120167,
+    120166,
+    -- Twilight Lord Bathiel
+    1155,
+    151440,
+    151439,
+    6903,
+    151438,
+    -- Aku'mai
+    6909,
+    132553,
+    6911,
+    6910,
+    151441,
 }
