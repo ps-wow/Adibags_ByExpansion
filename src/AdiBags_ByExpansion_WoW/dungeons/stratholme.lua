eb968316aaca3f667b6d1f0c5a854ddb9eb02bb3
David Craig <davidcraig1986@gmail.com>
2020-04-10 23:36:31 +0100

    Stratholme

--- src/AdiBags_ByExpansion_WoW/dungeons/stratholme.lua
+++ src/AdiBags_ByExpansion_WoW/dungeons/stratholme.lua
@@ -1,5 +1,106 @@
 local AddonName, AddonTable = ...
 
 AddonTable.stratholme = {
+    -- Hearthsinger Forresten
+    13379,  -- Piccolo of the Flaming Fire [Toy]
+    12103,  -- Star of Mystaria
+    13378,  -- Songbird Blouse
+    13384,  -- Rainbow Girdle
+    13383,  -- Woollies of the Prancing Minstrel
+    -- Timmy the Cruel
+    13401,  -- The Cruel Hand of Timmy
+    13400,  -- Vambraces of the Sadist
+    151403, -- Fetid Stranglers
+    13403,  -- Grimgore Noose
+    13402,  -- Timmy's Galoshes
+    -- Commander Malor
+    22403,  -- Diana's Pearl Necklace
+    -- Willey Hopebreaker
+    22404,  -- Willey's Back Scratcher
+    13380,  -- Willey's Portable Howitzer
+    22407,  -- Helm of the New Moon
+    22405,  -- Mantle of the Scarlet Crusade
+    18721,  -- Barrage Girdle
+    13381,  -- Master Cannoneer Boots
+    13382,  -- Cannonball Runner
+    -- Instructor Galford
+    13385,  -- Tome of Knowledge
+    13386,  -- Archivist Cape
+    13387,  -- Foresight Girdle
+    18716,  -- Ash Covered Boots
+    -- Balnazzar
+    13348,  -- Demonshear
+    18717,  -- Hammer of the Grand Crusader
+    13360,  -- Gift of the Elven Magi
+    13353,  -- Book of the Dead
+    13359,  -- Crown of Tyranny
+    18718,  -- Grand Crusader's Helm
+    18720,  -- Shroud of the Nathrezim
+    13358,  -- Wyrmtongue Shoulders
+    13369,  -- Fire Striders
+    -- The Unforgiven
+    22406,  -- Redemption
+    13408,  -- Soul Braeker
+    13404,  -- Mask of the Unforgiven
+    13405,  -- Wailing Nightbane Pauldrons
+    13409,  -- Tearfall Bracers
+    151404, -- Gauntlets of Purged Sanity
+    -- Baroness Anastari
+    13534,  -- Banshee Finger
+    18729,  -- Screeching Bow
+    18728,  -- Anastari Heirloom
+    13538,  -- Windshrieker Pauldrons
+    13535,  -- Coldtouch Phantom Wraps
+    13537,  -- Chillhide Bracers
+    13539,  -- Banshee's Touch
+    18730,  -- Shadowy Laced Handwraps
+    -- Nerub'enkan
+    18738,  -- Carapace Spine Crossbow
+    13529,  -- Husk of Nerub'enkan
+    13533,  -- Acid-Etched Pauldrons
+    13532,  -- Darkspinner Claws
+    18740,  -- Thuzadin Sash
+    18739,  -- Chitinous Plate Legguards
+    13531,  -- Crypt Stalker Leggings
+    13530,  -- Fangdrip Runners
+    -- Maleki the Pallid
+    18737,  -- Bone Slicing Hatchet
+    13524,  -- Skull of Burning Shadows
+    18734,  -- Pale Moon Cloak
+    13528,  -- Twilight Void Bracers
+    13525,  -- Darkbind Fingers
+    13526,  -- Flamescarred Girdle
+    13527,  -- Lavawalker Greaves
+    18735,  -- Maleki's Footwraps
+    -- Magistrate Barthilas
+    18725,  -- Peacemaker
+    18727,  -- Crimson Felt Hat
+    13376,  -- Royal Tribunal Cloak
+    18726,  -- Magistrate's Cuffs
+    18722,  -- Death Grips
+    -- Ramstein the Gorger
+    13372,  -- Slavedriver's Cane
+    13375,  -- Crest of Retribution
+    18723,  -- Animated Chain Necklace
+    13374,  -- Soulstealer Mantle
+    13373,  -- Band of Flesh
+    13515,  -- Ramstein's Lightning Bolts
+    -- Lord Aurius Rivendare
+    13335,  -- Deathcharger's Reins
+    13505,  -- Runeblade of Baron Rivendare
+    13368,  -- Bonescraper
+    13349,  -- Scepter of the Unholy
+    13361,  -- Skullforge Reaver
+    22408,  -- Ritssyn's Wand of Bad Mojo
+    22411,  -- Helm of the Executioner
+    22412,  -- Thuzadin Mantle
+    13340,  -- Cape of the Black Baron
+    13346,  -- Robes of the Exalted
+    22409,  -- Tunic of the Crescent Moon
+    13344,  -- Dracorian Gauntlets
+    22410,  -- Gauntlets of Deftness
+    13345,  -- Seal of Rivendare
+
+    -- Other
     65973, -- Fras Siabi's Cigar Cutter
 }
