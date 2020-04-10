dae8170d9f39a3e80dbdd8c82e5e6f772a35246f
David Craig <davidcraig1986@gmail.com>
2020-04-10 22:52:42 +0100

    Scholo

--- build/AdiBags_ByExpansion_WoW/dungeons/scholo.lua
+++ build/AdiBags_ByExpansion_WoW/dungeons/scholo.lua
@@ -1,4 +1,37 @@
 local AddonName, AddonTable = ...
 
 AddonTable.scholomance = {
+    -- Instructor Chillheart
+    88339,
+    88335,
+    88338,
+    88337,
+    88336,
+    -- Jandice Baron
+    88346,
+    88345,
+    88349,
+    88347,
+    88348,
+    -- Rattlegore
+    88344,
+    88341,
+    88357,
+    88340,
+    88342,
+    88343,
+    -- Lilian Voss
+    88351,
+    88354,
+    88352,
+    88353,
+    88350,
+    -- Darkmaster Gandling
+    88362,
+    88361,
+    88356,
+    88359,
+    88358,
+    88360,
+    88355,
 }
--- src/AdiBags_ByExpansion_WoW/dungeons/scholo.lua
+++ src/AdiBags_ByExpansion_WoW/dungeons/scholo.lua
@@ -1,4 +1,37 @@
 local AddonName, AddonTable = ...
 
 AddonTable.scholomance = {
+    -- Instructor Chillheart
+    88339, -- Gravetouch Greatsword
+    88335, -- Anarchist's Pendant
+    88338, -- Breastplate of Wracking Souls
+    88337, -- Shadow Puppet Bracers
+    88336, -- Icewrath Belt
+    -- Jandice Baron
+    88346, -- Metanoia Shield
+    88345, -- Barovian Ritual Hood
+    88349, -- Phantasmal Drape
+    88347, -- Ghostwoven Legguards
+    88348, -- Wraithplate Treads
+    -- Rattlegore
+    88344, -- Goresoaked Headreaper
+    88341, -- Necromantic Wand
+    88357, -- Vigorsteel Spaulders
+    88340, -- Deadwalker Bracers
+    88342, -- Rattling Gloves
+    88343, -- Bone Golem Boots
+    -- Lilian Voss
+    88351, -- Soulburner Crown
+    88354, -- Necklace of the Dark Blaze
+    88352, -- Shivbreaker Vest
+    88353, -- Dark Blaze Gauntlets
+    88350, -- Leggings of Unleashed Anguish
+    -- Darkmaster Gandling
+    88362, -- Shoulderguards of Painful Lessons
+    88361, -- Gloves of Explosive Pain
+    88356, -- Tombstone Gauntlets
+    88359, -- Incineration Belt
+    88358, -- Lessons of the Darkmaster
+    88360, -- Price of Progress
+    88355, -- Searing Words
 }
