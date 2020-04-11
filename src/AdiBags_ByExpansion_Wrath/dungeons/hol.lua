a3ae4603f5c20423c699379fcdfa2437b9c4d7db
David Craig <davidcraig1986@gmail.com>
2020-04-11 13:14:57 +0100

    Halls of Lightning.

--- src/AdiBags_ByExpansion_Wrath/dungeons/hol.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/hol.lua
@@ -2,5 +2,49 @@ local AddonName, AddonTable = ...
 
 -- Halls of Lightning
 AddonTable.hol = {
-
+    -- General Bjarngrim
+    36980,  -- Hewn Sparring Quarterstaff
+    157580, -- Spark of the Forge
+    37814,  -- Iron Dwarf Smith Pauldrons
+    36982,  -- Mantle of Electrical Charges
+    37825,  -- Traditionally Dyed Handguards
+    37826,  -- The General's Steel Girdle
+    37818,  -- Patroller's War-Kilt
+    36979,  -- Bjarngrim Family Signet
+    -- Volkhan
+    36984,  -- Eternally Folded Blade
+    36985,  -- Volkhan's Hood
+    36983,  -- Cape of Seething Steam
+    37840,  -- Shroud of Reverberation
+    37843,  -- Giant-Hair Woven Gloves
+    37842,  -- Belt of Vivacity
+    36986,  -- Kilt of Molten Golems
+    157579, -- Slag-Stained Legplates
+    37841,  -- Slag Footguards
+    -- Ionar
+    39534,  -- Pauldrons of the Lightning Revenant
+    39657,  -- Tornado Cuffs
+    37846,  -- Charged-Bolt Grips
+    39536,  -- Thundercloud Grasps
+    37845,  -- Cord of Swirling Winds
+    39535,  -- Ionar's Girdle
+    37847,  -- Skywall Striders
+    37844,  -- Winged Talisman
+    -- Loken
+    37852,  -- Colossal Skull-Clad Cleaver
+    37848,  -- Lightning Giant Staff
+    36989,  -- Ancient Measuring Rod
+    36994,  -- Projectile Activator
+    36996,  -- Hood of the Furtive Assassin
+    37849,  -- Planetary Helm
+    36988,  -- Chaotic Spiral Amulet
+    37851,  -- Ornate Woolen Stola
+    36991,  -- Raiments of the Titans
+    37853,  -- Advanced Tooled-Leather Bands
+    36995,  -- Fists of Loken
+    37850,  -- Flowing Sash of Order
+    37855,  -- Mail Girdle of the Audient Earth
+    36992,  -- Leather-Braced Chain Leggings
+    37854,  -- Woven Bracae Leggings
+    36993,  -- Seal of the Pantheon
 }
