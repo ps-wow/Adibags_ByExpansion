120d7f8183746b35d3f0f3567398a57637d7ee45
David Craig <davidcraig1986@gmail.com>
2020-04-11 16:46:18 +0100

    Niuzao temple.

--- src/AdiBags_ByExpansion_MoP/dungeons/niuzao.lua
+++ src/AdiBags_ByExpansion_MoP/dungeons/niuzao.lua
@@ -1,4 +1,34 @@
 local AddonName, AddonTable = ...
 
 AddonTable.niuzao = {
+    -- Vizier Jin'bak
+    144022, -- Hood of Viridian Residue
+    144162, -- Engraved Amber Pendant
+    144163, -- Girdle of Soothing Detonation
+    143996, -- Sap-Encrusted Legplates
+    144156, -- Flashfrozen Resin Globule
+    -- Commander Vo'jak
+    144164, -- Siege-Captain's Scimitar
+    144023, -- Sightfinder Helm
+    143963, -- Chestwrap of Arcing Flame
+    144165, -- Bombardment Bracers
+    144166, -- Archer's Precision Grips
+    -- General Pa'valak
+    144167, -- Tempestuous Longbow
+    144170, -- Aerial Bombardment Cloak
+    144168, -- Siegeworn Bracers
+    144169, -- Breezebinder Handwraps
+    144157, -- Vial of Ichorous Blood
+    -- Wing Leader Ner'onok
+    144172, -- Gustwalker Staff
+    144171, -- Ner'onok's Razor Katar
+    144219, -- Tolakesh, Horn of the Black Ox
+    144025, -- Breezeswept Hood
+    144024, -- Windblast Helm
+    143998, -- Spaulders of Immovable Stone
+    143999, -- Whisperwind Spaulders
+    143964, -- Galedodger Chestguard
+    144173, -- Belt of Totemic Binding
+    144000, -- Airbender Sandals
+    143997, -- Anchoring Sabatons
 }
