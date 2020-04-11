b87bb0b949b08c23b5f6468ad6189d9010360e4b
David Craig <davidcraig1986@gmail.com>
2020-04-11 10:56:19 +0100

    The Nexus complete.

--- src/AdiBags_ByExpansion_Wrath/dungeons/nexus.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/nexus.lua
@@ -1,5 +1,42 @@
 local AddonName, AddonTable = ...
 
 AddonTable.nexus = {
-
+    -- Grand Magus Telestra
+    35617,  -- Wand of Shimmering Scales
+    37134,  -- Telestra's Journal
+    37135,  -- Arcane-Shielded Helm
+    37139,  -- Spaulders of the Careless Thief
+    37138,  -- Bands of Channeled Energy
+    35604,  -- Insulating Bindings
+    35605,  -- Belt of Draconic Runes
+    -- Anomalus
+    35598,  -- Tome of the Lore Keepers
+    37149,  -- Helm of Anomalus
+    37141,  -- Amulet of Dazzling Light
+    37144,  -- Hauberk of the Arcane Wraith
+    35599,  -- Gauntlets of Serpent Scales
+    35600,  -- Cleated Ice Boots
+    37150,  -- Rift Striders
+    -- Ormorok the Tree-Shaper
+    35601,  -- Drakonid Arm Blade
+    35602,  -- Chiseled Stalagmite Pauldrons
+    37153,  -- Gloves of the Crystal Gardener
+    157559, -- Chilly Cinch
+    37152,  -- Girdle of Ice
+    37155,  -- Frozen Forest Kilt
+    35603,  -- Greaves of the Blue Flight
+    37151,  -- Band of Frosted Thorns
+    -- Keristrasza
+    35595,  -- Glacier Sharpened Vileblade
+    37169,  -- War Mace of Unrequited Love
+    37162,  -- Bulwark of the Noble Protector
+    37165,  -- Crystal-Infused Tunic
+    37170,  -- Interwoven Scale Bracers
+    37172,  -- Gloves of Glistening Runes
+    157565, -- Tangler-Leather Gloves
+    37171,  -- Flame-Bathed Steel Girdle
+    35596,  -- Attuned Crystalline Boots
+    37167,  -- Dragon Slayer's Sabatons
+    35597,  -- Band of Glittering Permafrost
+    37166,  -- Sphere of Red Dragon's Blood
 }
