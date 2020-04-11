2927a5563ca531908564248b2d4280686c36b18c
David Craig <davidcraig1986@gmail.com>
2020-04-11 12:46:34 +0100

    Gundrak

--- src/AdiBags_ByExpansion_Wrath/dungeons/gundrak.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/gundrak.lua
@@ -1,4 +1,42 @@
 local AddonName, AddonTable = ...
 
 AddonTable.gundrak = {
+    -- Slad'ran
+    35583,  -- Witch Doctor's Wildstaff
+    37626,  -- Wand of Sseratus
+    37627,  -- Snake Den Spaulders
+    35584,  -- Embroidered Gown of Zul'Drak
+    157578, -- Belt of Vile Concoctions
+    37628,  -- Slad'ran's Coiled Cord
+    35585,  -- Cannibal's Legguards
+    37629,  -- Slithering Slippers
+    -- Drakkari Colossus
+    35590,  -- Drakkari Hunting Bow
+    37636,  -- Helm of Cheated Fate
+    37635,  -- Pauldrons of the Colossus
+    35591,  -- Shoulderguards of the Ice Troll
+    35592,  -- Hauberk of Totemic Mastery
+    37634,  -- Bracers of the Divine Elemental
+    37637,  -- Living Mojo Belt
+    -- Moorabi
+    37631,  -- Fist of the Deity
+    35587,  -- Frozen Scepter of Necromancy
+    37633,  -- Ground Tremor Helm
+    157584, -- Drape of Moorabi
+    37630,  -- Shroud of Moorabi
+    35588,  -- Forlord Breastplate of War
+    37632,  -- Mojo Frenzy Greaves
+    35589,  -- Arcane Focal Signet
+    -- Gal'darah
+    43309,  -- Amulet of the Stampede
+    43305,  -- Shroud of Akali
+    37641,  -- Arcane Flame Altar-Garb
+    37639,  -- Grips of the Beast God
+    37645,  -- Horn-Tripped Gauntlets
+    37643,  -- Sash of Blood Removal
+    37644,  -- Gored Hide Legguards
+    37640,  -- Boots of Transformation
+    43306,  -- Gal'darahh's Signet
+    37642,  -- Hemorrhaging Circle
+    37638,  -- Offering of Sacrifice
 }
