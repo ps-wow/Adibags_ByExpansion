9705da8b96c322b245b82e44866e26eeeedafe9c
David Craig <davidcraig1986@gmail.com>
2020-04-11 22:44:48 +0100

    Re-add LBRS

---
+++ build/AdiBags_ByExpansion_WoW/dungeons/lbrs.lua
@@ -0,0 +1,13 @@
+local AddonName, AddonTable = ...
+
+AddonTable.lbrs = {
+    -- Highlord Omokk
+    -- Shadow Hunter Vosh'gajin
+    -- War Master Voone
+    -- Mother Smolderweb
+    -- Urok Doomhowl
+    -- Quartermaster Zigris
+    -- Halycon
+    -- Gizrul the Slavener
+    -- Overlord Wyrmthalak
+}
