abdb0d29313342756027aaaca8482f0d70163b6e
David Craig <davidcraig1986@gmail.com>
2020-04-11 16:16:57 +0100

    Dungeon: Operation Mechagon.

--- src/AdiBags_ByExpansion_BfA/dungeons/mechagon.lua
+++ src/AdiBags_ByExpansion_BfA/dungeons/mechagon.lua
@@ -2,7 +2,6 @@ local AddonName, AddonTable = ...
 
 AddonTable.mechagon = {
     -- King Gobbamak
-    170507, -- Omnipurpose Logic Board
     169050, -- Logg
     169035, -- Reclaimed Shock Coil
     169052, -- Cranial Recalibrator
@@ -10,7 +9,90 @@ AddonTable.mechagon = {
     169051, -- Anodized Plate Legguards
     169053, -- Roughshod Chain Boots
     169049, -- Supplicant's Soiled Slippers
-
     -- Gunker
+    169058, -- Salvaged Incendiary Tool
+    169062, -- Sharpened Trogg Femur
+    169061, -- Insulating Threaded Gloves
+    169059, -- Slick Tactical Grips
+    169060, -- Mekgineer's Utility Belt
+    169057, -- Well-Oiled Plate Girdle
+    169055, -- Greaves of Acid Resistance
+    169056, -- Ooey-Gooey Galoshes
+    -- Trixie & Naeno
+    169066, -- Trixie's Backup Backbiter
+    169068, -- Salvaged Mekacycle Shielding
+    169064, -- Mountebank's Colorful Cloak
+    169069, -- Wraps of Electrostatic Potential
+    169063, -- High Speed Gauntlets
+    169067, -- Silken Safety Harness
+    169065, -- Reinforced Riding Chausses
+    169070, -- Unseen Predator's Breeches
+    169769, -- Remote Guidance Device
+    -- HK-8 Aerial Oppression Unit
+    168657, -- Friend-or-Foe Identifier
+    167677, -- Harmonic Dematerializer
+    168826, -- Mechagon Peacekeeper [Mount]
+    169385, -- Microbot 8D [Pet]
+    168909, -- Subroutine: Emergency Repairs
+    168963, -- Fusion Hacker
+    169077, -- Light Auto-Stabilizing Energy Rifle
+    169074, -- Epaulettes of Arcing Power
+    169075, -- Tank Buster Pauldrons
+    169073, -- Type II Bomber Jacket
+    169072, -- Volatile Arming Doublet
+    169071, -- Overcharged Pantaloons
+    169157, -- Logic Loop of Division
+    169076, -- Logic Loop of Maintenance
+    169158, -- Logic Loop of Recursion
+    169156, -- Logic Loop of Synergy
+    -- Tussle Tonks
+    168962, -- Apex Perforator
+    168955, -- Electrifying Cognitive Amplifier
+    168967, -- Gold-Coated Superconductors
+    168957, -- Mekgineer's Championship Belt
+    168958, -- Ringmaster's Cummerbund
+    168966, -- Heavy Alloy Legplates
+    168964, -- Hyperthreaded Boots
+    168965, -- Modular Platinum Plating
+    -- K.U.-J.O.
+    168970, -- Trashmaster's Mantle
+    168969, -- Operator's Mitts
+    168971, -- Swift Pneumatic Grips
+    168968, -- Flame-Seared Leggings
+    168972, -- Pyroclastic Greatboots
+    -- Machinist's Garden
+    167556, -- Subroutine: Overclock
+    168973, -- Neural Synapse Enhancer
+    169608, -- Tearing Sawtooth Blade
+    168976, -- Automatic Waist Tightener
+    168974, -- Self-Repairing Cuisses
+    168975, -- Machinist's Treasured Treads
+    169159, -- Overclocking Bit Band
+    169161, -- Protecting Bit Band
+    168977, -- Rebooting Bit Band
+    169160, -- Shorting Bit Band
+    169344, -- Ingenious Mana Battery
+    -- King Mechagon
+    169172, -- Blueprint: Perfectly Timed Differential
+    168671, -- Electromagnetic Resistors
+    168842, -- Engine of Mecha-Perfection
+    169378, -- Golden Snorf
+    169774, -- Progression Sprocket
+    168984, -- Extravagant Epaulettes
+    168987, -- Shoulderguards of Fraying Sanity
+    168981, -- Circuit-Linked Chainmail
+    168979, -- Mechanized Plate Chasse
+    168978, -- Anodized Deflectors
+    168989, -- Hyperthread Wristwraps
+    168980, -- Gauntlets of Absolute Authority
+    168985, -- Self-Sanitizing Handwraps
+    168986, -- Mad King's Sporran
+    168983, -- Maniacal Monarch's Girdle
+    168988, -- Royal Attendant's Trousers
+    168982, -- Regal Mekanospurs
+    -- Multiple
+    170510, -- Forceful Logic Board
+    170509, -- Performant Logic Board
     170508, -- Optimized Logic Board
+    170507, -- Omnipurpose Logic Board
 }
