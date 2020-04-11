93a08c5dd23246321846c5a2069dc5fa4d7db1a4
David Craig <davidcraig1986@gmail.com>
2020-04-11 13:00:51 +0100

    Forge of Souls.

--- src/AdiBags_ByExpansion_Wrath/dungeons/forge.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/forge.lua
@@ -1,5 +1,43 @@
 local AddonName, AddonTable = ...
 
 AddonTable.forge = {
-
+    -- Bronjahm
+    50316, -- Papa's Brand New Bag
+    50317, -- Papa's New Bag
+    49783, -- Lucky Old Sun
+    50191, -- Nighttime
+    50197, -- Eyes of Bewilderment
+    50196, -- Love's Prisoner
+    49785, -- Bewildering Shoulderpads
+    50193, -- Very Fashionable Shoulders
+    49786, -- Robes of the Cheating Heart
+    39787, -- Seven Stormy Mornings
+    49788, -- Cold Sweat Grips
+    50194, -- Weeping Gauntlets
+    49784, -- Minister's Number One Legplates
+    -- Devourer of Souls
+    49790, -- Blood Boil Lancet
+    49793, -- Tower of the Mouldering Corpse
+    50203, -- Blood Weeper
+    49789, -- Heartshiver
+    50210, -- Seethe
+    50206, -- Frayed Scoundrel's Cap
+    50214, -- Helm of the Spirit Shock
+    50211, -- Arcane Loops of Anger
+    49799, -- Coil of Missing Gems
+    50208, -- Pauldrons of the Souleater
+    49792, -- Accursed Crawling Cape
+    49791, -- Lost Reliquary Chestguard
+    50213, -- Mord'rethar Robes
+    49797, -- Brace Guards of the Starless Night
+    50209, -- Essence of Suffering
+    50212, -- Essence of Desire
+    49796, -- Essence of Anger
+    49794, -- Legplates of Frozen Granite
+    50207, -- Black Spire Sabatons
+    50215, -- Recorvered Reliquary Boots
+    49795, -- Sollerets of Suffering
+    49798, -- Soul Screaming Boots
+    49800, -- Spiteful Signet
+    50198, -- Needle-Encrusted Scorpion
 }
