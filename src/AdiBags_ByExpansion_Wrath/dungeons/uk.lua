a982e4b5c849ce0a0fe34b23e65ec00a5065e42d
David Craig <davidcraig1986@gmail.com>
2020-04-11 11:09:01 +0100

    Utgarde Keep.

--- src/AdiBags_ByExpansion_Wrath/dungeons/uk.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/uk.lua
@@ -1,5 +1,33 @@
 local AddonName, AddonTable = ...
 
 AddonTable.uk = {
-
+    -- Prince Keleseth
+    37179,  -- Infantry Assault Blade
+    35570,  -- Keleseth's Blade of Evocation
+    37177,  -- Wand of the San'layn
+    37180,  -- Battlemap Hide Helm
+    35572,  -- Reinforced Velvet Helm
+    35571,  -- Dragon Stabler's Gauntlets
+    37178,  -- Strategist's Belt
+    -- Skarvald & Dalronn
+    35573,  -- Arm Blade of Augelmir
+    37181,  -- Dagger of Betrayal
+    157555, -- Constructor's Worklight
+    37182,  -- Helmet of the Constructor
+    35574,  -- Chestplate of the Northern Lights
+    37184,  -- Dalronn's Jerkin
+    35575,  -- Skarvald's Dragonskin Habergeon
+    37183,  -- Bindings of the Tunneler
+    -- Invagar the Plunderer
+    37190,  -- Enraged Feral Staff
+    35576,  -- Ingvar's Monolithic Cleaver
+    37191,  -- Drake Mounted Crossbow
+    37188,  -- Plunderer's Helmet
+    35578,  -- Overlaid Chain Spaulders
+    37194,  -- Sharp-Barbed Leather Belt
+    37189,  -- Breeches of the Caller
+    35577,  -- Holistic Patchword Breeches
+    37193,  -- Staggering Legplates
+    37192,  -- Annhylde's Ring
+    37186,  -- Unsmashable Heavy Band
 }
