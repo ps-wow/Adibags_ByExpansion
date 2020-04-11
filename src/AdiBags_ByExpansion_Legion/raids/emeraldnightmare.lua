a3b49eb5efeb1aa04e15ea08b2fcabe255b817c9
David Craig <davidcraig1986@gmail.com>
2020-04-11 11:49:25 +0100

    Emerald Nightmare.

--- src/AdiBags_ByExpansion_Legion/raids/emeraldnightmare.lua
+++ src/AdiBags_ByExpansion_Legion/raids/emeraldnightmare.lua
@@ -69,11 +69,51 @@ AddonTable.emeraldnightmare = {
     -- Dragons of Nightmare
     138228, -- Bioluminescent Mushroom (Artifact Relic)
     138227, -- Entrancing Stone (Artifact Relic)
+    138226, -- Nightmare Engulfed Jewel (Artifact Relic)
+    139205, -- Cowl of Fright
+    138215, -- Dreadmscale Inlaid Vestments
+    138216, -- Horror Inscribed Chestguard
+    138218, -- Dragonbone Wristclamps
+    138219, -- Dragonspur Wristguards
+    138214, -- Gauntlets of the Demented Mind
+    138212, -- Handwraps of Delusional Power
+    138211, -- Malignant Sabatons
+    138220, -- Mindrend Band
+    138225, -- Phantasmal Echo
+    138224, -- Unstable Horrorslime
+    138222, -- Vial of Nightmare Fog
     -- Cenarius
     139263, -- Blessing of Cenarius (Artifact Relic)
+    139265, -- Radiant Dragon Egg (Artifact Relic)
+    139264, -- Uplifting Emerald (Artifact Relic)
+    139231, -- Crown of Steely Brambles
+    139192, -- Mantle of Perpetual Bloom
+    139218, -- Thorny Bramblemail Pauldrons
+    139248, -- Evergreen Vinewrap Drape
+    139207, -- Grove Keeper's Robe
+    139225, -- Fitted Ironbark Gauntlets
+    139198, -- Forest-Lord's Waistwrap
+    139211, -- Laughing Sister's Pouch-Chain
+    139194, -- Cozy Dryad Hoof-Socks
+    139333, -- Horn of Cenarius
+    139334, -- Nature's Call
     -- Xavius
     139267, -- Azsharan Coucillor's Clasp (Artifact Relic)
     139269, -- Crystallized Drop of Eternity (Artifact Relic)
     139266, -- Fragment of Eternal Spite (Artifact Relic)
     138827, -- Illusion: Nightmare
+    139268, -- Nightmarish Elm Branch (Artifact Relic)
+    139189, -- Hood of Darkened Visions
+    139332, -- Blackened Portalstone Necklace
+    139232, -- Midnight Herald's Pauldrons
+    141006, -- The First Satyr's Spaulders
+    139193, -- Maddening Robe of Secrets
+    139222, -- Manacles of the Nightmare Colossus
+    139228, -- Eon-Tempered Waistplate
+    139216, -- Disjointed Linkage Leggings
+    139203, -- Repulsive Leathery Pants
+    139199, -- Boots of Endless Betrayal
+    139238, -- Twice-Wraped Azsharan Signet
+    139336, -- Bough of Corruption
+    139335, -- Grotesque Statuette
 }
