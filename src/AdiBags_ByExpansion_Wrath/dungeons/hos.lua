7a1556055d8ff003caf37dba42421989dbd874e2
David Craig <davidcraig1986@gmail.com>
2020-04-11 14:38:33 +0100

    Halls of Stone.

--- src/AdiBags_ByExpansion_Wrath/dungeons/hos.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/hos.lua
@@ -1,5 +1,41 @@
 local AddonName, AddonTable = ...
 
 AddonTable.hos = {
-
+    -- Krystallus
+    37649, -- Quarry Chisel
+    35670, -- Brann's Lost Mining Helmet
+    35672, -- Hollow Geode Help
+    37652, -- Spaulders of Krystallus
+    35673, -- Leggings of Burning Gleam
+    37650, -- Shardling Legguards
+    37651, -- The Prospector's Prize
+    -- Maiden of Grief
+    38618, -- Hammer of Grief
+    38613, -- Chain of Fiery Orbs
+    38614, -- Embrace of Sorrow
+    38615, -- Lightning-Charged Gloves
+    38616, -- Maiden's Girdle
+    38611, -- Ringlet of Repose
+    38617, -- Woeful Band
+    -- Tribunal of Ages
+    37653, -- Sword of Justice
+    37655, -- Mantle of the Tribunal
+    35677, -- Cosmos Vestments
+    35675, -- Linked Armor of the Sphere
+    157564, -- Mabled Bracers
+    37656, -- Raging Construct Bands
+    35676, -- Constellation Leggings
+    37654, -- Sabatons of the Ages
+    -- Sjonnir the Ironshaper
+    37667, -- The Fleshshaper
+    35679, -- Static Cowl
+    35680, -- Amulet of Wills
+    37658, -- Sun-Emblazoned Chestplate
+    37668, -- Bands of the Stoneforge
+    37670, -- Sjonnir's Girdle
+    35678, -- Ironshaper's Legplates
+    37669, -- Leggings of the Stone Halls
+    37666, -- Boots of the Whirling Mist
+    37660, -- Forge Ember
+    37657, -- Spark of Life
 }
