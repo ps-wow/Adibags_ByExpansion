7d05b06dd408bb5c1f96edc2e57551ee5c11f0a9
David Craig <davidcraig1986@gmail.com>
2020-04-11 12:33:00 +0100

    The Oculus

--- src/AdiBags_ByExpansion_Wrath/categories/junk.lua
+++ src/AdiBags_ByExpansion_Wrath/categories/junk.lua
@@ -1,21 +1,28 @@
 local AddonName, AddonTable = ...
 
 AddonTable.junk = {
+    33363, -- Thread-Bare Cloth Shoulderpads
     33365, -- Frost-Rimed Cloth Belt
     33366, -- Frost-Rimed Cloth Boots
     33367, -- Frost-Rimed Cloth Bracers
     33369, -- Frost-Rimed Cloth Pants
     33370, -- Frost-Rimed Cloth Shoulderpads
     33371, -- Frost-Rimed Cloth Vest
+    33376, -- Fur-Lined Gloves
+    33378, -- Fur-Lined Shoulderpads
     33379, -- Frozen Armor
     33380, -- Frozen Belt
     33381, -- Frozen Boots
+    33391, -- Icy Mail Belt
+    33393, -- Icy Mail Bracers
+    33395, -- Icy Mail Pants
     33397, -- Frigid Mail Armor
     33398, -- Frigid Mail Belt
     33399, -- Frigid Mail Boots
 
     33400, -- Frigid Mail Bracers
     33402, -- Frigid Mail Pants
+    33406, -- Ice-Bound Plate Bracers
     33412, -- Frost-Worn Plate Belt
     33415, -- Frost-Worn Plate Chestpiece
     33416, -- Frost-Worn Plate Gloves
@@ -25,6 +32,7 @@ AddonTable.junk = {
     33424, -- Cracked Iron Staff
     33425, -- Ice-Pitted Blade
     33426, -- Chipped Timber Axe
+    33427, -- Frost-Encrusted Rifle
     33428, -- Dulled Shiv
     33429, -- Ice Cleaver
     33430, -- Abandoned Greatsword
--- src/AdiBags_ByExpansion_Wrath/dungeons/oculus.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/oculus.lua
@@ -1,5 +1,49 @@
 local AddonName, AddonTable = ...
 
 AddonTable.oculus = {
-
+    -- Drakos the Interrogator
+    157562, -- Rod of Aggressive Questioning
+    36944,  -- Lifeblade of Belgaristrasz
+    37255,  -- The Interrogator
+    36943,  -- Timeless Beads of Eternos
+    37258,  -- Drakewing Raiments
+    36946,  -- Runic Cage Chestpiece
+    37256,  -- Scaled Armor of Drakos
+    36945,  -- Verdisa's Cuffs of Dreaming
+    37257,  -- Band of Torture
+    -- Varos Cloudstrider
+    37260,  -- Cloudstrider's Waraxe
+    36948,  -- Horned Helm of Varos
+    36947,  -- Centrifuge Core Cloak
+    36950,  -- Wing Commander's Breastplate
+    37261,  -- Gloves of Radiant Light
+    36949,  -- Gloves of the Azure-Lord
+    37262,  -- Azure Ringmail Leggings
+    37263,  -- Legplates of the Oculus Guardian
+    -- Mage-Lord Urom
+    36953,  -- Spaulders of Skillful Maneuvers
+    37288,  -- Catalytic Bands
+    36951,  -- Sidestepping Handguards
+    36952,  -- Girdle of Obscuring
+    37289,  -- Sash of Phantasmal Images
+    36954,  -- The Conjurer's Slippers
+    37195,  -- Band of Enchanted Growth
+    37264,  -- Pendulum of Telluric Currents
+    -- Ley-Guardian Eregos
+    36975,  -- Malygos' Favor
+    37360,  -- Staff of Draconic Combat
+    36962,  -- Wyrmclaw Battleaxe
+    37294,  -- Crown of Unbridled Magic
+    36971,  -- Headguard of Westrift
+    36969,  -- Helm of the Ley-Guardian
+    37293,  -- Mask of the Watcher
+    37291,  -- Ancient Dragon Spirit Cape
+    36974,  -- Eregos' Ornamented Chestguard
+    36973,  -- Vestments of the Scholar
+    37361,  -- Cuffs of Winged Levitation
+    37363,  -- Gauntlets of Dragon Wrath
+    37362,  -- Leggings of Protective Auras
+    37292,  -- Ley-Guardian's Legguards
+    36961,  -- Dragonflight Great-Ring
+    36972,  -- Tome of Arcane Phenomena
 }
