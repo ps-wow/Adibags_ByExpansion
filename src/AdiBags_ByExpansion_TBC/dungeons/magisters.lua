65a7c772bb0d369c434e15a6b077b30f7166612f
David Craig <davidcraig1986@gmail.com>
2020-04-12 21:02:02 +0100

    Magister's Terrace

--- src/AdiBags_ByExpansion_TBC/dungeons/magisters.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/magisters.lua
@@ -1,5 +1,57 @@
 local AddonName, AddonTable = ...
 
--- Mana-Tombs
+-- Magisters' Terrace
 AddonTable.magisters = {
+    -- Selin Fireheart
+    34604, -- Jaded Crystal Dagger
+    34699, -- Sun-Forged Cleaver
+    34601, -- Shoulderplates of Everlasting Pain
+    34702, -- Cloak of Swift Mending
+    34697, -- Bindings of Raging Fire
+    34698, -- Bracers of the Forest Stalker
+    34602, -- Eversong Cuffs
+    34700, -- Gauntlets of Divine Blessings
+    34701, -- Leggings of the Betrayed
+    -- Vexallus
+    34608, -- Rod of the Blazing Light
+    34606, -- Edge of Oppression
+    34703, -- Latro's Dancing Blade
+    34607, -- Fel-Tinged Mantle
+    34708, -- Cloak of the Coming Night
+    34605, -- Breastplate of Fierce Survival
+    34705, -- Bracers of Divine Infusion
+    34707, -- Boots of Resuscitation
+    34704, -- Band of Arcane Alacrity
+    34706, -- Band of Determination
+    -- Priestess Delrissa
+    34790, -- Battle-Mace of the High Priestess
+    34788, -- Duskhallow Mantle
+    34792, -- Cloak of the Betrayed
+    34789, -- Bracers of Slaughter
+    34791, -- Gauntlets of the Tranquil Waves
+    34473, -- Commendation of Kael'thas
+    24472, -- Shard of Contempt
+    34470, -- Timbal's Focusing Crystal
+    34471, -- Vial of the Sunwell
+    -- Kael'thas Sunstrider
+    34794, -- Axe of Shattered Dreams
+    34797, -- Sun-Infused Focus Staff
+    34616, -- Breeching Comet
+    34611, -- Cudgel of Consecration
+    34609, -- Quickening Blade of the Prince
+    34795, -- Helm of Sanctification
+    34613, -- Shoulderpads of the Silvermoon Retainer
+    34810, -- Cloak of Blade Turning
+    34799, -- Hauberk of the War Bringer
+    34615, -- Netherforce Chestplate
+    34796, -- Robes of Summer Flame
+    34610, -- Scarlet Sin'dorei Robes
+    34614, -- Tunic of the Ranger Lord
+    34808, -- Gloves of Arcane Acuity
+    34793, -- Cord of Reconstruction
+    34612, -- Greaves of the Penitent Knight
+    34809, -- Sunrage Treads
+    34807, -- Sunstrider Warboots
+    34798, -- Band of Celerity
+    34625, -- Kharmaa's Ring of Fate
 }
