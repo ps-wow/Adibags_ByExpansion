b777d4489d88f01a5d6beab3b00ff57d3243ed96
David Craig <davidcraig1986@gmail.com>
2020-04-12 20:28:13 +0100

    Steamvaults

--- build/AdiBags_ByExpansion_TBC/dungeons/steamvault.lua
+++ build/AdiBags_ByExpansion_TBC/dungeons/steamvault.lua
@@ -1,4 +1,33 @@
 local AddonName, AddonTable = ...
 
 AddonTable.steamvault = {
+    -- Hydromancer Thespia
+    27789,
+    27806,
+    27475,
+    27508,
+    27783,
+    27784,
+    -- Mekgineer Steamrigger
+    27791,
+    27794,
+    27790,
+    27792,
+    27787,
+    27793,
+    27795,
+    -- Warlord Kalithresh
+    29351,
+    27801,
+    27738,
+    27737,
+    27804,
+    28203,
+    27799,
+    29463,
+    29243,
+    27510,
+    27874,
+    30543,
+    27805,
 }
--- src/AdiBags_ByExpansion_TBC/dungeons/steamvault.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/steamvault.lua
@@ -1,4 +1,33 @@
 local AddonName, AddonTable = ...
 
 AddonTable.steamvault = {
+    -- Hydromancer Thespia
+    27789, -- Cloak of Whispering Shells
+    27806, -- Fathomheart Gauntlets
+    27475, -- Gauntlets of the Bold
+    27508, -- Incanter's Gloves
+    27783, -- Moonrage Girdle
+    27784, -- Scintillating Coral Band
+    -- Mekgineer Steamrigger
+    27791, -- Serpentcrest Life-Staff
+    27794, -- Recoilless Rocket Ripper X-54
+    27790, -- Mask of Penance
+    27792, -- Steam-Hinge Chain of Valor
+    27787, -- Chestguard of No Remorse
+    27793, -- Earth Mantle Handwraps
+    27795, -- Sash of Serpentra
+    -- Warlord Kalithresh
+    29351, -- Wrathtide Longbow
+    27801, -- Beast Lord Mantle
+    27738, -- Incanter's Pauldrons
+    27737, -- Moonglade Shoulders
+    27804, -- Devilshark Cape
+    28203, -- Breastplate of the Righteous
+    27799, -- Vermillion Robes of the Dominant
+    29463, -- Amber Bands of the Aggressor
+    29243, -- Wave-Fury Vambraces
+    27510, -- Tidefury Gauntlets
+    27874, -- Beast Lord Leggings
+    30543, -- Pontifex Kilt
+    27805, -- Ring of the Silver Hand
 }
