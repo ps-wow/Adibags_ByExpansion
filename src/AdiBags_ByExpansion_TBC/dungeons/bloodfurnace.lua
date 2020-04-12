d6b25543fb40fda46101281252445a3107182770
David Craig <davidcraig1986@gmail.com>
2020-04-12 22:12:57 +0100

    Blood Furnace

--- src/AdiBags_ByExpansion_TBC/dungeons/bloodfurnace.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/bloodfurnace.lua
@@ -1,4 +1,45 @@
 local AddonName, AddonTable = ...
 
 AddonTable.bloodfurnace = {
+    -- The Maker
+    24384, -- Diamond-Core Sledgemace
+    27488, -- Mage-Collar of the Firestorm
+    24385, -- Pendant of Battle-Lust
+    27485, -- Embroidered Cape of Mysteries
+    27483, -- Moon-Touched Bands
+    24387, -- Ironblade Gauntlets
+    24388, -- Girdle of the Gale Storm
+    27487, -- Bloodlord Legplates
+    -- Broggok
+    27490, -- Firebrand Battleaxe
+    24389, -- Legion Blunderbuss
+    24392, -- Arcing Bracers
+    27494, -- Emerald Eye Bracer
+    27489, -- Virtue Bearer's Vambraces
+    24393, -- Bloody Surgeon's Mitts
+    24391, -- Kilt of the Night Strider
+    27492, -- Moonchild Leggings
+    27848, -- Embroidreed Spellpyre Boots
+    27491, -- Signet of Repose
+    24390, -- Auslese's Light Channeler
+    -- Keli'dan the Breaker
+    24394, -- Warsong Howling Axe
+    27512, -- The Willbreaker
+    27507, -- Adamantine Repeater
+    27505, -- Ruby Helm of the Just
+    27495, -- Soldier's Dog Tags
+    29347, -- Talisman of the Breaker
+    32080, -- Mantle of Shadowy Embrace
+    24398, -- Mantle of the Dusk-Dweller
+    24397, -- Raiments of Divine Authority
+    27506, -- Robe of Effervescent Light
+    24396, -- Vest of Vengeance
+    28264, -- Wastewalker Tunic
+    27497, -- Doomplate Gauntlets
+    24395, -- Mindfire Waistband
+    27514, -- Leggings of the Unrepentant
+    27788, -- Bloodsworn Warboots
+    29239, -- Eaglecrest Warboots
+    29245, -- Wave-Crest Striders
+    28121, -- Icon of Unyielding Courage
 }
