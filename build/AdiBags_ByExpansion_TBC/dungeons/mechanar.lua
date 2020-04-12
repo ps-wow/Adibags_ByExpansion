2dd0022ba4b84566f2f2336585352c479a0d329f
David Craig <davidcraig1986@gmail.com>
2020-04-12 20:51:29 +0100

    Mechanar

--- build/AdiBags_ByExpansion_TBC/dungeons/mechanar.lua
+++ build/AdiBags_ByExpansion_TBC/dungeons/mechanar.lua
@@ -1,4 +1,32 @@
 local AddonName, AddonTable = ...
 
 AddonTable.mechanar = {
+    -- Mechano-Lord Capacitus
+    28253,
+    28257,
+    28254,
+    28255,
+    28256,
+    -- Nethermancer Sepethrea
+    28263,
+    28260,
+    28275,
+    28262,
+    28202,
+    28259,
+    -- Pathaleon the Calculator
+    28267,
+    27899,
+    29362,
+    28286,
+    28285,
+    28278,
+    28269,
+    28204,
+    32076,
+    28266,
+    30533,
+    29251,
+    28265,
+    28288,
 }
--- src/AdiBags_ByExpansion_TBC/dungeons/mechanar.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/mechanar.lua
@@ -1,4 +1,32 @@
 local AddonName, AddonTable = ...
 
 AddonTable.mechanar = {
+    -- Mechano-Lord Capacitus
+    28253, -- Plasma Rat's Hyper-Scythe
+    28257, -- Hammer of the Penitent
+    28254, -- Warp Engineer's Prismatic Chain
+    28255, -- Lunar-Claw Pauldrons
+    28256, -- Thoriumweave Cloak
+    -- Nethermancer Sepethrea
+    28263, -- Stellaris
+    28260, -- Manual of the Nethermancer
+    28275, -- Beast Lord Helm
+    28262, -- Jade-Skull Breastplate
+    28202, -- Moonglade Robe
+    28259, -- Cosmic Lifeband
+    -- Pathaleon the Calculator
+    28267, -- Edge of the Cosmos
+    27899, -- Mana Wrath
+    29362, -- The Sun Eater
+    28286, -- Telescopic Sharprifle
+    28285, -- Helm of the Righteous
+    28278, -- Incanter's Cowl
+    28269, -- Baba's Cloak of Arcanistry
+    28204, -- Tunic of Assassination
+    32076, -- Handguards of the Steady
+    28266, -- Molten Earth Kilt
+    30533, -- Vanquisher's Legplates
+    29251, -- Boots of the Pious
+    28265, -- Dath'Remar's Ring of Defense
+    28288, -- Abacus of Violent Odds
 }
