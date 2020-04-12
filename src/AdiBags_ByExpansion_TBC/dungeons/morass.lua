cbee33e7c7b386969d4dc495e76b1d41f4144e2d
David Craig <davidcraig1986@gmail.com>
2020-04-12 22:21:24 +0100

    Black Morass

--- src/AdiBags_ByExpansion_TBC/dungeons/morass.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/morass.lua
@@ -1,4 +1,35 @@
 local AddonName, AddonTable = ...
 
 AddonTable.morass = {
+    -- Chrono Lord Deja
+    27987, -- Melmorta's Twilight Longbow
+    27993, -- Mask of Inner Fire
+    27994, -- Mantle of Three Terrors
+    27995, -- Sun-Gilded Shouldercaps
+    27988, -- Burnoose of Shifting Ages
+    27977, -- Legplates of the Bold
+    27873, -- Moonglade Pants
+    27996, -- Ring of Spiritual Precision
+    -- Temporus
+    28033, -- Epoch-Mender
+    29356, -- Quantum Blade
+    28184, -- Millennium Blade
+    28187, -- Star-Heart Lamp
+    28186, -- Laughing Skull Battle-Harness
+    27509, -- Handgrips of Assassination
+    28185, -- Khadgar's Kilt of Abjuration
+    28034, -- Hourglass of the Unraveller
+    -- Aeonus
+    28188, -- Bloodfire Greatstaff
+    28189, -- Latro's Shifting Sword
+    28206, -- Cowl of the Guiltless
+    28192, -- Helm of Desolation
+    28193, -- Mana-Etched Crown
+    28207, -- Pauldrons of the Crimson Flight
+    28194, -- Primal Surge Bracers
+    29247, -- Girdle of the Deathdealer
+    29253, -- Girdle of Valorous Deeds
+    30531, -- Breeches of the Occultist
+    27839, -- Legplates of the Righteous
+    28190, -- Scarab of the Infinite Cycle
 }
