dc5584bf08117659ff81435c1f8376b477e26cb3
David Craig <davidcraig1986@gmail.com>
2020-04-12 21:11:38 +0100

    Botanica

--- src/AdiBags_ByExpansion_TBC/dungeons/botanica.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/botanica.lua
@@ -1,5 +1,45 @@
 local AddonName, AddonTable = ...
 
 AddonTable.botanica = {
+    -- Commander Sarannis
+    28311, -- Revenger
+    28350, -- Warhelm of the Bold
+    28306, -- Towering Mantle of the Hunt
+    28301, -- Sarannis' Mystic Sheen
+    28304, -- Prismatic Mittens of Mending
+    28347, -- Warpscale Leggings
+    -- High Botanist Freywinn
+    28315, -- Stormreaver Warblades
+    28316, -- Aegis of the Sunbird
+    28348, -- Moonglade Cowl
+    28321, -- Enchanted Thorium Torque
+    28228, -- Beast Lord Cuirass
+    28317, -- Energis Armwraps
+    28318, -- Obsidiuan Clodstompers
+    -- Thorngrin the Tender
+    28325, -- Dreamer's Dragonstaff
+    28322, -- Runed Dagger of Solace
+    28324, -- Gauntlets of Cruel Intention
+    28327, -- Arcane Netherband
+    28323, -- Ring of Umbral Doom
+    -- Laj
+    28349, -- Tidefury Helm
+    28340, -- Mantle of Autumn
+    27739, -- Spaulders of the Righteous
+    28328, -- Mithil-Bark Cloak
+    28338, -- Devil-Stitched Leggings
+    28339, -- Boots of the Shifting Sands
+    -- Warp Splinter
+    29359, -- Feral Staff of Lashing
+    28367, -- Greatsword of Forlorn Visions
+    28341, -- Warpstaff of Arcanum
+    28345, -- Warp Splinter's Thorn
+    28343, -- Jagged Bark Pendant
+    28371, -- Netherfury Cape
+    28229, -- Incanter's Robe
     28342, -- Warp Infused Drape
+    32072, -- Gauntlets of Dissension
+    29258, -- Boots of Ethereal Manipulation
+    29262, -- Boots of the Endless Hunt
+    28370, -- Bangle of Endless Blessings
 }
