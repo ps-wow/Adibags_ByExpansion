806e2e3385dcb1567852844fadc106fc9530f852
David Craig <davidcraig1986@gmail.com>
2020-04-12 20:37:53 +0100

    Slave Pens

--- src/AdiBags_ByExpansion_TBC/dungeons/slavepens.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/slavepens.lua
@@ -1,6 +1,51 @@
 local AddonName, AddonTable = ...
 
 AddonTable.slavepens = {
+    -- Mennu the Betrayer
+    24361, -- Spellfire Longword
+    27543, -- Starlight Dagger
+    24356, -- Wastewalker Shiv
+    27546, -- Traitor's Noose
+    24357, -- Vest of Living Lightning
+    27541, -- Archery Belt of the Broken
+    27542, -- Cord of Belief
+    24360, -- Tracker's Belt
+    27545, -- Mennu's Scaled Leggings
+    24359, -- Princely Reign Leggings
+    -- Rokmar the Crackler
+    24378, -- Coilfang Hammer of Renewal
+    24380, -- Calming Spore Reed
+    24381, -- Coilfang Needler
+    27551, -- Skeletal Necklace of Battlerage
+    24379, -- Bogstrok Scale Cloak
+    27550, -- Ironscale War Cloak
+    27547, -- Coldwhisper Cord
+    27548, -- Girdle of Many Blessings
+    28124, -- Liar's Cord
+    27549, -- Wavefury Boots
+    24376, -- Runed Fungalcap
+    -- Quagmirran
+    27741, -- Bleeding Hollow Warhammer
+    27673, -- Phosphorescent Blade
+    27714, -- Swamplight Lantern
+    29349, -- Adamantine Chain of the Unbroken
+    27796, -- Mana-Etched Spaulders
+    27713, -- Pauldrons of Desolation
+    32078, -- Pauldrons of Wild Magic
+    24366, -- Scorpid-Sting Mantle
+    24362, -- Spore-Soaked Vaneer
+    28337, -- Breastplate of Righteous Fury
+    24363, -- Unscarred Breastplate
+    27712, -- Shackles of Quagmirran
+    24365, -- Deft Handguards
+    27672, -- Girdle of the Immovable
+    27742, -- Mage-Fury Girdle
+    24364, -- Azureplate Greaves
+    27800, -- Earthsoul Britches
+    30538, -- Midnight Legguards
+    29242, -- Boots of Blasphemy
+    27740, -- Band of Ursol
+    27683, -- Quagmirran's Eye
     -- Quest Rewards
     25541, -- Cenarion Ring of Casting
 }
