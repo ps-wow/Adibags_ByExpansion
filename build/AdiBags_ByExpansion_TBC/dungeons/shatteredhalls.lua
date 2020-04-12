f5aabf0c40e317117fa3f6aed64b875ee4c7de6b
David Craig <davidcraig1986@gmail.com>
2020-04-12 20:45:25 +0100

    Shattered Halls

--- build/AdiBags_ByExpansion_TBC/dungeons/shatteredhalls.lua
+++ build/AdiBags_ByExpansion_TBC/dungeons/shatteredhalls.lua
@@ -1,4 +1,31 @@
 local AddonName, AddonTable = ...
 
 AddonTable.shatteredhalls = {
+    -- Grand Warlock Nethekurse
+    27534,
+    27520,
+    27519,
+    27517,
+    27521,
+    27529,
+    -- Warbringer O'mrogg
+    27524,
+    27868,
+    27526,
+    29263,
+    27522,
+    29254,
+    37525,
+    -- Warchief Kargath Bladefish
+    27533,
+    27538,
+    29348,
+    27540,
+    27802,
+    29255,
+    27528,
+    27535,
+    27537,
+    27531,
+    27527,
 }
--- src/AdiBags_ByExpansion_TBC/dungeons/shatteredhalls.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/shatteredhalls.lua
@@ -1,4 +1,31 @@
 local AddonName, AddonTable = ...
 
 AddonTable.shatteredhalls = {
+    -- Grand Warlock Nethekurse
+    27534, -- Hortus' Seal of Brilliance
+    27520, -- Greathelm of the Unbreakable
+    27519, -- Cloak of Malice
+    27517, -- Bands of Nethekurse
+    27521, -- Telaari Hunting Girdle
+    27529, -- Figuine of the Colossus
+    -- Warbringer O'mrogg
+    27524, -- Firemaul of Destruction
+    27868, -- Runesong Dagger
+    27526, -- Skyfire Hawk-Bow
+    29263, -- Forestheart Bracers
+    27522, -- World's End Bracers
+    29254, -- Boots of the Righteous Path
+    37525, -- Jeweled Booots of Sanctification
+    -- Warchief Kargath Bladefish
+    27533, -- Demonblood Eviscerator
+    27538, -- Lightsworn Hammer
+    29348, -- The Bladefist
+    27540, -- Nexus Torch
+    27802, -- Tidefury Shoulderguards
+    29255, -- Bands of Rarefied Magic
+    27528, -- Gauntlets of Desolation
+    27535, -- Gauntlets of the Righteous
+    27537, -- Gloves of Oblivion
+    27531, -- Wastewalker Gloves
+    27527, -- Greaves of the Shatterer
 }
