745a981a77f7423b95d888b24100e2eb58b22a66
David Craig <davidcraig1986@gmail.com>
2020-04-13 15:41:35 +0100

    More BT items.

--- src/AdiBags_ByExpansion_TBC/raids/bt.lua
+++ src/AdiBags_ByExpansion_TBC/raids/bt.lua
@@ -34,8 +34,45 @@ AddonTable.bt = {
     32256,  -- Waistwrap of Infinity
     32261,  -- Band of the Abyssal Lord
     -- Shade of Akama
+    32361, -- Blind-Seers Icon
+    32273, -- Amice of Brilliant Light
+    32264, -- Shoulders of the Hidden Predator
+    32270, -- Focused Mana Bindings
+    32279, -- The Seeker's Wristguards
+    32513, -- Wristbands of Divine Influence
+    32278, -- Grips of Silent Justice
+    32275, -- Spiritwalker Gauntlets
+    32276, -- Flashfire Girdle
+    32265, -- Shadow-Walker's Cord
+    32271, -- Kilt of Immortal Nature
+    32263, -- Praetorian's Legguards
+    32268, -- Myrmidon's Treads
+    32266, -- Ring of Deceitful Intent
     -- Teron Gorefiend
+    32348, -- Soul Cleaver
+    32325, -- Rifle of the Stoic Guardian
+    32329, -- Cowl of Benevolence
+    32323, -- Shadowmoon Destroyer's Drape
+    32327, -- Robe of the Shadow Council
+    32324, -- Insidious Bands
+    32328, -- Botanist's Gloves of Growth
+    32280, -- Gauntlets of Enforcement
+    32512, -- Girdle of Lordaeron's Fallen
+    32510, -- Softstep Boots of Tracking
     -- Gurtogg Bloodboil
+    32344, -- Staff of Immaculate Recovery
+    32269, -- Messenger of Fate
+    32343, -- Wand of Prismatic Focus
+    32338, -- Blood-Cursed Shoulderpads
+    32337, -- Shroud of Forgiveness
+    32340, -- Garments of Temperance
+    32334, -- Vest of Mounting Assault
+    32339, -- Belt of Primal Majesty
+    32342, -- Girdle of Might Resolve
+    32333, -- Girdle of Stability
+    32341, -- Leggings of Divine Retribution
+    32335, -- Unstoppable Aggressor's Ring
+    32501, -- Shadowmoon Insignia
     -- Reliquary of Souls
     -- Mother Shahraz
     -- The Illidari Council
