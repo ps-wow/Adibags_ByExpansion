c7bce2fa5096a21637497231996e08635e67db2d
David Craig <davidcraig1986@gmail.com>
2020-04-13 15:20:56 +0100

    Sethekk Halls

--- build/AdiBags_ByExpansion_TBC/dungeons/sethekk.lua
+++ build/AdiBags_ByExpansion_TBC/dungeons/sethekk.lua
@@ -1,4 +1,24 @@
 local AddonName, AddonTable = ...
 
 AddonTable.sethekk = {
+    -- Darkweaver Syth
+    27918,
+    27919,
+    27914,
+    27915,
+    -- Talon King Ikiss
+    27986,
+    29355,
+    27980,
+    27776,
+    32073,
+    27946,
+    27981,
+    29249,
+    29259,
+    27985,
+    27875,
+    27838,
+    27948,
+    27925,
 }
--- src/AdiBags_ByExpansion_TBC/dungeons/sethekk.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/sethekk.lua
@@ -1,4 +1,24 @@
 local AddonName, AddonTable = ...
 
 AddonTable.sethekk = {
+    -- Darkweaver Syth
+    27918, -- Bands of Syth
+    27919, -- Light-Woven Slippers
+    27914, -- Moonstrider Boots
+    27915, -- Sky-Hunter Swift Boots
+    -- Talon King Ikiss
+    27986, -- Crow Wing Reaper
+    29355, -- Terokk's Shadowstaff
+    27980, -- Terokk's Nightmace
+    27776, -- Shoulderpads of Assassination
+    32073, -- Spaulders of Dementia
+    27946, -- Avian Cloak of Feathers
+    27981, -- Sethekk Oracle Cloak
+    29249, -- Bands of the Benevolent
+    29259, -- Bracers of the Hunt
+    27985, -- Deathforge Girdle
+    27875, -- Hallowed Trousers
+    27838, -- Incanter's Trousers
+    27948, -- Trousers of Oblivion
+    27925, -- Ravenclaw Band
 }
