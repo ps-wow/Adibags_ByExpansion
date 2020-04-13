4d8820d9474b19f20fc816b6ca5b2026abb7c055
David Craig <davidcraig1986@gmail.com>
2020-04-13 15:33:05 +0100

    Old Hillsbrad.

--- src/AdiBags_ByExpansion_TBC/dungeons/hillsbrad.lua
+++ src/AdiBags_ByExpansion_TBC/dungeons/hillsbrad.lua
@@ -1,4 +1,50 @@
 local AddonName, AddonTable = ...
 
 AddonTable.hillsbrad = {
+    -- Lieutenant Drake
+    28210, -- Bloodskull Destroyer
+    28213, -- Lodaeron Medical Guide
+    28215, -- Mok'Nathal Mask of Battle
+    27417, -- Ravenwing Pauldrons
+    27423, -- Cloak of Impulsiveness
+    28214, -- Grips of the Lunar Eclipse
+    28212, -- Aran's Sorceerous Slacks
+    27418, -- Stormreaver Shadow-Kilt
+    27420, -- Uther's Ceremonial Warboots
+    27436, -- Iron Band of the Unbreakable
+    28211, -- Lieutenant's Signet of Lordaeron
+    -- Captain Skarloc
+    27424, -- Amani Venom Axe
+    28216, -- Dathrohan's Ceremonial Hammer
+    27426, -- Northshire Battlemace
+    28220, -- Moon-Crown Antlers
+    28217, -- Tarren Mill Vitality Locket
+    27427, -- Durotan's Battle Harness
+    27428, -- Stormfront Gauntlets
+    28219, -- Emerald-Scale Greaves
+    28218, -- Pontiff's Pantaloons of Prophecy
+    27430, -- Scaled Greaves of Patience
+    28221, -- Boots of the Watchful Heart
+    -- Epoch Hunter
+    28222, -- Reaver of the Infinites
+    27431, -- Time-Shifted Dagger
+    28226, -- Timeslicer
+    28225, -- Doomplate Warhelm
+    28224, -- Wastewalker Helm
+    27440, -- Diamond Prism of Recurrence
+    28233, -- Necklace of Resplendent Hope
+    27434, -- Mantle of Perenolde
+    27433, -- Pauldrons of Sufferance
+    28344, -- Wyrmfury Pauldrons
+    28401, -- Hauberk of Desolation
+    28191, -- Mana-Etched Vestments
+    29246, -- Nightfall Wristguards
+    29250, -- Cord of Sanctification
+    27911, -- Epoch's Whispering Cinch
+    30536, -- Greaves of the Martyr
+    30534, -- Wyrmscale Greaves
+    27432, -- Broxigar's Ring of Valor
+    27904, -- Resounding Ring of Glory
+    28227, -- Sparking Arcanite Ring
+    28223, -- Arcanist's Stone
 }
