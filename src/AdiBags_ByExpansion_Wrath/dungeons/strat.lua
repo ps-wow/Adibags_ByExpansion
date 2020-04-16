89304c45a94cad51e92e318dea705a2eb0fa658e
David Craig <davidcraig1986@gmail.com>
2020-04-16 20:28:51 +0100

    Culling of Stratholme.

--- src/AdiBags_ByExpansion_Wrath/dungeons/strat.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/strat.lua
@@ -1,5 +1,50 @@
 local AddonName, AddonTable = ...
 
 AddonTable.strat = {
-
+    -- Meathook
+    37079, -- enchanted-wire-stitching
+    37081, -- meathooks-slicer
+    37082, -- slaughterhouse-sabatons
+    37083, -- kilt-of-sewn-flesh
+    37679, -- spaulders-of-the-abomination
+    37680, -- belt-of-unified-souls
+    37675, -- legplates-of-steel-implants
+    37678, -- bile-cured-gloves
+    -- Salramm the Fleshcrafter
+    37684,  -- forgotten-shadow-hood
+    37682,  -- bindings-of-dark-will
+    37683,  -- necromancers-amulet
+    37681,  -- gavel-of-the-fleshcrafter
+    157563, -- freshly-sewn-leggings
+    37084,  -- flowing-cloak-of-command
+    37086,  -- tome-of-salramm
+    37095,  -- waistband-of-the-thuzadin
+    37088,  -- spiked-metal-cilice
+    -- Chrono-Lord Epoch
+    37686,  -- cracked-epoch-grasps
+    37685,  -- mobius-band
+    37687,  -- gloves-of-distorted-time
+    37688,  -- legplates-of-the-infinite-drakonid
+    37106,  -- ouroboros-belt
+    37099,  -- sempiternal-staff
+    37096,  -- necklace-of-the-chrono-lord
+    37105,  -- treads-of-altered-history
+    -- Mal'Ganis
+    37694,  -- band-of-guile
+    37112,  -- beguiling-scepter
+    37113,  -- demonic-fabric-bands
+    37109,  -- discarded-silver-hand-spaulders
+    37108,  -- dreadlords-blade
+    37110,  -- gauntlets-of-dark-conversion
+    37114,  -- gloves-of-northern-lordaeron
+    37693,  -- greed
+    37107,  -- leekas-shield
+    37695,  -- legguards-of-natures-power
+    37691,  -- mantle-of-deceit
+    37690,  -- pauldrons-of-destiny
+    37689,  -- pendant-of-the-nathrezim
+    37692,  -- pierces-pistol
+    37696,  -- plague-infected-bracers
+    43085,  -- royal-crest-of-lordaeron
+    37111,  -- soul-preserver
 }
