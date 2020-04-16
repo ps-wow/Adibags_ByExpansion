97a876a7419df14a55196b8b923ac526c06a1d71
David Craig <davidcraig1986@gmail.com>
2020-04-16 22:20:47 +0100

    Obsidium Sanctum (10 + 25).

--- src/AdiBags_ByExpansion_Wrath/raids/os.lua
+++ src/AdiBags_ByExpansion_Wrath/raids/os.lua
@@ -1,5 +1,52 @@
 local AddonName, AddonTable = ...
 
+-- The Obsidian Sanctum
+-- https://www.wowhead.com/the-obsidian-sanctum
 AddonTable.os = {
-
+    -- All Difficulties
+    43345, -- Dragon Hide Bag
+    -- Sartharion (10)
+    43986, -- reins-of-the-black-drake [Mount]
+    40615, -- gloves-of-the-lost-vanquisher
+    40613, -- gloves-of-the-lost-conqueror
+    40614, -- gloves-of-the-lost-protector
+    40427, -- circle-of-arcane-streams
+    40430, -- majestic-dragon-figurine
+    40428, -- titans-outlook
+    43990, -- blade-scarred-tunic
+    43998, -- chestguard-of-flagrant-prowess
+    43993, -- greatring-of-collision
+    43991, -- legguards-of-composure
+    43989, -- remembrance-girdle
+    43996, -- sabatons-of-firmament
+    40426, -- signet-of-the-accord
+    43992, -- volitant-amulet
+    43994, -- belabored-legplates
+    43988, -- gale-proof-cloak
+    43995, -- enamored-cowl
+    -- Sartharion (25)
+    43954, -- reins-of-the-twilight-drake [Mount]
+    40630, -- gauntlets-of-the-lost-vanquisher
+    40628, -- gauntlets-of-the-lost-conqueror
+    40629, -- gauntlets-of-the-lost-protector
+    44004, -- bountiful-gauntlets
+    44003, -- upstanding-spaulders
+    40431, -- fury-of-the-five-flights
+    44000, -- dragonstorm-breastplate
+    40432, -- illustration-of-the-dragon-soul
+    40455, -- staff-of-restraint
+    44007, -- headpiece-of-reconciliation
+    40453, -- chestplate-of-the-great-aspects
+    40437, -- concealment-shoulderpads
+    40439, -- mantle-of-the-eternal-sentinel
+    40446, -- dragon-brood-legguards
+    44006, -- obsidian-greathelm
+    44002, -- the-sanctums-flowing-vestments
+    40433, -- wyrmrest-band
+    40451, -- hyaline-helm-of-the-sniper
+    44005, -- pennant-cloak
+    40438, -- council-chamber-epaulets
+    44011, -- leggings-of-the-honored
+    44008, -- unsullied-cuffs
+    43999, -- ring-of-the-empty-horizon
 }
