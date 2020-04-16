862860ad74fe2b5b2a19a36e70fc9a2fa33ab60c
David Craig <davidcraig1986@gmail.com>
2020-04-16 20:38:12 +0100

    Pit of Saron.

--- src/AdiBags_ByExpansion_Wrath/dungeons/pit.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/pit.lua
@@ -1,5 +1,55 @@
 local AddonName, AddonTable = ...
 
 AddonTable.pit = {
-
+    -- Forgemaster Garfrost
+    50234, -- shoulderplates-of-frozen-blood
+    50227, -- surgeons-needle
+    50230, -- malykriss-vambraces
+    50233, -- spurned-valkyr-shoulderguards
+    50228, -- barbed-ymirheim-choker
+    50229, -- legguards-of-the-frosty-depths
+    49803, -- ring-of-carnelian-and-bone
+    49805, -- ice-steeped-sandals
+    49804, -- polished-mirror-helm
+    49806, -- flayers-black-belt
+    49802, -- garfrosts-two-ton-hammer
+    49801, -- unspeakable-secret
+    -- Ick & Krick
+    50266, -- ancient-polar-bear-hide
+    49808, -- bent-gold-belt
+    49811, -- black-dragonskin-breeches
+    50265, -- blackened-ghoul-skin-leggings
+    50263, -- braid-of-salt-and-fire
+    50264, -- chewed-leather-wristguards
+    50262, -- felglacier-bolter
+    50235, -- icks-rotting-thumb
+    49807, -- kricks-beetle-stabber
+    49812, -- purloined-wedding-ring
+    49810, -- scabrous-zombie-leather-belt
+    49809, -- wristguards-of-subterranean-moss
+    -- Scourgelord Tyrannus
+    49813, -- rimebane-rifle
+    49821, -- protector-of-frigid-souls
+    49822, -- rimewoven-silks
+    49826, -- shroud-of-rime
+    49824, -- horns-of-the-spurned-valkyr
+    49818, -- painfully-sharp-choker
+    49825, -- palebone-robes
+    49816, -- scourgelords-frigid-chestplate
+    49817, -- shaggy-wyrmleather-leggings
+    49820, -- gondrias-spectral-bracer
+    49819, -- skeleton-lords-cranium
+    49823, -- cloak-of-the-fallen-cardinal
+    50271, -- band-of-stained-souls
+    50267, -- tyrannical-beheader
+    50273, -- engraved-gargoyle-femur
+    50285, -- icebound-bronze-cuirass
+    50272, -- frost-wyrm-ribcage
+    50259, -- nevermelting-ice-crystal
+    50268, -- rimefangs-claw
+    50283, -- mudslide-boots
+    50286, -- prelates-snowshoes
+    50284, -- rusty-frozen-fingerguards
+    50270, -- belt-of-rotted-fingernails
+    50269, -- fleshwerk-leggings
 }
