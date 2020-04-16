7294cb3ae4cef67ca3b8547b6b0043be71d2bc58
David Craig <davidcraig1986@gmail.com>
2020-04-16 20:21:09 +0100

    Dungoen: Trial of the Champion

--- src/AdiBags_ByExpansion_Wrath/dungeons/toc.lua
+++ src/AdiBags_ByExpansion_Wrath/dungeons/toc.lua
@@ -1,5 +1,81 @@
 local AddonName, AddonTable = ...
 
 AddonTable.toc = {
-
+    -- Shared Loot
+    47213, -- abyssal-rune
+    47181, -- belt-of-the-churning-blaze
+    47185, -- leggings-of-the-haggard-apprentice
+    47177, -- gloves-of-the-argent-fanatic
+    47176, -- breastplate-of-the-imperial-joust
+    47178, -- carapace-of-grim-visions
+    47497, -- helm-of-the-crestfallen-challenger
+    47495, -- legplates-of-relentless-onslaught
+    47496, -- armbands-of-the-wary-lookout
+    47500, -- Peacekeeper Blade
+    -- Grand Champions
+    47172, -- helm-of-the-bested-gallant
+    47171, -- legguards-of-abandoned-fealty
+    47175, -- scale-boots-of-the-outlander
+    47173, -- bindings-of-the-wicked
+    47493, -- edge-of-ruin
+    47243, -- mark-of-the-relentless
+    47170, -- belt-of-fierce-competition
+    47174, -- binding-of-the-tranquil-glade
+    47250, -- pauldrons-of-the-deafening-gale
+    47244, -- chestguard-of-the-ravenous-fiend
+    47248, -- treads-of-dismal-fortune
+    47249, -- leggings-of-the-snowy-bramble
+    -- Eadric the Pure
+    47508, -- Aledar's Battlestar
+    47509, -- Mariel's Sorrow
+    47199, -- Greaves of the Grand Paladin
+    47197, -- Gauntlets of the Stoutheared Crusader
+    47201, -- boots-of-heartfelt-repentance
+    47200, -- signet-of-purity
+    47202, -- leggings-of-brazen-trespass
+    47210, -- mantle-of-gnarled-overgrowth
+    47503, -- legplates-of-the-argent-armistice
+    47502, -- majestic-silversmith-shoulderplates
+    47504, -- barkhide-treads
+    -- Argent Confessor Paletress
+    47214, -- banner-of-victory
+    47217, -- gaze-of-the-somber-keeper
+    47218, -- the-confessors-binding
+    47211, -- wristguards-of-ceaseless-regret
+    47212, -- mercys-hold
+    47219, -- brilliant-hailstone-amulet
+    47494, -- ancient-pendant-of-arathor
+    47511, -- plated-greaves-of-providence
+    47514, -- regal-aurous-shoulderplates
+    47510, -- trueheart-girdle
+    47498, -- gloves-of-dismal-fortune
+    47245, -- pauldrons-of-concealed-loathing
+    47522, -- marrowstrike
+    47512, -- sinners-confession
+    -- The Black Knight
+    47227, -- girdle-of-the-pallid-knight
+    47228, -- leggings-of-the-bloodless-knight
+    47215, -- tears-of-the-vanquished
+    47229, -- girdle-of-arrogant-downfall
+    47230, -- handwraps-of-surrendered-hope
+    47220, -- helm-of-the-violent-fray
+    47216, -- the-black-heart
+    47222, -- urukas-band-of-zeal
+    47231, -- belt-of-merciless-cruelty
+    47232, -- drape-of-the-undefeated
+    47221, -- shoulderpads-of-the-infamous-knave
+    47226, -- mantle-of-inconsolable-fear
+    47567, -- gauntlets-of-revelation
+    47529, -- mask-of-distant-memory
+    47569, -- spectral-kris
+    47566, -- the-warlords-depravity
+    47568, -- true-aim-long-rifle
+    47565, -- vambraces-of-unholy-command
+    49682, -- black-knights-rondel
+    47564, -- gaze-of-the-unknown
+    47563, -- girdle-of-the-dauntless-conqueror
+    47562, -- symbol-of-redemption
+    47560, -- boots-of-the-crackling-flame
+    47527, -- embrace-of-madness
+    47561, -- gloves-of-the-dark-exile
 }
