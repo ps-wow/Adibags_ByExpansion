8fafcb8307172434c29048a9b6ccfb6635a93103
David Craig <davidcraig1986@gmail.com>
2020-04-16 22:08:02 +0100

    VoA - Toravon the Ice Watcher - 25 Man

--- src/AdiBags_ByExpansion_Wrath/raids/archavon.lua
+++ src/AdiBags_ByExpansion_Wrath/raids/archavon.lua
@@ -1,5 +1,138 @@
 local AddonName, AddonTable = ...
 
 AddonTable.archavon = {
+    -- Archavon the Stone Watcher (10)
 
+    -- Archavon the Stone Watcher (25)
+
+    -- Emalon the Storm Watcher (10)
+
+    -- Emalon the Storm Watcher (25)
+
+    -- Koralon the Flame Watcher (10)
+
+    -- Koralon the Flame Watcher (25)
+
+    -- Toravon the Ice Watcher (10)
+
+    -- Toravon the Ice Watcher (25)
+    51154, -- sanctified-ahnkahar-blood-hunters-handguards
+    51186, -- sanctified-shadowblade-legplates
+    51159, -- sanctified-bloodmage-gloves
+    51157, -- sanctified-bloodmage-leggings
+    51179, -- sanctified-crimson-acolyte-gloves
+    51209, -- sanctified-dark-coven-gloves
+    51207, -- sanctified-dark-coven-leggings
+    51152, -- sanctified-ahnkahar-blood-hunters-legguards
+    51188, -- sanctified-shadowblade-gauntlets
+    51544, -- wrathful-gladiators-plate-legguards
+    51128, -- sanctified-scourgelord-gauntlets
+    51132, -- sanctified-scourgelord-handguards
+    51368, -- wrathful-gladiators-belt-of-triumph
+    51459, -- wrathful-gladiators-chain-gauntlets
+    51426, -- wrathful-gladiators-dragonhide-gloves
+    51416, -- wrathful-gladiators-dreadplate-legguards
+    51537, -- wrathful-gladiators-felweave-handguards
+    51542, -- wrathful-gladiators-plate-gauntlets
+    51466, -- wrathful-gladiators-silk-trousers
+    51183, -- sanctified-crimson-acolyte-handwraps
+    51181, -- sanctified-crimson-acolyte-pants
+    51196, -- sanctified-frost-witchs-grips
+    51148, -- sanctified-lasherweave-gloves
+    51144, -- sanctified-lasherweave-handgrips
+    51171, -- sanctified-lightsworn-legguards
+    51131, -- sanctified-scourgelord-legguards
+    51126, -- sanctified-scourgelord-legplates
+    51213, -- sanctified-ymirjar-lords-gauntlets
+    51217, -- sanctified-ymirjar-lords-handguards
+    51216, -- sanctified-ymirjar-lords-legguards
+    51211, -- sanctified-ymirjar-lords-legplates
+    51461, -- wrathful-gladiators-chain-leggings
+    51365, -- wrathful-gladiators-cord-of-salvation
+    51414, -- wrathful-gladiators-dreadplate-gauntlets
+    51359, -- wrathful-gladiators-girdle-of-salvation
+    51363, -- wrathful-gladiators-greaves-of-triumph
+    51493, -- wrathful-gladiators-leather-gloves
+    51495, -- wrathful-gladiators-leather-legguards
+    51355, -- wrathful-gladiators-pendant-of-triumph
+    51351, -- wrathful-gladiators-sabatons-of-triumph
+    51464, -- wrathful-gladiators-silk-handguards
+    51328, -- wrathful-gladiators-treads-of-dominance
+    51350, -- wrathful-gladiators-waistguard-of-triumph
+    51177, -- sanctified-crimson-acolyte-leggings
+    51201, -- sanctified-frost-witchs-gloves
+    51138, -- sanctified-lasherweave-gauntlets
+    51136, -- sanctified-lasherweave-legplates
+    51146, -- sanctified-lasherweave-trousers
+    51169, -- sanctified-lightsworn-gloves
+    51168, -- sanctified-lightsworn-greaves
+    51370, -- wrathful-gladiators-armwraps-of-triumph
+    51336, -- wrathful-gladiators-band-of-dominance
+    51361, -- wrathful-gladiators-bracers-of-salvation
+    51367, -- wrathful-gladiators-cuffs-of-salvation
+    51539, -- wrathful-gladiators-felweave-trousers
+    51362, -- wrathful-gladiators-girdle-of-triumph
+    51360, -- wrathful-gladiators-greaves-of-salvation
+    51471, -- wrathful-gladiators-ornamented-legplates
+    51333, -- wrathful-gladiators-pendant-of-subjugation
+    51366, -- wrathful-gladiators-treads-of-salvation
+    51436, -- wrathful-gladiators-wyrmhide-legguards
+    51198, -- sanctified-frost-witchs-war-kilt
+    51327, -- wrathful-gladiators-cord-of-dominance
+    51329, -- wrathful-gladiators-cuffs-of-dominance
+    51477, -- wrathful-gladiators-scaled-legguards
+    51352, -- wrathful-gladiators-wristguards-of-triumph
+    51163, -- sanctified-lightsworn-gauntlets
+    51369, -- wrathful-gladiators-boots-of-triumph
+    51357, -- wrathful-gladiators-pendant-of-victory
+    51500, -- wrathful-gladiators-ringmail-leggings
+    51372, -- wrathful-gladiators-sabatons-of-salvation
+    51191, -- sanctified-frost-witchs-handguards
+    51203, -- sanctified-frost-witchs-kilt
+    51354, -- wrathful-gladiators-cloak-of-triumph
+    51353, -- wrathful-gladiators-pendant-of-sundering
+    51193, -- sanctified-frost-witchs-legguards
+    51161, -- sanctified-lightsworn-legplates
+    51356, -- wrathful-gladiators-cloak-of-victory
+    51483, -- wrathful-gladiators-mooncloth-gloves
+    51485, -- wrathful-gladiators-mooncloth-leggings
+    51488, -- wrathful-gladiators-satin-gloves
+    51475, -- wrathful-gladiators-scaled-gauntlets
+    51374, -- wrathful-gladiators-waistguard-of-dominance
+    51172, -- sanctified-lightsworn-handguards
+    51364, -- wrathful-gladiators-bracers-of-triumph
+    51422, -- wrathful-gladiators-kodohide-legguards
+    51510, -- wrathful-gladiators-mail-gauntlets
+    51469, -- wrathful-gladiators-ornamented-gloves
+    51142, -- sanctified-lasherweave-legguards
+    51358, -- wrathful-gladiators-band-of-triumph
+    51341, -- wrathful-gladiators-boots-of-salvation
+    51344, -- wrathful-gladiators-boots-of-dominance
+    51330, -- wrathful-gladiators-cloak-of-dominance
+    51331, -- wrathful-gladiators-pendant-of-dominance
+    51371, -- wrathful-gladiators-waistguard-of-salvation
+    51376, -- wrathful-gladiators-wristguards-of-dominance
+    51506, -- wrathful-gladiators-linked-leggings
+    51490, -- wrathful-gladiators-satin-leggings
+    51340, -- wrathful-gladiators-belt-of-salvation
+    51334, -- wrathful-gladiators-cloak-of-ascendancy
+    51420, -- wrathful-gladiators-kodohide-gloves
+    51504, -- wrathful-gladiators-linked-gauntlets
+    51375, -- wrathful-gladiators-sabatons-of-dominance
+    51345, -- wrathful-gladiators-armwraps-of-dominance
+    51342, -- wrathful-gladiators-armwraps-of-salvation
+    51343, -- wrathful-gladiators-belt-of-dominance
+    51349, -- wrathful-gladiators-pendant-of-deliverance
+    51498, -- wrathful-gladiators-ringmail-gauntlets
+    44083, -- reins-of-the-grand-black-war-mammoth [Mount] [Horde]
+    43959, -- reins-of-the-grand-black-war-mammoth [Mount] [Alliance]
+    51348, -- wrathful-gladiators-cloak-of-deliverance
+    51346, -- wrathful-gladiators-cloak-of-salvation
+    51347, -- wrathful-gladiators-pendant-of-salvation
+    51428, -- wrathful-gladiators-dragonhide-legguards
+    51512, -- wrathful-gladiators-mail-leggings
+    51373, -- wrathful-gladiators-wristguards-of-salvation
+    51335, -- wrathful-gladiators-pendant-of-ascendancy
+    51434, -- wrathful-gladiators-wyrmhide-gloves
+    51332, -- wrathful-gladiators-cloak-of-subjugation
 }
