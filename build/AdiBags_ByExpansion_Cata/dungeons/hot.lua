68a2ee5489f31067f29ede642de05061ffc99ae7
David Craig <davidcraig1986@gmail.com>
2020-04-17 20:58:51 +0100

    Hour of Twilight

--- build/AdiBags_ByExpansion_Cata/dungeons/hot.lua
+++ build/AdiBags_ByExpansion_Cata/dungeons/hot.lua
@@ -1,4 +1,33 @@
 local AddonName, AddonTable = ...
 
+-- Hour of Twilight (Heroic)
 AddonTable.hot = {
+    -- Arcurion
+    72853,
+    72851,
+    72849,
+    72854,
+    76150,
+    72850,
+    -- Asira Dawnslayer
+    76151,
+    72859,
+    72860,
+    72855,
+    72857,
+    72856,
+    157616,
+    -- Archbishop Benedictus
+    72862,
+    72863,
+    72868,
+    72865,
+    72870,
+    72901,
+    72869,
+    72866,
+    72867,
+    72861,
+    72864,
+    72900,
 }
--- src/AdiBags_ByExpansion_Cata/dungeons/hot.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/hot.lua
@@ -1,4 +1,33 @@
 local AddonName, AddonTable = ...
 
+-- Hour of Twilight (Heroic)
 AddonTable.hot = {
+    -- Arcurion
+    72853,  -- arcurion-legguards
+    72851,  -- chillbane-belt
+    72849,  -- wayfinder-boots
+    72854,  -- iceward-cloak
+    76150,  -- evergreen-wristbands
+    72850,  -- surestride-boots
+    -- Asira Dawnslayer
+    76151,  -- cloak-of-subtle-light
+    72859,  -- dawnslayer-helm
+    72860,  -- mandible-of-the-old-ones
+    72855,  -- corrupted-carapace
+    72857,  -- leggings-of-blinding-speed
+    72856,  -- pauldrons-of-midnight-whispers
+    157616, -- chestguard-of-futility
+    -- Archbishop Benedictus
+    72862,  -- fanged-tentacle
+    72863,  -- stalk-of-corruption
+    72868,  -- desecrated-shoulderguards
+    72865,  -- mantle-of-false-virtue
+    72870,  -- betrayers-pauldrons
+    72901,  -- rosary-of-light
+    72869,  -- dragonsmaw-blaster
+    72866,  -- treacherys-bite
+    72867,  -- clattering-claw
+    72861,  -- pauldrons-of-the-dragonblight
+    72864,  -- pauldrons-of-conviction
+    72900,  -- veil-of-lies
 }
