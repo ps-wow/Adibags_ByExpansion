1858adda16fe4f25bdea7b326cd4b8ea70cad0ba
David Craig <davidcraig1986@gmail.com>
2020-04-17 20:32:50 +0100

    Well of Eternity.

--- build/AdiBags_ByExpansion_Cata/dungeons/well.lua
+++ build/AdiBags_ByExpansion_Cata/dungeons/well.lua
@@ -1,4 +1,32 @@
 local AddonName, AddonTable = ...
 
+-- Well of Eternity (Heroic)
 AddonTable.well = {
+    -- Peroth'arn
+    72832,
+    72831,
+    72830,
+    72827,
+    72829,
+    72828,
+    -- Queen Azshara
+    72835,
+    72833,
+    72838,
+    72836,
+    72834,
+    72837,
+    -- Mannoroth <The Destructor> / Captain Varo'then <The Hand of Azshara>
+    72898,
+    72844,
+    72839,
+    72845,
+    72841,
+    72848,
+    72846,
+    72899,
+    72843,
+    72847,
+    72840,
+    72842,
 }
--- src/AdiBags_ByExpansion_Cata/dungeons/well.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/well.lua
@@ -1,4 +1,32 @@
 local AddonName, AddonTable = ...
 
+-- Well of Eternity (Heroic)
 AddonTable.well = {
+    -- Peroth'arn
+    72832, -- girdle-of-the-queens-champion
+    72831, -- horned-band
+    72830, -- perotharns-belt
+    72827, -- gavel-of-perotharn
+    72829, -- orb-of-the-first-satyrs
+    72828, -- tricksters-edge
+    -- Queen Azshara
+    72835, -- puppeteers-pantaloons
+    72833, -- scepter-of-azshara
+    72838, -- cloak-of-the-royal-protector
+    72836, -- slippers-of-wizardry
+    72834, -- breastplate-of-the-queens-guard
+    72837, -- queens-boon
+    -- Mannoroth <The Destructor> / Captain Varo'then <The Hand of Azshara>
+    72898, -- foul-gift-of-the-demon-lord
+    72844, -- pit-lords-destroyer
+    72839, -- cowl-of-highborne-sorcerors
+    72845, -- mannoroths-signet
+    72841, -- demonsbane-chestguard
+    72848, -- legguards-of-the-legion
+    72846, -- thornwood-staff
+    72899, -- varothens-brooch
+    72843, -- helm-of-power
+    72847, -- helm-of-thorns
+    72840, -- spaulders-of-eternity
+    72842, -- annihilan-helm
 }
