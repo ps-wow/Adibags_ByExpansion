2a5fe6a64b3a4e0fa0622b36d53feacd17829d97
David Craig <davidcraig1986@gmail.com>
2020-04-17 20:48:43 +0100

    Throne of the Tides (All difficulties).

--- src/AdiBags_ByExpansion_Cata/dungeons/throne.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/throne.lua
@@ -1,4 +1,80 @@
 local AddonName, AddonTable = ...
 
+-- Throne of the Tides
 AddonTable.throne = {
+    -- Lady Naz'jar (N/HC)
+    55202,  -- periwinkle-cloak
+    55203,  -- lightning-whelk-axe
+    55195,  -- wrasse-handwraps
+    55198,  -- aurelian-miter
+    55201,  -- entwined-nereis
+    56270,  -- entwined-nereis
+    56267,  -- periwinkle-cloak
+    56269,  -- aurelian-miter
+    157588, -- ironshell-pendant
+    157587, -- old-one-eyes-cowl
+    56268,  -- wrasse-handwraps
+    56266,  -- lightning-whelk-axe
+    -- Lady Naz'jar (TW)
+    133180, -- periwinkle-cloak
+    133183, -- entwined-nereis
+    133182, -- aurelian-miter
+    133358, -- old-one-eyes-cowl
+    133205, -- alpheus-legguards
+    133181, -- wrasse-handwraps
+    133359, -- ironshell-pendant
+    133367, -- barnacled-shell-buckler
+    133179, -- lightning-whelk-axe
+    -- Ozumat (N/HC)
+    56288,  -- pipefish-cord
+    56283,  -- triton-legplates
+    56291,  -- abalone-plate-armor
+    56282,  -- nautilus-ring
+    56281,  -- wentletrap-vest
+    157589, -- salty-shell-studded-girdle
+    56289,  -- bioluminescent-lamp
+    56290,  -- sea-star
+    56284,  -- whitefin-axe
+    56285,  -- might-of-the-ocean
+    55252,  -- whitefin-axe
+    55258,  -- pipefish-cord
+    55251,  -- might-of-the-ocean
+    55250,  -- nautilus-ring
+    55259,  -- bioluminescent-lamp
+    55256,  -- sea-star
+    55255,  -- mnemiopsis-gloves
+    55253,  -- wentletrap-vest
+    55254,  -- abalone-plate-armor
+    55249,  -- triton-legplates
+    -- Ozumat (TW)
+    133194, -- nautilus-ring
+    133201, -- sea-star
+    133199, -- pipefish-cord
+    133368, -- salty-shell-studded-girdle
+    133193, -- wentletrap-vest
+    133202, -- abalone-plate-armor
+    133195, -- triton-legplates
+    133198, -- mnemiopsis-gloves
+    133196, -- whitefin-axe
+    133197, -- might-of-the-ocean
+    -- Commander Ulthok (N)
+    55228, -- cerith-spire-staff
+    55204, -- caridean-epaulettes
+    55206, -- eagle-ray-cloak
+    55205, -- chromis-chestpiece
+    55207, -- harp-shell-pauldrons
+    -- Commander Ulthok (HC)
+    56273, -- caridean-epaulettes
+    56271, -- cerith-spire-staff
+    56274, -- chromis-chestpiece
+    56275, -- eagle-ray-cloak
+    56272, -- harp-shell-pauldrons
+    -- Commander Ulthok (TW)
+    133188, -- eagle-ray-cloak
+    133203, -- barnacle-pendant
+    133185, -- harp-shell-pauldrons
+    133186, -- caridean-epaulettes
+    133184, -- cerith-spire-staff
+    133187, -- chromis-chestpiece
+    133204, -- ring-of-the-great-whale
 }
