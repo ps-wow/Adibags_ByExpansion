c0504ec69ef75f431ebdf27b6c41ffb1d0372cb3
David Craig <davidcraig1986@gmail.com>
2020-04-18 14:43:20 +0100

    Temple of the Jade Serpent

--- src/AdiBags_ByExpansion_MoP/dungeons/jadeserpent.lua
+++ src/AdiBags_ByExpansion_MoP/dungeons/jadeserpent.lua
@@ -1,5 +1,68 @@
 local AddonName, AddonTable = ...
 
+-- Temple of the Jade Serpent
 AddonTable.jadeserpent = {
-    144081, -- Girdle of Endemic Anger
+    -- Wise Mari
+    80861,  -- riverbed-chestguard
+    80862,  -- treads-of-corrupted-water
+    80860,  -- waterburst-helm
+    143960, -- riverbed-chestguard
+    81124,  -- crystallized-droplet
+    81083,  -- riverbed-chestguard
+    81075,  -- waterburst-helm
+    81123,  -- wind-soaked-drape
+    81072,  -- treads-of-corrupted-water
+    144112, -- crystallized-droplet
+    143973, -- treads-of-corrupted-water
+    144111, -- wind-soaked-drape
+    144017, -- waterburst-helm
+    -- Lorewalker Stonestep
+    143987, -- leggings-of-whispered-dreams
+    143974, -- stonestep-boots
+    144083, -- sunheart-waistband
+    144113, -- windswept-pages
+    144081, -- girdle-of-endemic-anger
+    -- Liu Flameheart
+    80866,  -- cape-of-entanglement
+    80872,  -- firebelcher-hand-cannon
+    80867,  -- serpentstrike-shoulderpads
+    144114, -- signet-of-dancing-jade
+    144094, -- cape-of-entanglement
+    144090, -- firebelcher-hand-cannon
+    143988, -- flameheart-sandals
+    143971, -- serpentstrike-shoulderpads
+    81084,  -- cape-of-entanglement
+    81067,  -- firebelcher-hand-cannon
+    81127,  -- flameheart-sandals
+    81070,  -- serpentstrike-shoulderpads
+    81128,  -- signet-of-dancing-jade
+    -- Sha of Doubt
+    80870,  -- chestguard-of-despair
+    80868,  -- doubtridden-shoulderguards
+    80873,  -- dubious-handaxe
+    80869,  -- hopecrusher-gauntlets
+    80871,  -- neverdare-shoulders
+    80874,  -- staff-of-trembling-will
+    144117, -- mindbreaker-pendant
+    144116, -- binding-of-broken-dreams
+    144115, -- cloak-of-failing-will
+    144086, -- dubious-handaxe
+    144091, -- hopecrusher-gauntlets
+    143970, -- neverdare-shoulders
+    143959, -- chestguard-of-despair
+    144118, -- paralyzing-gloves
+    144093, -- staff-of-trembling-will
+    143972, -- doubtridden-shoulderguards
+    144216, -- jelyu-spirit-of-the-serpent
+    81130,  -- binding-of-broken-dreams
+    81082,  -- chestguard-of-despair
+    81129,  -- cloak-of-failing-will
+    81074,  -- hopecrusher-gauntlets
+    81131,  -- mindbreaker-pendant
+    81132,  -- paralyzing-gloves
+    81079,  -- staff-of-trembling-will
+    81071,  -- doubtridden-shoulderguards
+    81063,  -- dubious-handaxe
+    81069,  -- neverdare-shoulders
+    87544,  -- jelyu-spirit-of-the-serpent
 }
