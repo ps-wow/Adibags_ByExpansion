286f557b69c9afd0e74d8e38a40bd123a23c9663
David Craig <davidcraig1986@gmail.com>
2020-04-18 20:40:49 +0100

    Shado-Pan Monestary

--- src/AdiBags_ByExpansion_MoP/dungeons/shadopan.lua
+++ src/AdiBags_ByExpansion_MoP/dungeons/shadopan.lua
@@ -1,4 +1,97 @@
 local AddonName, AddonTable = ...
 
 AddonTable.shadopan = {
+    -- Gu Cloudstrike
+    80909,  -- azure-serpent-chestguard
+    80910,  -- leggings-of-the-charging-soul
+    80908,  -- sparkbreath-girdle
+    81110,  -- azure-serpent-chestguard
+    81180,  -- cloudstrike-pendant
+    81092,  -- leggings-of-the-charging-soul
+    81086,  -- sparkbreath-girdle
+    81179,  -- star-summoner-bracers
+    144127, -- cloudstrike-pendant
+    144096, -- sparkbreath-girdle
+    144126, -- star-summoner-bracers
+    143961, -- azure-serpent-chestguard
+    143978, -- leggings-of-the-charging-soul
+    -- Master Snowdrift
+    144129, -- eye-of-the-tornado
+    144106, -- gauntlets-of-resolute-fury
+    144128, -- heart-of-fire
+    144097, -- quivering-heart-girdle
+    144110, -- snowdrifts-bladed-staff
+    80912,  -- gauntlets-of-resolute-fury
+    80937,  -- snowdrifts-bladed-staff
+    80911,  -- quivering-heart-girdle
+    81182,  -- eye-of-the-tornado
+    81101,  -- gauntlets-of-resolute-fury
+    81181,  -- heart-of-fire
+    81087,  -- quivering-heart-girdle
+    81108,  -- snowdrifts-bladed-staff
+    -- Sha of Violence
+    80883,  -- crescent-of-ichor
+    80913,  -- gloves-of-enraged-slaughter
+    80915,  -- spike-soled-stompers
+    144130, -- necklace-of-disorientation
+    144107, -- gloves-of-enraged-slaughter
+    143985, -- spike-soled-stompers
+    144099, -- crescent-of-ichor
+    144131, -- bladed-smoke-bracers
+    81184,  -- necklace-of-disorientation
+    81185,  -- bladed-smoke-bracers
+    81089,  -- crescent-of-ichor
+    81102,  -- gloves-of-enraged-slaughter
+    81113,  -- spike-soled-stompers
+    -- Taran Zhu
+    143990, -- robes-of-fevered-dreams
+    144132, -- seal-of-hateful-meditation
+    144133, -- ring-of-malice
+    143986, -- blastwalker-footguards
+    143962, -- hateshatter-chestplate
+    144108, -- mindbinder-plate-gloves
+    143981, -- shadowspine-shoulderguards
+    143979, -- darkbinder-leggings
+    144103, -- shield-of-blind-hate
+    144109, -- warmace-of-taran-zhu
+    144215, -- kaeng-breath-of-the-shadow
+    80936,  -- warmace-of-taran-zhu
+    80917,  -- blastwalker-footguards
+    80918,  -- mindbinder-plate-gloves
+    80935,  -- shield-of-blind-hate
+    80919,  -- darkbinder-leggings
+    80916,  -- shadowspine-shoulderguards
+    81187,  -- hateshatter-chestplate
+    81114,  -- blastwalker-footguards
+    81093,  -- darkbinder-leggings
+    81103,  -- mindbinder-plate-gloves
+    81189,  -- ring-of-malice
+    81188,  -- robes-of-fevered-dreams
+    81186,  -- seal-of-hateful-meditation
+    81099,  -- shadowspine-shoulderguards
+    81096,  -- shield-of-blind-hate
+    81107,  -- warmace-of-taran-zhu
+    87543,  -- kaeng-breath-of-the-shadow
+
+    -- Quest Rewards
+    --- Lord of the Shado-Pan
+    87315, -- Cowl of Angry Thoughts
+    87316, -- Headcover of Disturbed Serenity
+    87317, -- Vexing Coif
+    87318, -- Hood of Endless Agitation
+    87319, -- Antagonist's Headwrap
+    87320, -- Headcover of the Impatient
+    87321, -- Cowl of Internal Strife
+    87322, -- Helm of Eternity
+    87323, -- Helm of Unstable Termperament
+    --- The Path to Respect Lies in Violence
+    87324, -- cord-of-sha-savagery
+    87325, -- kun-lai-climbing-belt
+    87326, -- sha-trapper-waistguard
+    87327, -- cloudstriker-cord
+    87328, -- shado-pan-initiates-belt
+    87329, -- elemental-archers-links
+    87330, -- monastery-guardian-waistguard
+    87331, -- girdle-of-the-broken-seal
+    87332, -- cord-of-disturbed-dreams
 }
