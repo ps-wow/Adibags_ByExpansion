f0bd174239d257fba62fcf3a236172f61d1cccf0
David Craig <davidcraig1986@gmail.com>
2020-04-19 12:55:56 +0100

    Stormstout Brewery

--- src/AdiBags_ByExpansion_MoP/dungeons/stormstout.lua
+++ src/AdiBags_ByExpansion_MoP/dungeons/stormstout.lua
@@ -1,4 +1,60 @@
 local AddonName, AddonTable = ...
 
 AddonTable.stormstout = {
+    -- Ook-Ook
+    80892,  -- ooks-hozen-slicer
+    80897,  -- bracers-of-displaced-air
+    80896,  -- nimbletoe-chestguard
+    81134,  -- barreldodger-boots
+    81064,  -- bracers-of-displaced-air
+    81133,  -- empty-fruit-barrel
+    81080,  -- nimbletoe-chestguard
+    81061,  -- ooks-hozen-slicer
+    143957, -- nimbletoe-chestguard
+    143989, -- barreldodger-boots
+    144119, -- empty-fruit-barrel
+    144087, -- bracers-of-displaced-air
+    144084, -- ooks-hozen-slicer
+    -- Hoptallus
+    81135,  -- belt-of-brazen-inebriation
+    81076,  -- bottle-of-potent-potables
+    81065,  -- bubble-breaker-bracers
+    81136,  -- cloak-of-hidden-flasks
+    81077,  -- hopping-mad-leggings
+    80898,  -- hopping-mad-leggings
+    80899,  -- bubble-breaker-bracers
+    80893,  -- bottle-of-potent-potables
+    144121, -- cloak-of-hidden-flasks
+    144092, -- bottle-of-potent-potables
+    144120, -- belt-of-brazen-inebriation
+    144088, -- bubble-breaker-bracers
+    143975, -- hopping-mad-leggings
+    -- Yan-Zhu the Uncasked
+    80894,  -- gaos-keg-tapper
+    80901,  -- sudsy-legplates
+    80902,  -- uncasked-chestguard
+    80903,  -- fermenting-belt
+    80900,  -- fizzy-spaulders
+    80895,  -- yan-zhus-pressure-valve
+    81141,  -- alemental-seal
+    81062,  -- gaos-keg-tapper
+    81138,  -- carbonic-carbuncle
+    81059,  -- fermenting-belt
+    81068,  -- fizzy-spaulders
+    81139,  -- lime-rimmed-signet
+    81078,  -- sudsy-legplates
+    81081,  -- uncasked-chestguard
+    81140,  -- wort-stirring-rod
+    81066,  -- yan-zhus-pressure-valve
+    143958, -- uncasked-chestguard
+    144125, -- alemental-seal
+    144122, -- carbonic-carbuncle
+    144085, -- gaos-keg-tapper
+    144123, -- lime-rimmed-signet
+    144124, -- wort-stirring-rod
+    144082, -- fermenting-belt
+    143969, -- fizzy-spaulders
+    143976, -- sudsy-legplates
+    144089, -- yan-zhus-pressure-valve
+    144217, -- inelava-spirit-of-inebriation
 }
