8259d81ba45a62b0133b3f58a87d41ef55193f6e
David Craig <davidcraig1986@gmail.com>
2020-04-20 19:55:09 +0100

    Shadowfang Keep (Heroic)

--- build/AdiBags_ByExpansion_Cata/dungeons/sfk.lua
+++ build/AdiBags_ByExpansion_Cata/dungeons/sfk.lua
@@ -1,4 +1,40 @@
 local AddonName, AddonTable = ...
 
+-- Shadowfang Keep (Heroic)
 AddonTable.sfk = {
+    -- Baron Ashbury
+    63437,
+    63434,
+    63436,
+    63435,
+    63433,
+    -- Baron Silverlaine
+    63441,
+    63438,
+    63439,
+    63440,
+    63444,
+    -- Commander Springvale
+    63446,
+    63449,
+    63448,
+    63445,
+    63447,
+    -- Lord Walden
+    63452,
+    63453,
+    63450,
+    63455,
+    63454,
+    -- Lord Godfrey
+    63458,
+    63461,
+    63459,
+    63464,
+    63462,
+    63465,
+    63463,
+    63456,
+    63457,
+    157617,
 }
--- src/AdiBags_ByExpansion_Cata/dungeons/sfk.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/sfk.lua
@@ -1,4 +1,40 @@
 local AddonName, AddonTable = ...
 
+-- Shadowfang Keep (Heroic)
 AddonTable.sfk = {
+    -- Baron Ashbury
+    63437,  -- baron-ashburys-cuffs
+    63434,  -- gloves-of-the-greymane-wall
+    63436,  -- traitors-grips
+    63435,  -- boots-of-the-predator
+    63433,  -- robes-of-arugal
+    -- Baron Silverlaine
+    63441,  -- pendant-of-the-keep
+    63438,  -- baroness-silverlaines-locket
+    63439,  -- gloves-of-the-uplifted-cup
+    63440,  -- boots-of-lingering-sorrow
+    63444,  -- baron-silverlaines-greaves
+    -- Commander Springvale
+    63446,  -- haunting-footfalls
+    63449,  -- thieving-spaulders
+    63448,  -- springvales-cloak
+    63445,  -- arced-war-axe
+    63447,  -- breastplate-of-the-stilled-heart
+    -- Lord Walden
+    63452,  -- burden-of-lost-humanity
+    63453,  -- iron-will-girdle
+    63450,  -- phantom-armor
+    63455,  -- blinders-of-the-follower
+    63454,  -- double-dealing-bracers
+    -- Lord Godfrey
+    63458,  -- lord-waldens-breastplate
+    63461,  -- staff-of-isolation
+    63459,  -- worgen-hunters-helm
+    63464,  -- greaves-of-the-misguided
+    63462,  -- helm-of-untold-stories
+    63465,  -- mantle-of-loss
+    63463,  -- mantle-of-the-eastern-lords
+    63456,  -- meteor-shard
+    63457,  -- shackles-of-undeath
+    157617, -- wolfshide-vest
 }
