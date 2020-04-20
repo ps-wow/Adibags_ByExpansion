72c2299e0a335106e60ab892e36f8be027f4083a
David Craig <davidcraig1986@gmail.com>
2020-04-20 20:01:34 +0100

    Deadmines (Heroic)

--- src/AdiBags_ByExpansion_Cata/dungeons/deadmines.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/deadmines.lua
@@ -1,4 +1,44 @@
 local AddonName, AddonTable = ...
 
 AddonTable.deadmines = {
+    -- Glubtok
+    65163,  -- buzzer-blade
+    63468,  -- defias-brotherhood-vest
+    63470,  -- missing-diplomats-pauldrons
+    63467,  -- shadow-of-the-past
+    63471,  -- vest-of-the-curious-visitor
+    157628, -- gold-flecked-gloves
+    -- Helix Gearbreaker
+    63475,  -- old-friends-gloves
+    63473,  -- cloak-of-thredd
+    157752, -- armbands-of-exiled-architects
+    63476,  -- gearbreakers-bindings
+    63474,  -- gear-marked-gauntlets
+    65164,  -- cruel-barb
+    -- Foe Reaper 5000
+    65167,  -- emberstone-staff
+    65166,  -- buzz-saw
+    65165,  -- foe-reaper
+    157755, -- missing-diplomats-pauldrons
+    157753, -- vest-of-the-curious-visitor
+    157754, -- old-friends-gloves
+    -- Admiral Ripsnarl
+    65170,  -- smites-reaver
+    65169,  -- lavishly-jeweled-ring
+    65168,  -- rockslicer
+    -- "Captain" Cookie
+    65171,  -- cookies-tenderizer
+    65174,  -- corsairs-overshirt
+    65173,  -- thiefs-blade
+    65172,  -- cookies-stirring-rod
+    65177,  -- cape-of-the-brotherhood
+    -- Vanessa VanCleef
+    63479,  -- bracers-of-some-consequence
+    63478,  -- stonemasons-helm
+    65178,  -- vancleefs-boots
+    63485,  -- cowl-of-rebellion
+    63482,  -- daughters-hands
+    63483,  -- guildmasters-greaves
+    63486,  -- shackles-of-the-betrayed
+    63484,  -- armbands-of-exiled-architects
 }
