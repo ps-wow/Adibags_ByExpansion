dddedb18c7d0a03369d71fb70152e6d5b99bf95b
David Craig <davidcraig1986@gmail.com>
2020-04-20 19:01:19 +0100

    The Stonecore.

--- src/AdiBags_ByExpansion_Cata/dungeons/stonecore.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/stonecore.lua
@@ -3,5 +3,96 @@ local AddonName, AddonTable = ...
 -- The Stonecore
 -- https://www.wowhead.com/the-stonecore
 AddonTable.stonecore = {
-    56330, -- Cinnabar Shoulders
+    -- Corborus
+    55792,  -- cinnabar-shoulders
+    55793,  -- dolomite-adorned-gloves
+    55794,  -- phosphorescent-ring
+    55795,  -- key-to-the-endless-chamber
+    55796,  -- fist-of-pained-senses
+    56328,  -- key-to-the-endless-chamber
+    56329,  -- fist-of-pained-senses
+    56330,  -- Cinnabar Shoulders
+    56331,  -- dolomite-adorned-gloves
+    56332,  -- phosphorescent-ring
+    133206, -- key-to-the-endless-chamber
+    133207, -- fist-of-pained-senses
+    133208, -- cinnabar-shoulders
+    133209, -- dolomite-adorned-gloves
+    133210, -- phosphorescent-ring
+    133356, -- crystalgrinder-bracers
+    133364, -- crystal-chained-lodestone
+    133375, -- crackling-geode-mace
+    157590, -- crystalgrinder-bracers
+    157591, -- crystal-chained-lodestone
+    -- Slabhide
+    55797,  -- hematite-plate-gloves
+    55798,  -- deep-delving-gloves
+    55799,  -- rose-quartz-band
+    55801,  -- quicksilver-blade
+    56333,  -- rose-quartz-band
+    56334,  -- deep-delving-gloves
+    56335,  -- quicksilver-blade
+    56336,  -- hematite-plate-gloves
+    133211, -- rose-quartz-band
+    133212, -- deep-delving-gloves
+    133213, -- quicksilver-blade
+    133214, -- hematite-plate-gloves
+    133230, -- wand-of-dark-worship
+    133231, -- skin-of-stone
+    133357, -- earth-strength-legguards
+    133365, -- crystalpowder-amice
+    157593, -- crystalpowder-amice
+    157594, -- earth-strength-legguards
+    -- Ozruk
+    55802,  -- elementium-scale-bracers
+    55803,  -- belt-of-the-ringworm
+    55804,  -- pendant-of-the-lightless-grotto
+    55810,  -- tendrils-of-burrowing-dark
+    55811,  -- sword-of-the-bottomless-pit
+    56338,  -- pendant-of-the-lightless-grotto
+    56339,  -- tendrils-of-burrowing-dark
+    56340,  -- elementium-scale-bracers
+    56341,  -- belt-of-the-ringworm
+    56342,  -- sword-of-the-bottomless-pit
+    133215, -- pendant-of-the-lightless-grotto
+    133216, -- tendrils-of-burrowing-dark
+    133217, -- elementium-scale-bracers
+    133218, -- belt-of-the-ringworm
+    133219, -- sword-of-the-bottomless-pit
+    133227, -- tear-of-blood
+    133229, -- heavy-geode-mace
+    133366, -- fractured-earthstone-necklace
+    157595, -- fractured-earthstone-necklace
+    -- High Priestess Azil
+    55812,  -- helm-of-numberless-shadows
+    55813,  -- elementium-fang
+    55814,  -- magnetite-mirror
+    55815,  -- darkling-staff
+    55816,  -- leaden-despair
+    55817,  -- slippers-of-the-twilight-prophet
+    55818,  -- cowl-of-the-unseen-world
+    55819,  -- tear-of-blood
+    55820,  -- prophets-scepter
+    56343,  -- darkling-staff
+    56344,  -- helm-of-numberless-shadows
+    56345,  -- magnetite-mirror
+    56346,  -- elementium-fang
+    56347,  -- leaden-despair
+    56348,  -- slippers-of-the-twilight-prophet
+    56349,  -- prophets-scepter
+    56351,  -- tear-of-blood
+    56352,  -- cowl-of-the-unseen-world
+    133220, -- darkling-staff
+    133221, -- helm-of-numberless-shadows
+    133222, -- magnetite-mirror
+    133223, -- elementium-fang
+    133224, -- leaden-despair
+    133225, -- slippers-of-the-twilight-prophet
+    133226, -- prophets-scepter
+    133228, -- cowl-of-the-unseen-world
+    -- Quest Rewards
+    --- Followers and Leaders (28814)
+    66891, -- pauldrons-of-unholy-rituals
+    66892, -- band-of-the-one-hundred-and-one
+    66893, -- high-priestess-signet
 }
