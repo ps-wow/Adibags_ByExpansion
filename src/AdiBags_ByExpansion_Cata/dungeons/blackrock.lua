f19e70e1c58d6e8846e779e301d059d1fbc542e9
David Craig <davidcraig1986@gmail.com>
2020-04-21 08:02:48 +0100

    Blackrock Caverns

--- src/AdiBags_ByExpansion_Cata/dungeons/blackrock.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/blackrock.lua
@@ -3,4 +3,70 @@ local AddonName, AddonTable = ...
 -- Blackrock Caverns
 -- https://www.wowhead.com/blackrock-caverns
 AddonTable.blackrock = {
+    -- Rom'ogg Bonecrusher
+    55278,  -- inquisition-robes
+    55279,  -- manacles-of-pain
+    55776,  -- skullcracker-ring
+    55777,  -- torturers-mercy
+    55778,  -- shield-of-the-iron-maiden
+    56310,  -- skullcracker-ring
+    56311,  -- inquisition-robes
+    56312,  -- torturers-mercy
+    56313,  -- manacles-of-pain
+    56314,  -- shield-of-the-iron-maiden
+    157608, -- groundrumble-boots
+    -- Corla, Herald of Twilight
+    55263,  -- renouncers-cowl
+    55264,  -- armbands-of-change
+    55265,  -- signet-of-transformation
+    55266,  -- grace-of-the-herald
+    55267,  -- corlas-baton
+    56295,  -- grace-of-the-herald
+    56296,  -- corlas-baton
+    56297,  -- armbands-of-change
+    56298,  -- renouncers-cowl
+    56299,  -- signet-of-transformation
+    -- Karsh Steelbender
+    55268,  -- bracers-of-cooled-anger
+    55271,  -- quicksilver-amulet
+    55272,  -- steelbenders-masterpiece
+    55270,  -- burned-gatherings
+    55269,  -- heat-wave-leggings
+    56303,  -- heat-wave-leggings
+    56302,  -- steelbenders-masterpiece
+    56301,  -- bracers-of-cooled-anger
+    56304,  -- burned-gatherings
+    157610, -- heat-shielded-tunic
+    56300,  -- quicksilver-amulet
+    -- Beauty
+    55273,  -- beautys-chew-toy
+    55274,  -- beautys-plate
+    55275,  -- beautys-silken-ribbon
+    55276,  -- kibble
+    55277,  -- beautys-favorite-bone
+    56305,  -- beautys-silken-ribbon
+    56306,  -- beautys-favorite-bone
+    56307,  -- kibble
+    56308,  -- beautys-plate
+    56309,  -- beautys-chew-toy
+    157607, -- gently-gnawed-hauberk
+    -- Ascendant Lord Obsidius
+    55779,  -- razs-pauldrons
+    55780,  -- twitching-shadows
+    55781,  -- carrier-wave-pendant
+    55782,  -- amber-messenger
+    55784,  -- clutches-of-dying-light
+    55785,  -- willowy-crown
+    55786,  -- kyrstel-mantle
+    55787,  -- witching-hourglass
+    55788,  -- crepuscular-shield
+    56315,  -- twitching-shadows
+    56317,  -- amber-messenger
+    56318,  -- razs-pauldrons
+    56319,  -- carrier-wave-pendant
+    56320,  -- witching-hourglass
+    56321,  -- willowy-crown
+    56322,  -- crepuscular-shield
+    56323,  -- clutches-of-dying-light
+    56324,  -- kyrstel-mantle
 }
