f52b74a122d350979c45f7b4650527ae85b541f0
David Craig <davidcraig1986@gmail.com>
2020-04-21 09:37:11 +0100

    Zul'Gurub

--- build/AdiBags_ByExpansion_Cata/dungeons/zulgurub.lua
+++ build/AdiBags_ByExpansion_Cata/dungeons/zulgurub.lua
@@ -2,4 +2,58 @@ local AddonName, AddonTable = ...
 -- Zul'Gurub
 -- https://www.wowhead.com/zulgurub
 AddonTable.zulgurub = {
+    -- High Priest Venoxis
+    69600,
+    69603,
+    69604,
+    69601,
+    69602,
+    -- Bloodlord Mandokir
+    69605,
+    68823,
+    69609,
+    69608,
+    69606,
+    69607,
+    -- Edge of Madness
+    69630,
+    69632,
+    69633,
+    69631,
+    --- Hazza'rah
+    69637,
+    69636,
+    --- Renataki
+    69638,
+    69639,
+    --- Wushoolay
+    69640,
+    69641,
+    --- Gri'lek
+    69635,
+    69634,
+    -- High Priestess Kilnara
+    68824,
+    69610,
+    69611,
+    69612,
+    69613,
+    69614,
+    -- Zanzil
+    69615,
+    69616,
+    69617,
+    69618,
+    69619,
+    -- Jin'do the Godbreaker
+    69620,
+    69621,
+    69622,
+    69623,
+    69624,
+    69625,
+    69626,
+    69627,
+    69628,
+    69629,
 }
--- src/AdiBags_ByExpansion_Cata/dungeons/zulgurub.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/zulgurub.lua
@@ -3,4 +3,58 @@ local AddonName, AddonTable = ...
 -- Zul'Gurub
 -- https://www.wowhead.com/zulgurub
 AddonTable.zulgurub = {
+    -- High Priest Venoxis
+    69600, -- belt-of-slithering-serpents
+    69603, -- breastplate-of-serenity
+    69604, -- coils-of-hate
+    69601, -- serpentine-leggings
+    69602, -- signet-of-venoxis
+    -- Bloodlord Mandokir
+    69605, -- amulet-of-the-watcher
+    68823, -- armored-razzashi-raptor
+    69609, -- bloodlords-protector
+    69608, -- deathcharged-wristguards
+    69606, -- hakkari-loa-drape
+    69607, -- touch-of-discord
+    -- Edge of Madness
+    69630, -- handguards-of-the-tormented
+    69632, -- lost-bag-of-whammies
+    69633, -- plunderers-gauntlets
+    69631, -- zulian-voodoo-stick
+    --- Hazza'rah
+    69637, -- gurubashi-destroyer
+    69636, -- thekals-claws
+    --- Renataki
+    69638, -- arlokks-claws
+    69639, -- renatakis-soul-slicer
+    --- Wushoolay
+    69640, -- kilt-of-forgotten-rites
+    69641, -- troll-skull-chestplate
+    --- Gri'lek
+    69635, -- amulet-of-protection
+    69634, -- fascs-preserved-boots
+    -- High Priestess Kilnara
+    68824, -- swift-zulian-panther
+    69610, -- arlokks-signet
+    69611, -- sash-of-anguish
+    69612, -- claw-fringe-mantle
+    69613, -- leggings-of-the-pride
+    69614, -- roaring-mask-of-bethekk
+    -- Zanzil
+    69615, -- zombie-walker-legguards
+    69616, -- spiritbinder-spaulders
+    69617, -- plumed-medicine-helm
+    69618, -- zulian-slasher
+    69619, -- bone-plate-handguards
+    -- Jin'do the Godbreaker
+    69620, -- twinblade-of-the-hakkari
+    69621, -- twinblade-of-the-hakkari
+    69622, -- the-hexxers-mask
+    69623, -- vestments-of-the-soulflayer
+    69624, -- legacy-of-arlokk
+    69625, -- mandokirs-tribute
+    69626, -- jindos-verdict
+    69627, -- zulian-ward
+    69628, -- jekliks-smasher
+    69629, -- shield-of-the-blood-god
 }
