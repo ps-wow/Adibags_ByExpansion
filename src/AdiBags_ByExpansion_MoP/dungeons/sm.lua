c5121acb748169fc60811f48c987931ed427d21e
David Craig <davidcraig1986@gmail.com>
2020-04-21 10:07:18 +0100

    Scarlet Monastery (Heroic).

--- src/AdiBags_ByExpansion_MoP/dungeons/sm.lua
+++ src/AdiBags_ByExpansion_MoP/dungeons/sm.lua
@@ -1,4 +1,44 @@
 local AddonName, AddonTable = ...
 
+-- Scarlet Monastery (Heroic)
 AddonTable.sm = {
+    -- Thalnos the Soulrender
+    81560,  -- signet-of-the-hidden-door
+    81569,  -- forgotten-bloodmage-mantle
+    81570,  -- legguards-of-the-crimson-magus
+    81571,  -- soulrender-greatcloak
+    81572,  -- bracers-of-the-fallen-crusader
+    144001, -- forgotten-bloodmage-mantle
+    144002, -- legguards-of-the-crimson-magus
+    144174, -- signet-of-the-hidden-door
+    144183, -- soulrender-greatcloak
+    144184, -- bracers-of-the-fallen-crusader
+    -- Brother Korloff
+    81561,  -- firefinger-ring
+    81573,  -- korloffs-raiment
+    81574,  -- helm-of-rising-flame
+    81575,  -- scorched-earth-cloak
+    81576,  -- firestorm-greatstaff
+    143965, -- korloffs-raiment
+    144026, -- helm-of-rising-flame
+    144175, -- firefinger-ring
+    144185, -- scorched-earth-cloak
+    144186, -- firestorm-greatstaff
+    -- High Inquisitor Whitemane
+    81562,  -- triune-signet
+    81578,  -- crown-of-holy-flame
+    81689,  -- leggings-of-hallowed-fire
+    81690,  -- incarnadine-scarlet-spaulders
+    81692,  -- whitemanes-embroidered-chapeau
+    144003, -- dashing-strike-treads
+    144004, -- leggings-of-hallowed-fire
+    144005, -- incarnadine-scarlet-spaulders
+    144027, -- crown-of-holy-flame
+    144028, -- whitemanes-embroidered-chapeau
+    144158, -- flashing-steel-talisman
+    144176, -- triune-signet
+    144187, -- lightbreaker-greatsword
+    144188, -- waistplate-of-imminent-resurrection
+    144189, -- greatstaff-of-righteousness
+    144221, -- helios-durands-soul-of-purity
 }
