4873ccc8f44822183fce6fe6357e886c6d15c7e4
David Craig <davidcraig1986@gmail.com>
2020-04-21 09:43:06 +0100

    Zul'Aman

--- build/AdiBags_ByExpansion_Cata/dungeons/zulaman.lua
+++ build/AdiBags_ByExpansion_Cata/dungeons/zulaman.lua
@@ -2,4 +2,46 @@ local AddonName, AddonTable = ...
 -- Zul'aman
 -- https://www.wowhead.com/zulaman
 AddonTable.zulaman = {
+    -- Akil'zon (Eagle)
+    69552,
+    69551,
+    69550,
+    69553,
+    69549,
+    -- Nalorakk (Bear)
+    69556,
+    69555,
+    69557,
+    69554,
+    69558,
+    -- Jan'alai (Dragonhawk)
+    69559,
+    69562,
+    69561,
+    69560,
+    69563,
+    -- Halazzi (Lynx)
+    69565,
+    69568,
+    69566,
+    69564,
+    69567,
+    -- Hex Lord Malacrass
+    69572,
+    69573,
+    70080,
+    69569,
+    69571,
+    69570,
+    -- Daakara
+    69579,
+    69581,
+    69577,
+    69576,
+    69578,
+    69583,
+    69575,
+    69580,
+    69582,
+    69574,
 }
--- src/AdiBags_ByExpansion_Cata/dungeons/zulaman.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/zulaman.lua
@@ -3,4 +3,46 @@ local AddonName, AddonTable = ...
 -- Zul'aman
 -- https://www.wowhead.com/zulaman
 AddonTable.zulaman = {
+    -- Akil'zon (Eagle)
+    69552, -- bracers-of-hidden-purpose
+    69551, -- feathers-of-akilzon
+    69550, -- leggings-of-ancient-magics
+    69553, -- talonguard-band
+    69549, -- wristguards-of-the-predator
+    -- Nalorakk (Bear)
+    69556, -- armbands-of-the-bear-spirit
+    69555, -- boots-of-the-ursine
+    69557, -- jungle-striders
+    69554, -- pauldrons-of-nalorakk
+    69558, -- spiritshield-mask
+    -- Jan'alai (Dragonhawk)
+    69559, -- amanishi-bracers
+    69562, -- boots-of-bad-mojo
+    69561, -- hawkscale-waistguard
+    69560, -- janalais-spaulders
+    69563, -- ring-of-the-numberless-brood
+    -- Halazzi (Lynx)
+    69565, -- breastplate-of-primal-fury
+    69568, -- shadowmender-wristguards
+    69566, -- shimmerclaw-band
+    69564, -- the-savagers-mask
+    69567, -- wristwraps-of-departed-spirits
+    -- Hex Lord Malacrass
+    69572, -- hex-lords-bloody-cloak
+    69573, -- pauldrons-of-sacrifice
+    70080, -- reforged-heartless
+    69569, -- shadowtooth-trollskin-breastplate
+    69571, -- soul-drain-signet
+    69570, -- waistband-of-hexes
+    -- Daakara
+    69579, -- amani-headdress
+    69581, -- amani-scepter-of-rites
+    69577, -- collar-of-bones
+    69576, -- headdress-of-sharpened-vision
+    69578, -- hexing-robes
+    69583, -- legguards-of-the-unforgiving
+    69575, -- mace-of-the-sacrificed
+    69580, -- mask-of-restless-spirits
+    69582, -- skullpiercer-pauldrons
+    69574, -- tusked-shoulderpads
 }
