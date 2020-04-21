fcb5c506f048164cb52c62ee6f429d64689c7c43
David Craig <davidcraig1986@gmail.com>
2020-04-21 08:30:46 +0100

    Halls of Origination

--- src/AdiBags_ByExpansion_Cata/dungeons/hoo.lua
+++ src/AdiBags_ByExpansion_Cata/dungeons/hoo.lua
@@ -1,6 +1,91 @@
 local AddonName, AddonTable = ...
 
 -- Halls of Origination
--- https://www.wowhead.com/halls-of-origination
 AddonTable.hoo = {
+    -- Temple Guardian Anhuur
+    55886,  -- poison-fang-bracers
+    55887,  -- belt-of-petrified-tears
+    55888,  -- darkhowl-amulet
+    55889,  -- anhuurs-hymnal
+    55890,  -- awakening-footfalls
+    56407,  -- anhuurs-hymnal
+    56408,  -- awakening-footfalls
+    56409,  -- poison-fang-bracers
+    56410,  -- belt-of-petrified-tears
+    56411,  -- darkhowl-amulet
+    -- Earthrager Ptah
+    56093,  -- breastplate-of-the-risen-land
+    56094,  -- underworld-cord
+    56095,  -- mouth-of-the-earth
+    56096,  -- bulwark-of-the-primordial-mound
+    56097,  -- soul-releaser
+    56422,  -- mouth-of-the-earth
+    56423,  -- underworld-cord
+    56424,  -- soul-releaser
+    56425,  -- breastplate-of-the-risen-land
+    56426,  -- bulwark-of-the-primordial-mound
+    -- Anraphet
+    57855,  -- alpha-bracers
+    57856,  -- omega-breastplate
+    57857,  -- boots-of-crumbling-ruin
+    57858,  -- mantle-of-soft-shadows
+    57860,  -- anraphets-regalia
+    57866,  -- mantle-of-soft-shadows
+    57867,  -- boots-of-crumbling-ruin
+    57868,  -- anraphets-regalia
+    57869,  -- omega-breastplate
+    57870,  -- alpha-bracers
+    157611, -- crown-of-patient-vigil
+    -- Isiset
+    55992,  -- armguards-of-unearthly-light
+    55993,  -- legwraps-of-astral-rain
+    55994,  -- ring-of-blinding-stars
+    55995,  -- blood-of-isiset
+    55996,  -- nova-band
+    56412,  -- ring-of-blinding-stars
+    56413,  -- legwraps-of-astral-rain
+    56414,  -- blood-of-isiset
+    56415,  -- nova-band
+    56416,  -- armguards-of-unearthly-light
+    157609, -- scepter-of-stargazing
+    -- Ammunae
+    55997,  -- bloodpetal-mantle
+    55998,  -- robes-of-rampant-growth
+    55999,  -- seedling-pod
+    56000,  -- band-of-life-energy
+    56417,  -- robes-of-rampant-growth
+    56418,  -- band-of-life-energy
+    56419,  -- bloodpetal-mantle
+    56421,  -- seedling-pod
+    -- Setesh
+    57861,  -- helm-of-setesh
+    57862,  -- chaotic-wrappings
+    57863,  -- hieroglyphic-vest
+    57864,  -- helm-of-the-typhonic-beast
+    57865,  -- scepter-of-power
+    57871,  -- helm-of-the-typhonic-beast
+    57872,  -- scepter-of-power
+    57873,  -- helm-of-setesh
+    57874,  -- hieroglyphic-vest
+    57875,  -- chaotic-wrappings
+    -- Rajh
+    56098,  -- red-beam-cord
+    56099,  -- fingers-of-light
+    56100,  -- right-eye-of-rajh
+    56101,  -- sun-strike
+    56102,  -- left-eye-of-rajh
+    56104,  -- legguards-of-noon
+    56105,  -- hekatic-slippers
+    56106,  -- band-of-rays
+    56107,  -- solar-wind-cloak
+    56108,  -- blade-of-the-burning-sun
+    56427,  -- left-eye-of-rajh
+    56428,  -- fingers-of-light
+    56429,  -- red-beam-cord
+    56431,  -- right-eye-of-rajh
+    56432,  -- band-of-rays
+    56433,  -- blade-of-the-burning-sun
+    56434,  -- solar-wind-cloak
+    56435,  -- legguards-of-noon
+    56436,  -- hekatic-slippers
 }
