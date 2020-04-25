35202f8add93ede85b9d52f09ddf5d4542cf57a8
David Craig <davidcraig1986@gmail.com>
2020-04-25 19:24:20 +0100

    Ny'alotha

--- src/AdiBags_ByExpansion_BfA/raids/nyalotha.lua
+++ src/AdiBags_ByExpansion_BfA/raids/nyalotha.lua
@@ -1,18 +1,128 @@
 local AddonName, AddonTable = ...
 
 AddonTable.nyalotha = {
-    -- Note: all item id's are the same regardless of difficulty.
+    174279, -- Encrypted Nyalothan Text
 
     -- Wrathion, the Black Emporer
+    174044, -- humming-black-dragonscale
+    174170, -- dragonbone-vambraces
+    174153, -- ebony-scaled-gauntlets
+    172185, -- destroyers-shadowblade
+    174125, -- emberscale-gloves
+    174139, -- onyx-imbued-breeches
+    172199, -- faralos-empires-dream
+    174105, -- mishun-blade-of-tyrants
     -- Maut
+    174446, -- fractured-obsidian-claw [Battle Pet]
+    173944, -- forbidden-obsidian-claw
+    174155, -- greaves-of-forbidden-magics
+    174141, -- boots-of-manifest-shadow
+    174172, -- living-obsidian-legguards
+    174132, -- stygian-guise
+    174124, -- mana-infused-sash
+    173940, -- sigil-of-warding
+    174152, -- pauldrons-of-ill-portent
+    172200, -- skshuul-vaz
+    172191, -- anzig-vra
     -- The Prophet Skitra
+    172201, -- bloodstained-ritual-athame
+    174173, -- boots-of-hallucinatory-reality
+    174138, -- bracers-of-dark-prophecy
+    174143, -- macabre-ritual-pants
+    174060, -- psyche-shredder
+    174123, -- psychics-subtle-slippers
+    174119, -- robes-of-unreality
+    174157, -- talons-of-grim-revelations
+    172193, -- whispering-eldritch-bow
+    174165, -- writhing-spaulders-of-madness
     -- Dark Inquisitor Xanesh
+    173943, -- torment-in-a-jar
+    174140, -- cord-of-anguished-cries
+    174169, -- gauntlets-of-foul-inquisition
+    172196, -- vorzz-yoqal
+    174156, -- chainlink-belt-of-ill-omens
+    174126, -- binding-of-dark-heresies
+    174121, -- trousers-of-peculiar-potency
+    172190, -- uhnish-vwah
     -- The Hivemind
+    174530, -- ring-of-collective-consciousness
+    174122, -- void-drenched-wristwraps
+    174154, -- nightmarish-chain-shackles
+    174171, -- dark-crystalline-girdle
+    174137, -- chitinspine-gloves
+    174106, -- qwor-nlyeth
+    172192, -- the-all-seeing-eye
+    174448, -- aqir-hivespawn [Battle Pet]
+    174449, -- rakim [Battle Pet]
     -- Shad'har the Insatiable
+    174177, -- bracers-of-phantom-pains
+    174162, -- ego-annihilating-grips
+    174130, -- grips-of-occult-reminiscence
+    174107, -- insidious-writhing-longbow
+    174531, -- void-etched-band
+    172186, -- warmace-of-waking-nightmares
+    174145, -- wristwraps-of-volatile-power
     -- Drest'agath
+    174174, -- belt-of-muttering-truths
+    174146, -- gloves-of-abyssal-authority
+    172195, -- halsheth-slumberers-spear
+    174532, -- ichorspine-loop
+    172198, -- markowa-the-mindpiercer
+    174159, -- spinebarb-legplates
+    173946, -- writhing-segment-of-drestagath
     -- Il'gynoth, Corruption Reborn
+    174142, -- belt-of-braided-vessels
+    174161, -- bracers-of-manifest-apathy
+    174116, -- cowl-of-unspeakable-horrors
+    174129, -- cuffs-of-grim-conjuration
+    172189, -- eyestalk-of-ilgynoth
+    174180, -- oozing-coagulum
+    172188, -- precipice-of-oblivion
+    174176, -- sabatons-of-malevolent-intent
+    174150, -- scales-of-the-scheming-behemoth
+    174163, -- second-sight-helm
+    174135, -- spaulders-of-aberrant-allure
     -- Vexiona
+    174131, -- darkheart-robe
+    174120, -- void-ascendants-mantle
+    174164, -- breastplate-of-twilight-decimation
+    174160, -- greaves-of-the-twilight-drake
+    174148, -- helm-of-deep-despair
+    172194, -- gift-of-the-void
     -- Ra-den the Despoiled
+    174528, -- void-twisted-titanshard
+    174500, -- vita-charged-titanshard
+    174168, -- carapace-of-pulsing-vita
+    174115, -- robe-of-the-fallen-keeper
+    174128, -- boots-of-wrought-shadow
+    172228, -- shandai-watcher-of-cosmos
+    174149, -- dreamers-unblinking-pauldrons
+    174134, -- gibbering-maw
+    174175, -- reality-defying-greaves
+    172197, -- unguent-caress
+    174447, -- void-scarred-anubisath [Battle Pet]
     -- Carapace of N'Zoth
+    174127, -- corporeal-supplicants-trousers
+    174144, -- corpuscular-leather-greaves
+    174151, -- helm-of-actualized-visions
+    174109, -- lurkers-piercing-gaze
+    174178, -- mirage-weavers-gauntlets
+    174166, -- pauldrons-of-infinite-darkness
+    172227, -- shard-of-the-black-empire
+    174108, -- shglayos-astral-malignity
+    174117, -- spaulders-of-miasmic-mycelia
+    174136, -- tortured-fleshbeast-cuirass
+    174158, -- watchers-scheming-girdle
     -- N'Zoth the Corruptor
+    172187, -- devastations-hour
+    172229, -- dominion-lurker-beyond-dreams
+    174167, -- greathelm-of-phantasmic-reality
+    174147, -- last-vestige-of-neltharion
+    174277, -- lingering-psychic-shell
+    174103, -- manifesto-of-madness
+    174133, -- pauldrons-of-the-great-convergence
+    174533, -- ring-of-cosmic-potential
+    174118, -- visage-of-nightmarish-machinations
+    174452, -- eye-of-corruption [Battle Pet]
+    174872, -- nyalotha-allseer [Mount]
 }
