c36b2ffaeca42512648ab4210a088ac6ba206c1e
David Craig <davidcraig1986@gmail.com>
2020-04-25 18:58:00 +0100

    Battle for Dazar'alor

--- src/AdiBags_ByExpansion_BfA/raids/dazaralor.lua
+++ src/AdiBags_ByExpansion_BfA/raids/dazaralor.lua
@@ -3,6 +3,126 @@ local AddonName, AddonTable = ...
 AddonTable.dazaralor = {
     -- Note: all item id's are the same regardless of difficulty.
 
-    -- Grong, the Revenant
+    -- Champion of the Light
+    165919, -- desecrated-blade-of-the-disciples
+    165517, -- bracers-of-regal-devotion
+    165519, -- cowl-of-righteous-resolve
+    165549, -- crusade-pummelers
+    165514, -- gloves-of-spiritual-grace
+    165501, -- bracers-of-zealous-calling
+    165834, -- divine-fury-raiment
+    165533, -- lightgrace-sabatons
+    165921, -- pauldrons-of-ancestral-vengeance
+    165550, -- breastplate-of-divine-purification
+    165586, -- dawnbreaker
+    165584, -- sunburst-crest
+    165569, -- ward-of-envelopment
+    -- Jadefire Masters
+    165565, -- band-of-multi-sided-strikes
+    165500, -- blazewing-hood
+    165521, -- cranedancer-leggings
+    165552, -- embersear-waistguard
+    165764, -- firecallers-handwraps
+    165531, -- grips-of-harmonious-spirits
+    165548, -- helm-of-tempered-jade
+    165568, -- invocation-of-yulon
+    165777, -- maras-boneblade-mantle
+    165540, -- mistfire-raiment
+    165587, -- phoenixfire-staff
+    -- Grong
     165515, -- Grongpelt Vest
+    165534, -- ape-wranglers-wristguards
+    165920, -- apetagonizers-claw
+    165588, -- bonelash-paw
+    165922, -- bristling-fur-lined-amice
+    165582, -- cursed-monkey-palm
+    165574, -- grongs-primal-rage
+    165589, -- hornridged-crusher
+    165499, -- leggings-of-dire-research
+    165513, -- silverback-cloak
+    165535, -- simian-berserkers-helm
+    165555, -- spaulders-of-the-gorilla-king
+    165551, -- splinter-bone-vambraces
+    165525, -- stretched-sinew-waistcord
+    -- Opulence [A]
+    165561, -- coinage-stampers
+    165571, -- incandescent-sliver
+    165526, -- crown-of-aakuls-dark-reign
+    165821, -- crown-of-bloody-succession
+    165592, -- goblet-of-glittering-favor
+    165818, -- crown-of-the-seducer
+    165538, -- goldenscale-girdle
+    165820, -- electrified-crown-of-rahuai
+    165504, -- waistcord-of-flowing-silk
+    165524, -- amethyst-studded-bindings
+    165541, -- boots-of-the-gilded-path
+    165573, -- diamond-laced-refracting-prism
+    165593, -- greeds-folly
+    165591, -- bloodtooth-the-soulfeaster
+    -- Conclave of the Chosen [A]
+    165512, -- loa-exultants-shroud
+    165833, -- vestments-of-indomitable-will
+    165579, -- kimbuls-razor-claw
+    165507, -- mantle-of-the-skyterror
+    166418, -- crest-of-paku
+    165562, -- ridgeplate-pauldrons
+    165502, -- lightfeather-footpads
+    165581, -- crest-of-paku
+    165560, -- arcing-thunderlizard-legplates
+    165532, -- gonks-scale-robes
+    165594, -- akundas-shocksplitter
+    165595, -- blade-of-encroaching-death
+    165599, -- pterrorwing-longbow
+    -- King Rastakhan [A]
+    165567, -- seal-of-the-zandalari-empire
+    165597, -- deathspeaker-spire
+    165523, -- phantom-stalker-shoulders
+    165832, -- breastplate-of-the-deathbound
+    165558, -- rokas-bonecrushing-manacles
+    165536, -- deathhunters-legguards
+    165537, -- mantle-of-the-soulbinders-caress
+    165498, -- vestments-of-the-afterlife
+    165577, -- bwonsamdis-bargain
+    165578, -- mirror-of-entwined-fate
+    165596, -- last-fang-of-rezan
+    -- High Tinker Mekkatorque [H]
+    165825, -- dyno-sprocket-spaulders
+    165497, -- giga-charged-shoulderpads
+    165924, -- high-tinkers-cape
+    165508, -- lever-stabilizing-wristwraps
+    165522, -- mech-jockey-grips
+    165830, -- mekkatorques-bomber-jacket
+    165580, -- ramping-amplitude-gigavolt-engine
+    165598, -- servo-claw-smasher
+    165543, -- target-tracking-headgear
+    165600, -- twin-pipe-buster-cannon
+    165572, -- variable-intensity-gigavolt-oscillating-reactor
+    -- Stormwall Blockade [H]
+    165585, -- blockade-bulwark
+    165822, -- cowl-of-tideborne-omens
+    165602, -- crash-of-tides
+    165590, -- docksplitter-siege-hook
+    165528, -- kelp-laced-greaves
+    165557, -- sea-swell-chestplate
+    165603, -- sirens-song
+    165546, -- slimy-kelpweavers
+    165601, -- storm-toothed-kasuyu
+    165556, -- stormwrought-gauntlets
+    165923, -- tidal-shroud-spaulders
+    165819, -- tidemothers-cover
+    165506, -- wavecaller-leggings
+    -- Lady Jaina Proudmoore [H]
+    165824, -- admiraltys-ceremonial-epaulets
+    165604, -- daelin-proudmoores-saber
+    165527, -- embossed-deckwalkers
+    165570, -- everchill-anchor
+    165831, -- flag-officers-overcoat
+    165583, -- fogbreaker-light-of-the-sea
+    165823, -- glaciercrest-helm
+    165559, -- hullplate-girdle
+    165542, -- icebinders-bracers
+    165566, -- lord-admirals-signet
+    165505, -- robes-of-biting-cold
+    165576, -- tidestorm-codex
+    166518, -- G.M.O.D [Mount]
 }
