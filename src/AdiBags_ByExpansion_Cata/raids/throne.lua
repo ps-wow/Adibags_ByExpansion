70c53e686c693037febca32fa383db538e435301
David Craig <davidcraig1986@gmail.com>
2020-05-01 19:27:35 +0100

    Throne of the Four Winds

--- src/AdiBags_ByExpansion_Cata/raids/throne.lua
+++ src/AdiBags_ByExpansion_Cata/raids/throne.lua
@@ -1,43 +1,80 @@
 local AddonName, AddonTable = ...
 
+-- Throne of the Four Winds
 AddonTable.throne = {
     -- The Conclave of Wind
-    65367, -- Mistral Circle
-    65368, -- Star Chaser Belt
-    65369, -- Sky Strider Belt
-    65370, -- Thunder Wall Belt
-    65371, -- Wind Stalker Belt
-    65372, -- Permafrost Signet
-    65373, -- Planetary Band
-    65374, -- Gale Rouser Belt
-    65375, -- Tempest Keeper Belt
-    65376, -- Soul Breath Belt
-    65377, -- Lightning Well Belt
-
+    63488,  -- mistral-circle
+    63489,  -- permafrost-signet
+    63490,  -- sky-strider-belt
+    63491,  -- thunder-wall-belt
+    63492,  -- star-chaser-belt
+    63493,  -- wind-stalker-belt
+    63494,  -- planetary-band
+    63495,  -- tempest-keeper-belt
+    63496,  -- lightning-well-belt
+    63497,  -- gale-rouser-belt
+    65367,  -- Mistral Circle
+    65368,  -- Star Chaser Belt
+    65369,  -- Sky Strider Belt
+    65370,  -- Thunder Wall Belt
+    65371,  -- Wind Stalker Belt
+    65372,  -- Permafrost Signet
+    65373,  -- Planetary Band
+    65374,  -- Gale Rouser Belt
+    65375,  -- Tempest Keeper Belt
+    65376,  -- Soul Breath Belt
+    71716,  -- soothsayers-runes
+    152974, -- breezy-essence
     -- Al'Akir
-    66998, -- Essence of the Forlorn
-    63041, -- Reins of the Drake of the South Wind
-    68130, -- Stormwake, the Tempest's Reach
-    68131, -- Stormwake, the Tempest's Reach
-    68132, -- Stormwake, the Tempest's Reach
-    69885, -- Cloudburst Necklace
-    69880, -- Mistral Pendant
-    69883, -- Permafrost Choker
-    69882, -- Planetary Amulet
-    69879, -- Cloudburst Cloak
-    69884, -- Mistral Drape
-    69878, -- Permafrost Cape
-    69881, -- Planetary Drape
-    65384, -- Gale Rouser Leggings
-    65386, -- Lightning Well Legguards (Duplicate: BoT)
-    65379, -- Sky Strider Greaves (Duplicate: BoT)
-    65383, -- Soul Breath Leggings (Duplicate: BoT)
-    65378, -- Star Chaser Legguards (Duplicate: BoT)
-    65385, -- Tempest Keeper Leggings (Duplicate: BoT)
-    65380, -- Thunder Wall Greaves (Duplicate: BoT)
-    65381, -- Wind Stalker Leggings (Duplicate: BoT)
-    65382, -- Cloudburst Ring
-
     138836, -- Illusion: Windfury
+    138952, -- remnant-of-air
     152973, -- Zephyr's Call (Battle Pet)
+    63041,  -- Reins of the Drake of the South Wind
+    63499,  -- cloudburst-ring
+    63500,  -- sky-strider-greaves
+    63501,  -- thunder-wall-greaves
+    63502,  -- star-chaser-legguards
+    63503,  -- wind-stalker-leggings
+    63504,  -- tempest-keeper-leggings
+    63505,  -- lightning-well-legguards
+    63506,  -- gale-rouser-leggings
+    63507,  -- soul-breath-leggings
+    63682,  -- helm-of-the-forlorn-vanquisher
+    63683,  -- helm-of-the-forlorn-conqueror
+    63684,  -- helm-of-the-forlorn-protector
+    64314,  -- mantle-of-the-forlorn-vanquisher
+    64315,  -- mantle-of-the-forlorn-conqueror
+    64316,  -- mantle-of-the-forlorn-protector
+    65378,  -- Star Chaser Legguards (Duplicate: BoT)
+    65379,  -- Sky Strider Greaves (Duplicate: BoT)
+    65380,  -- Thunder Wall Greaves (Duplicate: BoT)
+    65381,  -- Wind Stalker Leggings (Duplicate: BoT)
+    65382,  -- Cloudburst Ring
+    65383,  -- Soul Breath Leggings (Duplicate: BoT)
+    65384,  -- Gale Rouser Leggings
+    65385,  -- Tempest Keeper Leggings (Duplicate: BoT)
+    65386,  -- Lightning Well Legguards (Duplicate: BoT)
+    66998,  -- Essence of the Forlorn
+    68127,  -- stormwake-the-tempests-reach
+    68128,  -- stormwake-the-tempests-reach
+    68129,  -- stormwake-the-tempests-reach
+    68130,  -- Stormwake, the Tempest's Reach
+    68131,  -- Stormwake, the Tempest's Reach
+    68132,  -- Stormwake, the Tempest's Reach
+    69827,  -- mistral-pendant
+    69828,  -- permafrost-choker
+    69829,  -- cloudburst-necklace
+    69830,  -- planetary-amulet
+    69831,  -- mistral-drape
+    69833,  -- permafrost-cape
+    69834,  -- cloudburst-cloak
+    69835,  -- planetary-drape
+    69878,  -- Permafrost Cape
+    69879,  -- Cloudburst Cloak
+    69880,  -- Mistral Pendant
+    69881,  -- Planetary Drape
+    69882,  -- Planetary Amulet
+    69883,  -- Permafrost Choker
+    69884,  -- Mistral Drape
+    69885,  -- Cloudburst Necklace
 }
