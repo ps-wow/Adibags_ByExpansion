ecb62d2cf2e3a8aa569518c4aa914711fa408c13
David Craig <davidcraig1986@gmail.com>
2020-05-01 20:19:52 +0100

    Bastion of Twilight

--- src/AdiBags_ByExpansion_Cata/raids/bot.lua
+++ src/AdiBags_ByExpansion_Cata/raids/bot.lua
@@ -20,6 +20,18 @@ AddonTable.bot = {
     67424, -- Chest of the Forlorn Protector (Warr, Hunt, Sham)
     67425, -- Chest of the Forlorn Vanquisher (Rog, DK, Mage, Druid)
     65224, -- Reinforced Sapphirium Chestguard
+    59469, -- storm-riders-boots
+    59470, -- bracers-of-impossible-strength
+    59471, -- pauldrons-of-the-great-ettin
+    59472, -- proto-handlers-gauntlets
+    59473, -- essence-of-the-cyclone
+    59474, -- malevolence
+    59475, -- bracers-of-the-bronze-flight
+    59476, -- legguards-of-the-emerald-brood
+    59481, -- helm-of-the-nether-scion
+    59482, -- robes-of-the-burning-acolyte
+    59483, -- wyrmbreakers-amulet
+    59484, -- book-of-binding-will
     -- Theralion and Valiona
     152969, -- Odd Twilight Egg (Twilight Clutch-Sister) [Battle Pet]
     65091, -- Blade of the Witching Hour
@@ -34,8 +46,32 @@ AddonTable.bot = {
     65106, -- Ring of Rivalry
     65105, -- Theralion's Mirror
     65109, -- Vial of Stolen Memories
+    59512, -- valionas-medallion
+    59515, -- vial-of-stolen-memories
+    59516, -- drape-of-the-twins
+    59517, -- necklace-of-strife
+    59518, -- ring-of-rivalry
+    59519, -- theralions-mirror
+    63531, -- daybreaker-helm
+    63532, -- dragonheart-piercer
+    63533, -- fang-of-twilight
+    63534, -- helm-of-eldritch-authority
+    63535, -- waistguard-of-hatred
+    63536, -- blade-of-the-witching-hour
     -- Ascendant Council
     152970, -- Lesser Circle of Binding (Bound Stream) [Battle Pet]
+    59502, -- dispersing-belt
+    59503, -- terrastras-legguards
+    59504, -- arions-crown
+    59505, -- gravitational-pull
+    59506, -- crushing-weight
+    59507, -- glittering-epidermis
+    59508, -- treads-of-liquid-ice
+    59509, -- glaciated-helm
+    59510, -- feludius-mantle
+    59511, -- hydrolance-gloves
+    59513, -- scepter-of-ice
+    59514, -- heart-of-ignacious
     65111, -- Scepter of Ice
     65120, -- Arion's Crown
     65115, -- Glaciated Helm
@@ -50,6 +86,22 @@ AddonTable.bot = {
     65110, -- Heart of Ignacious
     -- Cho'gall
     152972, -- Twilight Summoning Portal (Faceless Minion) [Battle Pet]
+    59330, -- shalugdoom-the-axe-of-unmaking
+    59485, -- coil-of-ten-thousand-screams
+    59486, -- battleplate-of-the-apocalypse
+    59487, -- helm-of-maddening-whispers
+    59490, -- membrane-of-cthun
+    59494, -- uhnagh-fash-the-darkest-betrayal
+    59495, -- treads-of-hideous-transformation
+    59497, -- shackles-of-the-end-of-days
+    59498, -- hands-of-the-twilight-council
+    59499, -- kilt-of-the-forgotten-battle
+    59500, -- fall-of-mortality
+    59501, -- signet-of-the-fifth-circle
+    63680, -- twilights-hammer
+    64314, -- mantle-of-the-forlorn-vanquisher
+    64315, -- mantle-of-the-forlorn-conqueror
+    64316, -- mantle-of-the-forlorn-protector
     65145, -- Shalug'doom, the Axe of Unmaking
     65090, -- Twilight's Hammer
     68600, -- Uhn'agh Fash, the Darkest Betrayal
