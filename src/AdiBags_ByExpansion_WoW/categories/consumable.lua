6f6587216595fafef0b32645c3318f0f7b6085cb
David Craig <davidcraig1986@gmail.com>
2020-05-15 14:04:31 +0100

    Add all WoW alchemy crafts.

--- src/AdiBags_ByExpansion_WoW/categories/consumable.lua
+++ src/AdiBags_ByExpansion_WoW/categories/consumable.lua
@@ -13,6 +13,7 @@ AddonTable.consumable = {
     1710,  -- Greater Healing Potion
     2455,  -- Minor Mana Potion
     2456,  -- Minor Rejuvenation Potion
+    2457,  -- Elixir of Minor Agility
     2459,  -- Swiftness Potion
     3383,  -- Elixir of Wisdom
     3385,  -- Lesser Mana Potion
@@ -20,8 +21,15 @@ AddonTable.consumable = {
     3387,  -- Limited Invulnerability Potion
     3388,  -- Strong Troll's Blood Elixir
     3389,  -- Elixir of Defense
+    3390,  -- Elixir of Lesser Agility
+    3391,  -- Elixir of Ogre's Strength
     3823,  -- Lesser Invisibility Potion
+    3824,  -- Shadow Oil
+    3825,  -- Elixir of Fortitude
+    3826,  -- Major Troll's Blood Elixir
     3827,  -- Mana Potion
+    3828,  -- Elixir of Detect Lesser Invisibility
+    3829,  -- Frost Oil
     3928,  -- Superior Healing Potion
     5631,  -- Rage Potion
     5633,  -- Great Rage Potion
@@ -36,21 +44,47 @@ AddonTable.consumable = {
     6372,  -- Swim Speed Potion
     6373,  -- Elixir of Firepower
     6452,  -- Anti-Venom
+    6453,  -- Strong Anti-Venom
+    6662,  -- Elixir of Giant Growth
+    8949,  -- Elixir of Agility
+    8951,  -- Elixir of Greater Defense
+    8956,  -- Oil of Immolation
     9144,  -- Wildvine Potion
+    9154,  -- Elixir of Detect Undead
+    9155,  -- Arcane Elixir
     9172,  -- Invisibility Potion
+    9179,  -- Elixir of Greater Intellect
+    9187,  -- Elixir of Greater Agility
+    9197,  -- Elixir of Dream Vision
+    9206,  -- Elixir of Giants
+    9224,  -- Elixir of Demonslaying
+    9233,  -- Elixir of Detect Demon
+    9264,  -- Elixir of Shadow Power
+    10592, -- Catseye Elixir
     12190, -- Dreamless Sleep Potion
     13442, -- Mighty Rage Potion
     13443, -- Superior Mana Potion
     13444, -- Major Mana Potion
+    13445, -- Elixir of Superior Defense
     13446, -- Major Healing Potion
+    13447, -- Elixir of the Sages
+    13453, -- Elixir of Brute Force
+    13454, -- Greater Arcane Elixir
     13455, -- Greater Stoneshield Potion
     13457, -- Greater Fire Protection Potion
     13458, -- Greater Nature Protection Potion
     13462, -- Purification Potion
     13506, -- Potion of Petrification
+    13510, -- Flask of the Titans
+    13511, -- Flask of Distilled Wisdom
+    13512, -- Flask of Supreme Power
+    17708, -- Elixir of Frost Power
     18253, -- Major Rejuvenation Potion
+    18294, -- Elixir of Greater Water Breathing
+    19440, -- Powerful Anti-Venom
     20008, -- Living Action Potion
     45621, -- Elixir of Minor Accuracy
+    75525, -- Alchemist's Flask
 }
 
 AddonTable.foodDrink = {
--- src/AdiBags_ByExpansion_WoW/professions/crafting/alchemy.lua
+++ src/AdiBags_ByExpansion_WoW/professions/crafting/alchemy.lua
@@ -6,9 +6,12 @@ AddonTable.alchemy = {
     6371,  -- Fire Oil
 
     9061,  -- Goblin Rocket Fuel
+    9149,  -- Philosopher's Stone
     9210,  -- Ghost Dye
 
     12360, -- Arcanite Bar
+
+    13423, -- Stonescale Oil
 
     -- Recipe's
     3832, -- Recipe: Elixir of Detect Lesser Invisibility
