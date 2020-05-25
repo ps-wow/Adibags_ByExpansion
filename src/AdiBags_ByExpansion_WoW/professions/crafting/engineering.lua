adad157db933a6a6b4a0e2f044e70da93ac343a0
David Craig <davidcraig1986@gmail.com>
2020-05-25 10:37:53 +0100

    All wow engineering explosives.

--- src/AdiBags_ByExpansion_WoW/professions/crafting/engineering.lua
+++ src/AdiBags_ByExpansion_WoW/professions/crafting/engineering.lua
@@ -7,20 +7,45 @@ AddonTable.engineering = {
     4362,  -- Rough Boomstick
     4365,  -- Coarse Dynamite
     4366,  -- Target Dummy
+    4367,  -- Small Seaforium Charge
     4368,  -- Flying Tiger Goggles
     4369,  -- Deadly Blunderbuss
     4370,  -- Large Copper Bomb
     4372,  -- Lovingly Crafted Boomstick
     4374,  -- Small Bronze Bomb
+    4378,  -- Heavy Dynamite
+    4380,  -- Big Bronze Bomb
     4381,  -- Minor Recombobulator
+    4384,  -- Explosive Sheep
+    4390,  -- Iron Grenade
+    4394,  -- Big Iron Bomb
+    4395,  -- Goblin Land Mine
+    4398,  -- Large Seaforium Charge
     4401,  -- Mechanical Squirrel Box [Battle Pet]
+    4403,  -- Portable Bronze Mortar
     4405,  -- Crude Scope
     4406,  -- Standard Scope
+    4852,  -- Flash Bomb
     6219,  -- Arclight Spanner
     6712,  -- Clockwork Box
+    6714,  -- Ez-Thro Dynamite
     9318,  -- Red Firework
+    10507, -- Solid Dynamite
+    10514, -- Mithril Frag Bomb
+    10562, -- Hi-Explosive Bomb
+    10577, -- Goblin Mortar
+    10586, -- The Big One
+    10587, -- Goblin Bomb Dispenser
     10644, -- Recipe: Goblin Rocket Fuel (Crafted by Eng, used by Alchemy)
+    10646, -- Goblin Sapper Charge
     10713, -- Plans: Inlaid Mithril Cylinder (Crafted by Eng, used by Blacksmithing)
+    10727, -- Goblin Dragon Gun
+    15993, -- Thorium Grenade
+    16005, -- Dark Iron Bomb
+    16040, -- Arcane Bomb
+    18588, -- Ez-Thro Dynamite II
+    18594, -- Powerful Seaforium Charge
+    18641, -- Dense Dynamite
     21557, -- Small Red Rocket
     21558, -- Small Blue Rocket
     21559, -- Small Green Rocket
