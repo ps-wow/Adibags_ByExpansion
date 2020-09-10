cb4c71bb7870a85c54f98f1bfdb96b8a69ff3de0
me <here>
2020-09-11 00:24:07 +0100

    fix dupes

--- src/AdiBags_ByExpansion_Legion/professions/secondary/cooking.lua
+++ src/AdiBags_ByExpansion_Legion/professions/secondary/cooking.lua
@@ -3,12 +3,6 @@ local AddonName, AddonTable = ...
 -- Legion Cooking
 AddonTable.cooking = {
     -- Ingredients
-    124118, -- Fatty Bearsteak
-    124119, -- Big Gamy Ribs
-    124120, -- Leyblood
-    124121, -- Wildfowl Egg
-    133680, -- Slice of Bacon
-
     146757, -- Prepared Ingredients
     151653, -- Broken Isles Recipe Scrap
 
