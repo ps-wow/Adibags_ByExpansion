53d5f49c7b5eb0b2cb207c87d0c9121cfad2579a
davidcraig <davidcraig1986@gmail.com>
2020-09-19 21:25:35 +0100

    Add all castle nathria loot from PTR.

--- src/AdiBags_ByExpansion_Shadowlands/raids/castlenathria.lua
+++ src/AdiBags_ByExpansion_Shadowlands/raids/castlenathria.lua
@@ -2,4 +2,131 @@ local AddonName, AddonTable = ...
 
 -- Castle Nathria
 AddonTable.castlenathria = {
+    -- Shriekwing
+    183034, -- Cowled Batwing Cloak
+    182976, -- Double-Chained Utility Belt
+    182993, -- Chiropteran Leggings
+    183027, -- Errant Crusader's Greaves
+    182979, -- Slippers of the Forgotten Heretic
+    -- Huntsman Altimor
+    183258, -- A Memory of Eagletalon's True Focus
+    183361, -- A Memory of Spiritwalker's Tidal Totem
+    183235, -- A Memory of The Natural Order's Will
+    183892, -- Mystic Anima Spherule
+    183040, -- Charm of Eternal Winter
+    182988, -- Master Huntsman's Bandolier
+    182996, -- Grim Pursuant's Maille
+    183018, -- Hellhound Cuffs
+    182995, -- Spell-Woven Tourniquet
+    -- Sun King's Salvation
+    183304, -- A Memory of Shadowbreaker, Dawn of the Sun
+    183277, -- A Memory of Sun King's Blessing
+    183893, -- Abominable Anima Spherule
+    183033, -- Mantle of Manifest Sins
+    182986, -- High Torturer's Smock
+    182977, -- Bangles of Errant Pride
+    183007, -- Bleakwing Assassin's Grips
+    183025, -- Stoic Guardsman's Belt
+    -- Artificer Xy'mox
+    183370, -- A Memory of Balespider's Burning Core
+    183296, -- A Memory of Last Emporer's Capacitor
+    183888, -- Apogee Anima Bead
+    183960, -- Portable Pocket Dimension [Bag]
+    182987, -- Breastplate of Cautious Calculation
+    183019, -- Precisely Calibrated Chronometer
+    183004, -- Shadewarped Sash
+    183012, -- Greaves of Enigmatic Energies
+    183038, -- Hyperlight Band
+    -- Hungering Destroyer
+    182630, -- A Memory of Gorefiend's Domination
+    183391, -- A Memory of The Wall
+    183891, -- Venerated Anima Spherule
+    183001, -- Helm of Insatiable Appetite
+    182994, -- Epaulettes of Overwhelming Force
+    183000, -- Consumptive Chainmail Carapace
+    183009, -- Miasma-lacquered Jerkin
+    183028, -- Cinch of Infinite Tightness
+    182992, -- Endlessly Gluttonous Greaves
+    183024, -- Volatile Shadestitch Legguards
+    -- Lady Inerva Darkvein
+    183218, -- A Memory of Cloak of Fel Flames
+    183240, -- A Memory of Memory of the Mother Tree
+    183889, -- Thaumaturgic Anima Bead
+    183021, -- Confidant's Favored Cap
+    183026, -- Gloves of Phantom Shadows
+    183015, -- Binding of Warped Desires
+    182985, -- Memento-Laden Cuisses
+    183037, -- Ritulist's Treasured Ring
+    -- The Council of Blood
+    183328, -- A Memory of Talbadar's Stratagem
+    183334, -- A Memory of the Dashing Scoundrel
+    183890, -- Zenith Anima Spherule
+    183039, -- Noble's Birthstone Pendant
+    182989, -- Corset of the Deft Duelist
+    183014, -- Castellan's Chainlink Grips
+    183011, -- Courtier's Costume Trousers
+    183030, -- Enchanted Toe-Tappers
+    183023, -- Sparkling Glass Slippers
+    182983, -- Stoneguard Attendant's Boots
+    -- Sludgefist
+    183374, -- A Memory of Cinders of the Azj'Aqir
+    183318, -- A Memory of Clarity of Mind
+    183233, -- A Memory of Frenzyband
+    183340, -- A Memory of Greenskin's Wickers
+    182635, -- A Memory of Koltira's Favor
+    183356, -- A Memory of Primal Lava Actuators
+    183264, -- A Memory of Rylakstalker's Confounding Strikes
+    183272, -- A Memory of Siphon Storm
+    183293, -- A Memory of Tear of Morning
+    183309, -- A Memory of Ardent Protector's Sanctum
+    183389, -- A Memory of Will of the Berserker
+    182999, -- Rampaging Giant's Chestplate
+    182984, -- Colossal Plate Gauntlets
+    183022, -- Impossibly Oversized Mitts
+    183005, -- Heedless Pugilist's Harness
+    183016, -- Load-Bearing Belt
+    182981, -- Leggings of Lethal Reverberations
+    183006, -- Stoneclas Stompers
+    -- Stone Legion Generals
+    183346, -- A Memory of Ancestral Reminder
+    183250, -- A Memory of Call of the Wild
+    183223, -- A Memory of Circle of Life and Death
+    183330, -- A Memory of Essence of Bloodfang
+    183267, -- A Memory of Expanded Potential
+    183283, -- A Memory of Invoker's Delight
+    183367, -- A Memory of Mark of Borrowed Power
+    183299, -- A Memory of Of Dust and Dawn
+    183213, -- A Memory of Sigil of the Illidari
+    183381, -- A Memory of Signet of Tormented Kings
+    182627, -- A Memory of Superstrain
+    183316, -- A Memory of Twins of the Sun Priestess
+    183895, -- Apogee Anima Bead
+    183894, -- Thaumaturgic Anima Bead
+    183029, -- Wicked Flanker's Gorget
+    183032, -- Crest of the Legionnaire General
+    182998, -- Robes of the Cursed Commando
+    182991, -- Oathsworn Soldier's Gauntlets
+    183002, -- Ceremonial Parade Legguards
+    -- Sire Denathrius
+    183288, -- A Memory of Celestial Infusion
+    183214, -- A Memory of Chaos Theory
+    182636, -- A Memory of Deadliest Coil
+    183384, -- A Memory of Exploiter
+    183344, -- A Memory of Finality
+    183279, -- A Memory of Freezing Winds
+    183324, -- A Memory of Harmonious Apparatus
+    183362, -- A Memory of Malefic Wrath
+    183227, -- A Memory of Oneth's Clear Vision
+    183256, -- A Memory of Qa'pla, Eredun War Order
+    183352, -- A Memory of Skybreaker's Fiery Demise
+    183310, -- A Memory of Vanguard's Momentum
+    183896, -- Abominable Anima Spherule
+    183897, -- Mystic Anima Spherule
+    183898, -- Venerated Anima Spherule
+    183899, -- Zenith Anima Spherule
+    182997, -- Diadem of Imperious Desire
+    182980, -- Sadist's Sinister Mask
+    183003, -- Pauldrons of Fatal Finality
+    183020, -- Shawl of the Penitent
+    183036, -- Most Regal Signet of Sire Denathrius
 }
