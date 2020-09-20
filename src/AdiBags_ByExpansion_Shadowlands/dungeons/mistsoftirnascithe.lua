66eefa126f66106c0195d013fc285a2e92650a1a
davidcraig <davidcraig1986@gmail.com>
2020-09-20 12:25:28 +0100

    Add loot for mists of tirna scithe (ptr).

--- src/AdiBags_ByExpansion_Shadowlands/dungeons/mistsoftirnascithe.lua
+++ src/AdiBags_ByExpansion_Shadowlands/dungeons/mistsoftirnascithe.lua
@@ -2,4 +2,47 @@ local AddonName, AddonTable = ...
 
 -- Mists of Tirna
 AddonTable.mistsoftirnascithe = {
+    -- Ingra Maloch
+    182582, -- Enkindled Spirit [Potency Conduit]
+    181844, -- Pain Transformation [Potency Conduit]
+    182686, -- Powerful Precision [Potency Conduit]
+    183491, -- Ready for Anything [Potency Conduit]
+    182143, -- Swirling Currents [Potency Conduit]
+    178713, -- Drustlord's Greataxe
+    178709, -- Scithewood Scepter
+    178692, -- Soulthorn Visage
+    178694, -- Wrathbark Greathelm
+    178696, -- Ingra Maloch's Mantle
+    178698, -- Rainshadow Hauberk
+    178704, -- Deathshackle Wristwraps
+    178700, -- Clasp of Waning Shadow
+    178708, -- Unbound Changeling
+    -- Mistcaller
+    181462, -- Coordinated Offensive [Potency Conduit]
+    182754, -- Duplicitous Havoc [Potency Conduit]
+    181734, -- Magi's Brand [Potency Conduit]
+    183514, -- Perforated Veins [Potency Conduit]
+    183482, -- Sudden Ambush [Potency Conduit]
+    178710, -- Tanglewood Thorn
+    178691, -- Hood of the Hidden Path
+    178707, -- Trailspinner Pendant
+    178697, -- Prankster's Pauldrons
+    178695, -- Wintersnap Shoulderguards
+    178706, -- Fogweaver Gauntlets
+    178705, -- Tricksprite Gloves
+    178715, -- Mistcaller Ocarina
+    -- Tred'ova
+    182206, -- Convocation of the Dead [Potency Conduit]
+    183132, -- Echoing Call [Potency Conduit]
+    182142, -- Embrace of Earth [Potency Conduit]
+    183485, -- Savage Combatant [Potency Conduit]
+    182471, -- Soul Furnace [Potency Conduit]
+    178714, -- Lakali's Spire of Knowledge
+    178711, -- Axe of the Deadgrove
+    178712, -- Acidslough Bulwark
+    178693, -- Cocoonsilk Cowl
+    178702, -- Bands of the Undergrowth
+    178703, -- Hiveswarm Bracers
+    178699, -- Sapgorger Belt
+    178701, -- Gormshell Greaves
 }
