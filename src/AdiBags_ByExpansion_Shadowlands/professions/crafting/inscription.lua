b1c4c5fb9be9984a37b9961a4e05ac0030fc78b7
davidcraig <davidcraig1986@gmail.com>
2020-09-20 14:42:39 +0100

    Add all current inscription crafts on ptr.

--- src/AdiBags_ByExpansion_Shadowlands/professions/crafting/inscription.lua
+++ src/AdiBags_ByExpansion_Shadowlands/professions/crafting/inscription.lua
@@ -5,6 +5,35 @@ AddonTable.inscription = {
     173059, -- Luminous Ink
     173058, -- Umbral Ink
     175970, -- Tranquil Ink
+    -- Optional Reagents
+    173161, -- Missive of Critical Strike
+    173160, -- Missive of Haste
+    173162, -- Missive of Mastery
+    173163, -- Missive of Versatility
+    -- Books and Scrolls
+    173048, -- Codex of the Still Mind
+    173049, -- Tome of the Still Mind
+    173065, -- Writ of Grave Robbing
+    -- Contracts
+    173062, -- Contract: Court of Harvesters
+    173051, -- Contract: The Ascended
+    175924, -- Contract: The Undying Army
+    173053, -- Contract: The Wild Hunt
+    -- Cards
+    173066, -- Darkmoon Card of Death
+    177875, -- Darkmoon Card of Putresence
+    177876, -- Darkmoon Card of Repose
+    177873, -- Darkmoon Card of the Indomitable
+    177874, -- Darkmoon Card of Voracity
+    -- Vantus Runes
+    173067, -- Vantus Rune: Castle Nathria
+    -- Staves
+    173428, -- Soul Keeper's Column
+    173054, -- Soul Keeper's Spire
+    -- Off-Hands
+    173050, -- Newly Departed Codex
+    -- Hats
+    180755, -- Fae Revel Masque
     -- Misc
     177841, -- Blank Card of Atonement
     177840, -- Blank Card of Ascendance
