d90cefd0d06c5722c50a6fe9a71d5c44f0d5c43f
davidcraig <davidcraig1986@gmail.com>
2020-09-20 15:23:59 +0100

    Add all blacksmithing from ptr.

--- src/AdiBags_ByExpansion_Shadowlands/professions/crafting/blacksmithing.lua
+++ src/AdiBags_ByExpansion_Shadowlands/professions/crafting/blacksmithing.lua
@@ -1,8 +1,55 @@
 local AddonName, AddonTable = ...
 
+-- Shadowlands Blacksmithing
 AddonTable.blacksmithing = {
-    -- Armour
-    -- Weapons
+    -- Materials
+    180733, -- Luminous Flux
+    -- Armor
+    171442, -- Shadowsteel Breastplate
+    171446, -- Shadowsteel Greaves
+    171447, -- Shadowsteel Pauldrons
+    171445, -- Shadowsteel Helm
+    171444, -- Shadowsteel Gauntlets
+    171448, -- Shadowsteel Waistguard
+    171443, -- Shadowsteel Sabatons
+    171449, -- Shadowsteel Armguards
+    171379, -- Ceremonious Pauldrons
+    171378, -- Ceremonious Greaves
+    171374, -- Ceremonious Breastplate
+    171377, -- Ceremonious Helm
+    171376, -- Ceremonious Gauntlets
+    171380, -- Ceremonious Waistguard
+    171375, -- Ceremonious Sabatons
+    171381, -- Ceremonious Armguards
     -- Other
-    -- Material
+    171441, -- Laestrite Skeleton Key
+    -- Shields
+    171391, -- Ceremonious Shield
+    -- Reagents
+    171428, -- Shadowghast Ingot
+    -- Specialized Armor
+    171419, -- Shadowghast Armguards
+    171412, -- Shadowghast Breastplate
+    171414, -- Shadowghast Gauntlets
+    171416, -- Shadowghast Greaves
+    171415, -- Shadowghast Helm
+    171417, -- Shadowghast Pauldrons
+    171413, -- Shadowghast Sabatons
+    171418, -- Shadowghast Waistguard
+    -- Weapons
+    171383, -- Ceremonious Claymore
+    171390, -- Ceremonious Blade
+    171386, -- Ceremonious Spear
+    171388, -- Ceremonious Axe
+    171382, -- Ceremonious Rapier
+    171392, -- Ceremonious Warglaive
+    171387, -- Ceremonious Mace
+    171389, -- Ceremonious Reaper
+    171384, -- Ceremonious Smasher
+    171385, -- Ceremonious Shanker
+    -- Weapons Mods
+    171439, -- Shaded Weightstone
+    171437, -- Shaded Sharpening Stone
+    171436, -- Porous Sharpening Stone
+    171438, -- Porous Weightstone
 }
