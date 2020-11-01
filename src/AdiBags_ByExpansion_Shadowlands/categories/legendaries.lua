b6c9d13cec925358704b1864f90ced5dcd67afc6
davidcraig <davidcraig1986@gmail.com>
2020-11-01 12:41:37 +0000

    Add shadowlands legendaries category.

---
+++ src/AdiBags_ByExpansion_Shadowlands/categories/legendaries.lua
@@ -0,0 +1,22 @@
+local AddonName, AddonTable = ...
+
+-- Shadowlands Legendaries
+AddonTable.legendaries = {
+    -- Generic
+
+    -- Mage
+    -- Priest
+    -- Warlock
+
+    -- Rogue
+    -- Demon Hunter
+    -- Druid
+    -- Monk
+
+    -- Shaman
+    -- Hunter
+
+    -- Warrior
+    -- Paladin
+    -- Death Knight
+}
