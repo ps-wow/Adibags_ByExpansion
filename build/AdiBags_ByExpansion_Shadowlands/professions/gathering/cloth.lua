28dc566d1048a435bb5861591b1c099ff62f2f50
davidcraig <davidcraig1986@gmail.com>
2020-11-13 18:29:23 +0000

    Add extra info from shadowlands unlearned tab.

--- build/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
+++ build/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
@@ -2,8 +2,6 @@ local AddonName, AddonTable = ...
 AddonTable.cloth = {
     173202,
     173204,
-    -- Enchanted
-    172439,
     -- Vendor Bought
     177062,
 }
--- src/AdiBags_ByExpansion_Shadowlands/professions/crafting/enchanting.lua
+++ src/AdiBags_ByExpansion_Shadowlands/professions/crafting/enchanting.lua
@@ -1,9 +1,14 @@
 local AddonName, AddonTable = ...
 
 AddonTable.enchanting = {
+    -- Reagents
+    172437, -- Enchanted Elethium Bar
+    172438, -- Enchanted Heavy Desolate Hide
+    172439, -- Enchanted Lightless Silk
     -- Materials
     172230, -- Soul Dust
     172231, -- Sacred Shard
+    172232, -- Eternal Crystal
     -- Wands
     172462, -- Enchanted Twilight Wand
 }
