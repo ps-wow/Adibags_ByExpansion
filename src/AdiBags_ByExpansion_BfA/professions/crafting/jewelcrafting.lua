561dcec43db5978229329633c3330409b9470128
Geurtje <tomgeurtsen@gmail.com>
2020-11-20 18:07:53 +0100

    Updated ordering to be based on the item ids

--- src/AdiBags_ByExpansion_BfA/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_BfA/professions/crafting/jewelcrafting.lua
@@ -8,45 +8,41 @@ AddonTable.jewelcrafting = {
     153703, -- Solstone [Uncommon]
     153704, -- Viridium [Uncommon]
     153705, -- Kyanite [Uncommon]
+    153706, -- Kraken's Eye [Epic]
+    154120, -- Owlseye [Rare]
+    154121, -- Scarlet Diamond [Rare]
     154122, -- Tidal Amethyst [Rare]
     154123, -- Amberblaze [Rare]
     154124, -- Laribole [Rare]
-    154120, -- Owlseye [Rare]
-    154121, -- Scarlet Diamond [Rare]
     154125, -- Royal Quartz [Rare]
-    153706, -- Kraken's Eye [Epic]
-    168193, -- Azsharine [Epic]
     168188, -- Sage Agate [Epic]
     168189, -- Dark Opal [Epic]
     168190, -- Lava Lazuli [Epic]
     168191, -- Sea Currant [Epic]
     168192, -- Sand Spinel [Epic]
+    168193, -- Azsharine [Epic]
     168635, -- Leviathan's Eye [Epic]
 
     -- Cut Gems
-    168636, -- Leviathon's Eye of Strength
-    168637, -- Leviathon's Eye of Agility
-    168638, -- Leviathon's Eye of Intellect
-    153707, -- Kraken's Eye of Strength
-    153708, -- Kraken's Eye of Agility
-    153709, -- Kraken's Eye of Intellect
-
+    153707, -- Kraken's Eye of Strength [Epic]
+    153708, -- Kraken's Eye of Agility [Epic]
+    153709, -- Kraken's Eye of Intellect [Epic]
     153710, -- Deadly Solstone [Uncommon]
     153711, -- Quick Golden Beryl [Uncommon]
     153712, -- Versatile Kyanite [Uncommon]
     153713, -- Masterful Kubiline [Uncommon]
     153714, -- Natant Rubellite [Uncommon]
     153715, -- Straddling Viridium [Uncommon]
-
     154126, -- Deadly Amberblaze [Rare]
     154127, -- Quick Owlseye [Rare]
     154128, -- Versatile Royal Quartz [Rare]
     154129, -- Masterful Tidal Amethyst [Rare]
-    169220, -- Straddling Sage Agate [Rare]
-
+    168636, -- Leviathan's Eye of Strength [Epic]
+    168637, -- Leviathan's Eye of Agility [Epic]
+    168638, -- Leviathan's Eye of Intellect [Epic]
     168639, -- Deadly Lava Lazuli [Epic]
     168640, -- Masterful Sea Currant [Epic]
     168641, -- Quick Sand Spinel [Epic]
     168642, -- Versatile Dark Opal [Epic]
-
+    169220, -- Straddling Sage Agate [Rare]
 }
