772146a30ef2eb9a31c153377adb9d59ada183e9
Filip Patafta <fpatafta@live.com>
2020-12-11 12:40:43 +0100

    added conduits shaman/druid, anima powers added to misc

--- src/AdiBags_ByExpansion_Shadowlands/categories/conduits.lua
+++ src/AdiBags_ByExpansion_Shadowlands/categories/conduits.lua
@@ -5,4 +5,58 @@ AddonTable.conduits = {
     -- Mage
     181467, -- Flow of Time
     181498, -- Grounding Surge
+
+    -- Druid
+    340543, -- Innate Resolve
+    340605, -- Layered Mane
+    340529, -- Tough as Bark
+    340540, -- Ursine Vigor
+    340553, -- Well-Honed Instincts
+    341280, -- Born Anew
+    341451, -- Born of the Wilds
+    341450, -- Front of the Pack
+    340545, -- Tireless Pursuit
+    340705, -- Carnivorous Instinct
+    341446, -- Conflux of Elements
+    341378, -- Deep Allegiance
+    341383, -- Endless Thirst
+    341447, -- Evolved Swarm
+    340616, -- Flash of Clarity
+    340621, -- Floral Recycling
+    340708, -- Fury of the Skies
+    340686, -- Incessant Hunter
+    340706, -- Precise Alignment
+    340550, -- Ready for Anything
+    340609, -- Savage Combatant
+    340720, -- Stellar Inspiration
+    340694, -- Sudden Ambush
+    340682, -- Taste for Blood
+    340719, -- Umbral Intensity
+    340552, -- Unchecked Aggression
+    340549, -- Unstoppable Growth
+
+    -- Shaman
+    337964, -- Astral Protection
+    337974, -- Refreshing Waters
+    337981, -- Vital Accretion
+    338054, -- Crippling Hex
+    338048, -- Spiritual Resonance
+    338033, -- Thunderous Paws
+    338042, -- Totemic Surge
+    338303, -- Call of Flame
+    338325, -- Chilled to the Core
+    339182, -- Elysian Dirge
+    338329, -- Embrace of Earth
+    339183, -- Essential Extraction
+    338322, -- Focused Lightning
+    338343, -- Heavy Rainfall
+    338131, -- High Voltage
+    339185, -- Lavish Harvest
+    338331, -- Magma Fist
+    338346, -- Nature's Focus
+    345594, -- Pyroclastic Shock
+    338252, -- Shake the Foundations
+    338339, -- Swirling Currents
+    339186, -- Tumbling Waves
+    338318, -- Unruly Winds
 }
--- src/AdiBags_ByExpansion_Shadowlands/categories/misc.lua
+++ src/AdiBags_ByExpansion_Shadowlands/categories/misc.lua
@@ -1,5 +1,20 @@
 local AddonName, AddonTable = ...
 
 -- Shadowlands Misc.
+-- I will put anima items here
 AddonTable.misc = {
+    -- Anima
+    183727, -- Resonance of Conflict
+    184371, -- Vivacity of Collaboration
+    181743, -- Plume of the Archon
+    184315, -- Multi-Modal Anima Container
+    184387, -- Misty Shimmerleaf
+    184766, -- Chronicles of the Paragons
+    184383, -- Duskfall Tuber
+    181477, -- Ardendew Pearl
+    184777, -- Gravedredger's Shovel
+    181548, -- Darkhaven Soul Lantern
+    184767, -- Handheld Soul Mirror
+    184765, -- Vesper Strikehammer
+    184773, -- Battle-Tested Armor Componen
 }
