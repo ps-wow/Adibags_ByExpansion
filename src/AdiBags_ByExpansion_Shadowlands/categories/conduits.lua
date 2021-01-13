74c7738b58c7c697137eeb9c23251ba56f236a36
davidcraig <davidcraig1986@gmail.com>
2021-01-13 12:30:32 +0000

    Move all conduits from dungeons into conduits category.

--- src/AdiBags_ByExpansion_Shadowlands/categories/conduits.lua
+++ src/AdiBags_ByExpansion_Shadowlands/categories/conduits.lua
@@ -5,4 +5,145 @@ AddonTable.conduits = {
     -- Mage
     181467, -- Flow of Time
     181498, -- Grounding Surge
+    -- Hunter
+    183396, -- Flame Infusion
+
+    -- Misc
+    182624, -- Show of Force
+    183509, -- Sleight of Hand
+    181509, -- Arcane Prodigy
+    182748, -- Borne of Blood
+    183490, -- Florall Recycling
+    182145, -- Heavy Rainfall
+    181376, -- Inner Fury
+    182138, -- Mind Devourer
+    182684, -- Resolute Defender
+    182736, -- Rolling Agony
+    181737, -- Nourishing Chi
+    182621, -- One With the Beast
+    183479, -- Umbral Intensity
+    182134, -- Unruly Winds
+    183504, -- Well-Placed Steel
+    181848, -- Accelerated Cold
+    180935, -- Crash the Ramparts
+    181845, -- Exaltation
+    181736, -- Flame Accretion
+    182681, -- Vengeful Shock
+    183511, -- Deeper Daggers
+    181735, -- Hack and Slash
+    183487, -- Layered Mane
+    182127, -- Shake the Foundations
+    183184, -- Stinging Strike
+    181963, -- Blood Bond
+    181867, -- Swift Penitence
+    182675, -- Untemptered Dedication
+    182598, -- Demon Muzzle
+    181461, -- Ice Bite
+    183477, -- Precise Alignment
+    181944, -- Resonant Words
+    182751, -- Tyrant's Soul
+    182582, -- Enkindled Spirit
+    181844, -- Pain Transformation
+    182686, -- Powerful Precision
+    183491, -- Ready for Anything
+    182143, -- Swirling Currents
+    181462, -- Coordinated Offensive
+    182754, -- Duplicitous Havoc
+    181734, -- Magi's Brand
+    183514, -- Perforated Veins
+    183482, -- Sudden Ambush
+    182206, -- Convocation of the Dead
+    183132, -- Echoing Call
+    182142, -- Embrace of Earth
+    183485, -- Savage Combatant
+    182385, -- Growing Inferno
+    181742, -- Walk with the Ox
+    182471, -- Soul Furnace
+    182125, -- Earth and Sky
+    181709, -- Unnerving Focus
+    183505, -- Maim, Mangle
+    181641, -- Rising Sun Revival
+    181982, -- Biting Cold
+    183402, -- Bloodletting
+    181712, -- Depths of Insanity
+    183481, -- Incessant Hunter
+    182772, -- Infernal Brand
+    181389, -- Shivering Core
+    181738, -- Artifice of the Archmage
+    182750, -- Carnivorous Stalkers
+    183512, -- Planned Execution
+    181700, -- Scalding Brew
+    182136, -- Chilled to the Core
+    182622, -- Resplendent Light
+    181843, -- Shining Radiance
+    182201, -- Unleashed Frenzy
+    181383, -- Unrelenting Cold
+    182368, -- Demon's Touch
+    182208, -- Lingering Plague
+    183167, -- Strength of the Pack
+    182747, -- Cold Embrace
+    181943, -- Eradicating Blow
+    183478, -- Fury of the Skies
+    181504, -- Infernal Cascade
+    182139, -- Rapid Shadows
+    180933, -- Ashen Juggernaut
+    182140, -- Dissonant Echoes
+    182752, -- Fel Commando
+    181455, -- Icy Propulsion
+    181495, -- Jade Bond
+    182187, -- Meat Shield
+    181786, -- Eternal Hunger
+    183506, -- Lethal Poisons
+    182465, -- Truth's Wake
+    183486, -- Well-Honed Instincts
+    183202, -- Deadly Tandem
+    181740, -- Evasive Stride
+    183480, -- Taste for Blood
+    181776, -- Vicious Contempt
+    182755, -- Ashen Remains
+    181942, -- Focused Mending
+    182604, -- Roaring Fire
+    183489, -- Flash of Clarity
+    181847, -- Lasting Spirit
+    182137, -- Magma Fist
+    182608, -- Virtuous Command
+    182677, -- Punish the Guilty
+    183507, -- Triple Threat
+    182649, -- Brutal Projectiles
+    182667, -- Focused Light
+    182135, -- Focused Lightning
+    182141, -- Holy Oration
+    183488, -- Unstoppable Growth
+    181465, -- Xuen's Bond
+    182126, -- High Voltage
+    180944, -- Merciless Bonegrinder
+    182753, -- Royal Decree
+    183508, -- Ambidexterity
+    181435, -- Calculated Strikes
+    182478, -- Corrupting Leer
+    182462, -- Expurgation
+    183483, -- Carniveorous Instinct
+    182610, -- Ferocious Appetite
+    182468, -- Mortal Combo
+    183513, -- Stilleto Staccato
+    181705, -- Celestial Effervescence
+    183197, -- Controlled Destruction
+    183503, -- Poisoned Katar
+    183484, -- Unchecked Aggression
+    183510, -- Count the Odds
+    180932, -- Iron Maiden
+    181866, -- Withering Plague
+    182283, -- Dancing with Fate
+    182657, -- Deadly Chain
+    182203, -- Debilitating Malady
+    182559, -- Templar's Vindication
+    182128, -- Call of Flame
+    182769, -- Combusting Engine
+    181980, -- Embrace Death
+    181506, -- Master Flame
+    182743, -- Focused Malignancy
+    182131, -- Haunting Apparitions
+    181511, -- Nether Precision
+    182648, -- Sharpshooter's Focus
+    183476, -- Stellar Inspiration
 }
