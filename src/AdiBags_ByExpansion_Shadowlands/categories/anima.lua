28f95e35f884ec374c3688c82aef885ac242680f
davidcraig <davidcraig1986@gmail.com>
2021-06-23 13:17:54 +0100

    Remove duplicate and sort by id.

--- src/AdiBags_ByExpansion_Shadowlands/categories/anima.lua
+++ src/AdiBags_ByExpansion_Shadowlands/categories/anima.lua
@@ -2,29 +2,27 @@ local AddonName, AddonTable = ...
 
 AddonTable.anima = {
     181368, -- Centurion Power Core
+    181477, -- Ardendew Pearl
     181479, -- Starlight Catcher
     181540, -- Animaflower bud
     181541, -- Celestial Acorn
     181544, -- Confessions of Misdeed
     181547, -- Noble's Draught
+    181548, -- Darkhaven Soul Lantern
     181549, -- Timeworn Sinstone
     181551, -- Depleted Stoneborn Heart
     181642, -- Novice Principles of Plaguistry
     181643, -- Weeping Corpseshroom
     181644, -- Unbalanced Culture Jars
+    181743, -- Plume of the Archon
     181745, -- Forgesmith's Coal
 
     183727, -- Resonance of Conflict
-    181743, -- Plume of the Archon
-    184315, -- Multi-Modal Anima Container
-    184387, -- Misty Shimmerleaf
-    181477, -- Ardendew Pearl
-    184777, -- Gravedredger's Shovel
-    181548, -- Darkhaven Soul Lantern
-    184767, -- Handheld Soul Mirror
-    184765, -- Vesper Strikehammer
-    184773, -- Battle-Tested Armor Componen
 
+    181645, -- Engorged Monstosity Hearth
+    181647, -- Stabilized Plague Strain
+    181648, -- Ziggurat Focusing Crystal
+    181649, -- Preserved Preternatural Braincase
     184146, -- Singed Soul Shackles
     184148, -- Concealed Sinvyr Flask
     184151, -- Counterfeit Ruby Brooch
@@ -33,7 +31,10 @@ AddonTable.anima = {
     184305, -- Maldraxxi Champion's Armaments
     184306, -- Soulcathcing Sludge
     184307, -- Maldraxxi Armor Scraps
+    184315, -- Multi-Modal Anima Container
+    184338, -- Plump Glitterrot
     184360, -- Musings on Repetition
+    184363, -- Consideration on Courage
     184371, -- Vivacity of Collaboration
     184373, -- Small Anima Globe
     184374, -- Cartel Exchange Vessel
@@ -41,17 +42,14 @@ AddonTable.anima = {
     184382, -- Luminous Sylberry
     184383, -- Duskfall Tuber
     184384, -- Hibernal Sproutling
+    184387, -- Misty Shimmerleaf
     184763, -- Mnemis Neural Network
+    184765, -- Vesper Strikehammer
     184766, -- Chronicles of the Paragons
+    184767, -- Handheld Soul Mirror
     184772, -- Ritual Maldracite Crystal
-    181645, -- Engorged Monstosity Hearth
+    184773, -- Battle-Tested Armor Componen
     184774, -- Juvenile Sporespindle
-    181647, -- Stabilized Plague Strain
-    184338, -- Plump Glitterrot
-    184363, -- Consideration on Courage
-    184763, -- Mnemis Neural Network,
-    181648, -- Ziggurat Focusing Crystal
-    181649, -- Preserved Preternatural Braincase
     184775, -- Necromancy for practical ritualist
-
+    184777, -- Gravedredger's Shovel
 }
