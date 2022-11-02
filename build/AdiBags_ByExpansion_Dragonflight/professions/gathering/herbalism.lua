ae076f261bd22deea9c4b966bc138a34b964efba
David Craig <david@dcraig.dev>
2022-11-02 11:31:00 +0000

    Add all herbs currently on wowhead.

--- build/AdiBags_ByExpansion_Dragonflight/professions/gathering/herbalism.lua
+++ build/AdiBags_ByExpansion_Dragonflight/professions/gathering/herbalism.lua
@@ -1,4 +1,15 @@
 local AddonName, AddonTable = ...
 -- Dragonflight Herbalism
 AddonTable.herbalism = {
+    191460,
+    191461,
+    191462,
+    191470,
+    191471,
+    191472,
+    191464,
+    191465,
+    191467,
+    191468,
+    191469,
 }
