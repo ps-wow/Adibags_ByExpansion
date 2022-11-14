7a1d4b2008eb605ae8b06d603a9e153b556cbedd
David Craig <david@dcraig.dev>
2022-11-14 09:36:06 +0000

    Merge 9.2 cloth pr.

--- build/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
+++ build/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
@@ -1,6 +1,8 @@
 local AddonName, AddonTable = ...
+-- Shadowlands Cloth
 AddonTable.cloth = {
     173202,
     173204,
+    -- 9.2
     187703,
 }
--- src/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
+++ src/AdiBags_ByExpansion_Shadowlands/professions/gathering/cloth.lua
@@ -1,6 +1,9 @@
 local AddonName, AddonTable = ...
 
+-- Shadowlands Cloth
 AddonTable.cloth = {
     173202, -- Shrouded Cloth
     173204, -- Lightless Silk
+    -- 9.2
+    187703, -- Silken Protofiber
 }
