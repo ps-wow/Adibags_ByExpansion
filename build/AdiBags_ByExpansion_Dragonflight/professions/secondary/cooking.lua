5d26a104d7a8e9e13c062d3547af0bd10100ec74
David Craig <david@dcraig.dev>
2022-11-29 09:54:04 +0000

    Eggs, protein, yum :)

--- build/AdiBags_ByExpansion_Dragonflight/professions/secondary/cooking.lua
+++ build/AdiBags_ByExpansion_Dragonflight/professions/secondary/cooking.lua
@@ -4,5 +4,7 @@ AddonTable.cooking = {
     -- Meat
     197744,
     197741,
+    -- Other
     197754,
+    197745,
 }
--- src/AdiBags_ByExpansion_Dragonflight/professions/secondary/cooking.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/secondary/cooking.lua
@@ -5,5 +5,7 @@ AddonTable.cooking = {
     -- Meat
     197744, -- Hornswog Hunk
     197741, -- Maybe Meat
+    -- Other
     197754, -- Salt Deposit
+    197745, -- Basilisk Eggs
 }
