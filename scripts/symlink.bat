3506e94ebdb299044b59cf0f5e70aee68bb7b667
davidcraig <davidcraig-uk@protonmail.com>
2023-11-09 12:40:29 +0000

    Update paths

--- scripts/symlink.bat
+++ scripts/symlink.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set "source=D:\Code\Adibags_ByExpansion\build"
-set "dest=C:\Games\World of Warcraft\_retail_\Interface\AddOns"
+set "source=E:\Code\Adibags_ByExpansion\build"
+set "dest=E:\Games\World of Warcraft\_retail_\Interface\AddOns"
 
-mklink /D "%dest%\AdiBags_ByExpansion" "D:\Code\Adibags_ByExpansion\"
+mklink /D "%dest%\AdiBags_ByExpansion" "E:\Code\Adibags_ByExpansion\"
 mklink /D "%dest%\AdiBags_ByExpansion_WoW" "%source%\AdiBags_ByExpansion_WoW"
 mklink /D "%dest%\AdiBags_ByExpansion_TBC" "%source%\AdiBags_ByExpansion_TBC"
 mklink /D "%dest%\AdiBags_ByExpansion_Wrath" "%source%\AdiBags_ByExpansion_Wrath"
