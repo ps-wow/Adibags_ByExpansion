639a35bbe4c21edf514caa1aae1c833774b96706
davidcraig <davidcraig-uk@protonmail.com>
2023-11-12 23:54:17 +0000

    Add some JC items.

--- build/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ build/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -1,3 +1,13 @@
 local AddonName, AddonTable = ...
 AddonTable.jewelcrafting = {
+    -- Gems (Uncut)
+    192866,
+    192859,
+    192849,
+    192846,
+    -- Stone/Glass
+    192880,
+    192872,
+    -- Gems (Cut)
+    192913,
 }
--- src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -1,3 +1,13 @@
 local AddonName, AddonTable = ...
 AddonTable.jewelcrafting = {
+    -- Gems (Uncut)
+    192866, -- Nozdorite [Bronze (1)]
+    192859, -- Ysemerald [Bronze (1)]
+    192849, -- Eternity Amber [Bronze (1)]
+    192846, -- Sundered Onyx [Bronze (1)]
+    -- Stone/Glass
+    192880, -- Crumbled Stone
+    192872, -- Fractured Glass
+    -- Gems (Cut)
+    192913, -- Solid Eternity Amber [Bronze (1)]
 }
