1b78c520273ec171a835f607d0ca4e321aa7407b
davidcraig <davidcraig-uk@protonmail.com>
2023-11-18 15:47:45 +0000

    Add severite 2 and 3

--- src/AdiBags_ByExpansion_Dragonflight/professions/gathering/mining.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/gathering/mining.lua
@@ -5,7 +5,9 @@ AddonTable.mining = {
     189143, -- Draconium Ore (Bronze [1])
     188658, -- Draconium Ore (Silver [2])
     190312, -- Khaz'gorite Ore (Rare)
-    190395, -- Serevite Ore
+    190394, -- Severite Ore (Gold [3])
+    190395, -- Serevite Ore (Bronze [1])
+    190396, -- Severite Ore (Silver [2])
     197754, -- Salt Deposit
 
     -- Knowledge
