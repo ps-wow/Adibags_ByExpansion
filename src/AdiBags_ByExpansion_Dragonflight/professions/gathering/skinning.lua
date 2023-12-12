a9b6aa2689a099897dba71e7785ad66be9e5ee7b
davidcraig <davidcraig-uk@protonmail.com>
2023-12-12 12:51:55 +0000

    Add some more skinning materials

--- src/AdiBags_ByExpansion_Dragonflight/professions/gathering/skinning.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/gathering/skinning.lua
@@ -6,7 +6,10 @@ AddonTable.skinning = {
     193210, -- Resilient Leather [Silver (2)]
     193213, -- Adamant Scales [Bronze (1)]
     193214, -- Adamant Scales [Silver (2)]
+    193215, -- Adamant Scales [Gold (3)]
     193216, -- Dense Hide [Bronze (1)]
+    193217, -- Dense Hide [Silver (2)]
+    193223, -- Lustrous Scaled Hide [Silver (2)]
     193252, -- Salamanther Scales
     193259, -- Flawless Proto Dragon Scale
 }
