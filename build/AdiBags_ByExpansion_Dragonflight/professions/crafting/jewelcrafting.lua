55a832886c7970f1ddab5be52f63dc4aa4b72302
davidcraig <davidcraig-uk@protonmail.com>
2024-02-05 12:46:45 +0000

    Add more JC crafts.

--- build/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ build/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -2,19 +2,32 @@ local AddonName, AddonTable = ...
 -- Dragonflight Jewelcrafting
 AddonTable.jewelcrafting = {
     -- Gems (Uncut)
+    192837,
+    192838,
     192840,
     192843,
+    192844,
     192846,
     192847,
     192849,
+    192850,
     192852,
     192853,
+    192857,
     192859,
+    192862,
     192866,
     192867,
+    -- Jewelry
+    192996,
+    194082,
     -- Stone/Glass
     192880,
     192872,
     -- Gems (Cut)
+    192900,
+    192903,
+    192906,
+    192910,
     192913,
 }
--- src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_Dragonflight/professions/crafting/jewelcrafting.lua
@@ -3,19 +3,34 @@ local AddonName, AddonTable = ...
 -- Dragonflight Jewelcrafting
 AddonTable.jewelcrafting = {
     -- Gems (Uncut)
+    192837, -- Queen's Ruby [Bronze (1)]
+    192838, -- Queen's Ruby [Silver (2)]
     192840, -- Mystic Sapphire [Bronze (1)]
     192843, -- Vibrant Emerald [Bronze (1)]
+    192844, -- Vibrant Emerald [Silver (2)]
     192846, -- Sundered Onyx [Bronze (1)]
     192847, -- Sundered Onyx [Silver (2)]
     192849, -- Eternity Amber [Bronze (1)]
+    192850, -- Eternity Amber [Silver (2)]
     192852, -- Alexstraszite [Bronze (1)]
     192853, -- Alexstraszite [Silver (2)]
+    192857, -- Malygite [Silver (2)]
     192859, -- Ysemerald [Bronze (1)]
+    192862, -- Neltharite [Bronze (1)]
     192866, -- Nozdorite [Bronze (1)]
     192867, -- Nozdorite [Silver (2)]
+    -- Jewelry
+    192996, -- Pendant of Impending Perils
+    194082, -- Band of New Beginnings
     -- Stone/Glass
     192880, -- Crumbled Stone
     192872, -- Fractured Glass
+    -- Extravagant Glasswares
+    202052, -- Timewatcher's Patience [Silver (2)]
     -- Gems (Cut)
+    192900, -- Crafty Queen's Ruby [Bronze (1)]
+    192903, -- Zen Mystic Sapphire [Bronze (1)]
+    192906, -- Energized Vibrant Emerald [Bronze (1)]
+    192910, -- Sensei's Sundered Onyx [Bronze (1)]
     192913, -- Solid Eternity Amber [Bronze (1)]
 }
