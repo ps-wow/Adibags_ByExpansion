d2ef3c821f989687a6a970ea61b6da26cb77d22f
davidcraig <davidcraig-uk@protonmail.com>
2024-02-06 13:02:51 +0000

    Add Talasite

--- build/AdiBags_ByExpansion_TBC/professions/crafting/jewelcrafting.lua
+++ build/AdiBags_ByExpansion_TBC/professions/crafting/jewelcrafting.lua
@@ -1,6 +1,8 @@
 local AddonName, AddonTable = ...
 -- TBC Jewelcrafting
 AddonTable.jewelcrafting = {
+    -- Gems (Uncut)
+    23437,
     -- Recipe's
     24169,
     32303,
--- src/AdiBags_ByExpansion_TBC/professions/crafting/jewelcrafting.lua
+++ src/AdiBags_ByExpansion_TBC/professions/crafting/jewelcrafting.lua
@@ -2,6 +2,8 @@ local AddonName, AddonTable = ...
 
 -- TBC Jewelcrafting
 AddonTable.jewelcrafting = {
+    -- Gems (Uncut)
+    23437, -- Talasite
     -- Recipe's
     24169, -- Design: Eye of the Night
     32303, -- Design: Inscribed Pyrestone
